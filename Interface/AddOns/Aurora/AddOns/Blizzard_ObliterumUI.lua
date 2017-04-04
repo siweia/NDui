cc0a76fe3c076200ccc823da65e59106dccfa1d0
siweia <siwei1990@outlook.com>
2017-04-04 23:00:37 +0800

    Remove empty tooltip fix, cos it taints

--- Interface/AddOns/Aurora/AddOns/Blizzard_ObliterumUI.lua
+++ Interface/AddOns/Aurora/AddOns/Blizzard_ObliterumUI.lua
@@ -1,12 +1,10 @@
 local F, C = unpack(select(2, ...))
 
 C.themes["Blizzard_ObliterumUI"] = function()
-	F.ReskinPortraitFrame(ObliterumForgeFrame)
-	ObliterumForgeFrameBtnCornerLeft:Hide()
-	ObliterumForgeFrameBtnCornerRight:Hide()
-	ObliterumForgeFrameButtonBottomBorder:Hide()
-	ObliterumForgeFrameInset:Hide()
-	F.Reskin(ObliterumForgeFrame.ObliterateButton)
-	ObliterumForgeFrame.ItemSlot.Icon:SetTexCoord(.08, .92, .08, .92)
-	F.CreateBDFrame(ObliterumForgeFrame.ItemSlot.Icon)
+	local obliterum = ObliterumForgeFrame
+
+	F.ReskinPortraitFrame(obliterum, true)
+	F.Reskin(obliterum.ObliterateButton)
+	obliterum.ItemSlot.Icon:SetTexCoord(.08, .92, .08, .92)
+	F.CreateBDFrame(obliterum.ItemSlot.Icon)
 end
--- Interface/AddOns/NDui/Modules/Bags/Core.lua
+++ Interface/AddOns/NDui/Modules/Bags/Core.lua
@@ -364,16 +364,3 @@ function module:OnLogin()
 	SetSortBagsRightToLeft(not NDuiDB["Bags"]["ReverseSort"])
 	SetInsertItemsLeftToRight(false)
 end
-
--- Fix Empty Toolip
-NDui:EventFrame("BAG_UPDATE_DELAYED"):SetScript("OnEvent", function()
-	local done
-	if BagsMain and BagsMain:IsShown() and GameTooltip:IsShown() then
-		GameTooltip:HookScript("OnTooltipCleared", function(self)
-			if not done and self:NumLines() == 0 and self:IsShown() then
-				self:Hide()
-				done = true
-			end
-		end)
-	end
-end)
