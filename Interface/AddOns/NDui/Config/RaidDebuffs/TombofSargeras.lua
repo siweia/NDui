0ba450a017aad005f7e4a2c4c7f66810bb287a3e
siweia <siwei1990@outlook.com>
2017-06-11 19:44:31 +0800

    Add TOS raiddebuff namespace

---
+++ Interface/AddOns/NDui/Config/RaidDebuffs/TombofSargeras.lua
@@ -0,0 +1,33 @@
+local module = NDui:GetModule("RaidFrameAuras")
+if not module then return end
+
+local TIER = 7 -- Legion
+local INSTANCE = 875 -- 萨墓
+local BOSS
+
+BOSS = 1862 -- 格罗斯
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1867 -- 恶魔审判庭
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1856 -- 哈亚坦
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1903 -- 月之姐妹
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1861 -- 主母萨丝琳
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1896 -- 绝望的聚合体
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1897 -- 戒卫侍女
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1873 -- 堕落的化身
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
+
+BOSS = 1898 -- 基尔加丹
+--module:RegisterDebuff(TIER, INSTANCE, BOSS, 203096)
