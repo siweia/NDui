023d09b72d83b0d9159c7252920ce53556738a99
siweia <siwei1990@outlook.com>
2017-07-02 13:48:01 +0800

    Use default ilvl retrieving

--- Interface/AddOns/NDui/Modules/Misc/ItemLevel.lua
+++ Interface/AddOns/NDui/Modules/Misc/ItemLevel.lua
@@ -43,7 +43,7 @@ function module:ShowItemLevel()
 			local link = GetInventoryItemLink(unit, id)
 			if link and id ~= 4 then
 				local _, _, quality, level = GetItemInfo(link)
-				level = NDui:GetItemLevel(link, quality) or level
+				level = GetDetailedItemLevelInfo(link) or level
 
 				if level and level > 1 and quality then
 					local color = BAG_ITEM_QUALITY_COLORS[quality]
