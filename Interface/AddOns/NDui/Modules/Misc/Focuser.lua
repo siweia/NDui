51775252b3d6a4a259f919f2e4fb2561848cdbe8
siweia <siwei1990@outlook.com>
2017-07-09 16:51:21 +0800

    Update focuser

--- Interface/AddOns/NDui/Modules/Misc/Focuser.lua
+++ Interface/AddOns/NDui/Modules/Misc/Focuser.lua
@@ -4,11 +4,22 @@ local module = NDui:GetModule("Misc")
 function module:Focuser()
 	if not NDuiDB["Misc"]["Focuser"] then return end
 
+	local oUF = NDui.oUF or oUF
 	local modifier = "shift" -- shift, alt or ctrl
 	local mouseButton = "1" -- 1 = left, 2 = right, 3 = middle, 4 and 5 = thumb buttons if there are any
+	local pending = {}
 
 	local function SetFocusHotkey(frame)
-		frame:SetAttribute(modifier.."-type"..mouseButton, "focus")
+		if not frame or frame.focuser then return end
+		if frame:GetName() and string.match(frame:GetName(), "oUF_NPs") then return end
+
+		if not InCombatLockdown() then
+			frame:SetAttribute(modifier.."-type"..mouseButton, "focus")
+			frame.focuser = true
+			pending[frame] = nil
+		else
+			pending[frame] = true
+		end
 	end
 
 	local function CreateFrame_Hook(type, name, parent, template)
@@ -24,28 +35,16 @@ function module:Focuser()
 	f:SetAttribute("macrotext", "/focus mouseover")
 	SetOverrideBindingClick(FocuserButton, true, modifier.."-BUTTON"..mouseButton, "FocuserButton")
 
-	-- Set the keybindings on the default unit frames since we won't get any CreateFrame notification about them
-	local duf = {
-		PlayerFrame,
-		PetFrame,
-		PartyMemberFrame1,
-		PartyMemberFrame2,
-		PartyMemberFrame3,
-		PartyMemberFrame4,
-		PartyMemberFrame1PetFrame,
-		PartyMemberFrame2PetFrame,
-		PartyMemberFrame3PetFrame,
-		PartyMemberFrame4PetFrame,
-		TargetFrame,
-		TargetofTargetFrame,
-	}
-	for i, frame in pairs(duf) do
-		SetFocusHotkey(frame)
-	end
-
-	-- For UFs
-	local oUF = NDui.oUF or oUF
-	for _, object in pairs(oUF.objects) do
-		SetFocusHotkey(object)
-	end
+	local delay = NDui:EventFrame({"PLAYER_REGEN_ENABLED", "GROUP_ROSTER_UPDATE", "PLAYER_ENTERING_WORLD"})
+	delay:SetScript("OnEvent", function()
+		if event == "PLAYER_REGEN_ENABLED" then
+			for frame in pairs(pending) do
+				SetFocusHotkey(frame)
+			end
+		else
+			for _, object in pairs(oUF.objects) do
+				SetFocusHotkey(object)
+			end
+		end
+	end)
 end
