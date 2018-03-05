ce3dd320d811b6877d521c9ff74d4e3540028f2e
siweia <siwei1990@outlook.com>
2018-03-05 13:20:57 +0800

    Toybox background fix

--- Interface/AddOns/Aurora/AddOns/Blizzard_Collections.lua
+++ Interface/AddOns/Aurora/AddOns/Blizzard_Collections.lua
@@ -409,7 +409,9 @@ C.themes["Blizzard_Collections"] = function()
 		bu.slotFrameUncollected:SetTexture("")
 
 		ic:SetTexCoord(.08, .92, .08, .92)
-		F.CreateBG(ic)
+		local bg = F.CreateBG(bu)
+		bg:SetPoint("TOPLEFT", 2.8, -1.8)
+		bg:SetPoint("BOTTOMRIGHT", -2.8, 3.8)
 
 		hooksecurefunc(bu.name, "SetTextColor", changeTextColor)
 	end
