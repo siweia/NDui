4db6843411800635adf9f64a533ac35b24ae5efa
siweia <siwei1990@outlook.com>
2018-05-29 16:25:18 +0800

    Reskin the new channels frame

---
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_Channels.lua
@@ -0,0 +1,43 @@
+local F, C = unpack(select(2, ...))
+
+tinsert(C.themes["AuroraClassic"], function()
+	local r, g, b = C.r, C.g, C.b
+
+	F.StripTextures(ChannelFrame)
+	F.SetBD(ChannelFrame)
+	F.ReskinClose(ChannelFrameCloseButton)
+	F.Reskin(ChannelFrame.NewButton)
+	F.Reskin(ChannelFrame.SettingsButton)
+	F.ReskinScroll(ChannelFrame.ChannelRoster.ScrollFrame.scrollBar)
+
+	ChannelFrameInset:Hide()
+	ChannelFrame.LeftInset:Hide()
+	ChannelFrame.RightInset:Hide()
+
+	hooksecurefunc(ChannelFrame.ChannelList, "Update", function(self)
+		for i = 1, self.Child:GetNumChildren() do
+			local tab = select(i, self.Child:GetChildren())
+			if not tab.styled and tab:IsHeader() then
+				tab:SetNormalTexture("")
+				tab.bg = F.CreateBDFrame(tab, .25)
+				tab.bg:SetAllPoints()
+
+				tab.styled = true
+			end
+		end
+	end)
+
+	F.StripTextures(CreateChannelPopup)
+	F.SetBD(CreateChannelPopup)
+	F.Reskin(CreateChannelPopup.OKButton)
+	F.Reskin(CreateChannelPopup.CancelButton)
+	F.ReskinClose(CreateChannelPopup.CloseButton)
+	F.ReskinInput(CreateChannelPopup.Name)
+	F.ReskinInput(CreateChannelPopup.Password)
+
+	F.CreateBD(VoiceChatPromptActivateChannel)
+	F.CreateSD(VoiceChatPromptActivateChannel)
+	F.Reskin(VoiceChatPromptActivateChannel.AcceptButton)
+	F.CreateBD(VoiceChatChannelActivatedNotification)
+	F.CreateSD(VoiceChatChannelActivatedNotification)
+end)
