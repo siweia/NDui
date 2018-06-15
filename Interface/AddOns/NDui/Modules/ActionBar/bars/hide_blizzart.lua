167a5864bf3f0bbe858dfae5fb05c31426289d9e
siweia <siwei1990@outlook.com>
2018-06-15 23:52:26 +0800

    Update actionbar hider

--- Interface/AddOns/NDui/Modules/ActionBar/bars/hide_blizzart.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/bars/hide_blizzart.lua
@@ -2,9 +2,6 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 local Bar = B:GetModule("Actionbar")
 
-local hiddenFrame = CreateFrame("Frame")
-hiddenFrame:Hide()
-
 local scripts = {
 	"OnShow", "OnHide", "OnEvent", "OnEnter", "OnLeave", "OnUpdate", "OnValueChanged", "OnClick", "OnMouseDown", "OnMouseUp",
 }
@@ -24,14 +21,14 @@ local framesToDisable = {
 local function DisableAllScripts(frame)
 	for _, script in next, scripts do
 		if frame:HasScript(script) then
-			frame:SetScript(script,nil)
+			frame:SetScript(script, nil)
 		end
 	end
 end
 
 function Bar:HideBlizz()
 	for _, frame in next, framesToHide do
-		frame:SetParent(hiddenFrame)
+		frame:SetParent(B.HiddenFrame)
 	end
 
 	for _, frame in next, framesToDisable do
