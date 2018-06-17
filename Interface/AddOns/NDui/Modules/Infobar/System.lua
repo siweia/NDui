e1bbc44834485b7766cf5813e4d53b746e83ca1a
siweia <siwei1990@outlook.com>
2018-06-18 01:22:02 +0800

    One info a time on infobar system module

--- Interface/AddOns/NDui/Modules/Infobar/System.lua
+++ Interface/AddOns/NDui/Modules/Infobar/System.lua
@@ -6,30 +6,36 @@ local module = B:GetModule("Infobar")
 local info = module:RegisterInfobar(C.Infobar.SystemPos)
 
 local function colorLatency(latency)
-	if latency < 300 then
+	if latency < 250 then
 		return "|cff0CD809"..latency
-	elseif latency >= 300 and latency < 500 then
+	elseif latency < 500 then
 		return "|cffE8DA0F"..latency
 	else
 		return "|cffD80909"..latency
 	end
 end
 
+local function colorFPS(fps)
+	if fps < 15 then
+		return "|cffD80909"..fps
+	elseif fps < 30 then
+		return "|cffE8DA0F"..fps
+	else
+		return "|cff0CD809"..fps
+	end
+end
+
 info.onUpdate = function(self, elapsed)
 	self.timer = (self.timer or 0) + elapsed
 	if self.timer > 1 then
-		local _, _, latencyHome, latencyWorld = GetNetStats()
-		local latency = math.max(latencyHome, latencyWorld)
-		local fps = floor(GetFramerate())
-		local color
-		if fps >= 30 then
-			color = "|cff0CD809"
-		elseif fps > 15 and fps < 30 then
-			color = "|cffE8DA0F"
+		if NDuiADB["ShowFPS"] then
+			local fps = floor(GetFramerate())
+			self.text:SetText(colorFPS(fps).."|rFPS ")
 		else
-			color = "|cffD80909"
+			local _, _, latencyHome, latencyWorld = GetNetStats()
+			local latency = math.max(latencyHome, latencyWorld)
+			self.text:SetText(colorLatency(latency).."|rMS")
 		end
-		self.text:SetText(color..fps.."|rFPS "..colorLatency(latency).."|rMS")
 
 		self.timer = 0
 	end
@@ -100,9 +106,12 @@ info.onEnter = function(self)
 	end
 
 	local _, _, latencyHome, latencyWorld = GetNetStats()
+	local fps = floor(GetFramerate())
 	GameTooltip:AddDoubleLine(L["Home Latency"]..":", colorLatency(latencyHome).."|r MS", .6,.8,1, 1,1,1)
 	GameTooltip:AddDoubleLine(L["World Latency"]..":", colorLatency(latencyWorld).."|r MS", .6,.8,1, 1,1,1)
+	GameTooltip:AddDoubleLine(FRAMERATE_LABEL, colorFPS(fps).."|r FPS", .6,.8,1, 1,1,1)
 	GameTooltip:AddDoubleLine(" ", DB.LineString)
+	GameTooltip:AddDoubleLine(" ", DB.LeftButton..(NDuiADB["ShowFPS"] and L["Show Latency"] or L["Show FPS"]).." ", 1,1,1, .6,.8,1)
 	GameTooltip:AddDoubleLine(" ", DB.RightButton..L["CPU Usage"]..": "..(GetCVarBool("scriptProfile") and "|cff55ff55"..VIDEO_OPTIONS_ENABLED or "|cffff5555"..VIDEO_OPTIONS_DISABLED).." ", 1,1,1, .6,.8,1)
 	GameTooltip:Show()
 
@@ -124,18 +133,20 @@ StaticPopupDialogs["CPUUSAGE"] = {
 
 local status = GetCVarBool("scriptProfile")
 info.onMouseUp = function(self, btn)
-	if btn ~= "RightButton" then return end
+	if btn == "LeftButton" then
+		NDuiADB["ShowFPS"] = not NDuiADB["ShowFPS"]
+	elseif btn == "RightButton" then
+		if GetCVarBool("scriptProfile") then
+			SetCVar("scriptProfile", 0)
+		else
+			SetCVar("scriptProfile", 1)
+		end
 
-	if GetCVarBool("scriptProfile") then
-		SetCVar("scriptProfile", 0)
-	else
-		SetCVar("scriptProfile", 1)
-	end
-
-	if GetCVarBool("scriptProfile") == status then
-		StaticPopup_Hide("CPUUSAGE")
-	else
-		StaticPopup_Show("CPUUSAGE")
+		if GetCVarBool("scriptProfile") == status then
+			StaticPopup_Hide("CPUUSAGE")
+		else
+			StaticPopup_Show("CPUUSAGE")
+		end
 	end
 	self:GetScript("OnEnter")(self)
 end
