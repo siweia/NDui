54c84b29ab1b746e21544e9d4df9940d860d48b9
siweia <siwei1990@outlook.com>
2018-06-22 16:15:50 +0800

    Fix WarboardUI skin

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_WarboardUI.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_WarboardUI.lua
@@ -1,18 +1,27 @@
 local F, C = unpack(select(2, ...))
 
 C.themes["Blizzard_WarboardUI"] = function()
-	for i = 1, 13 do
-		select(i, WarboardQuestChoiceFrame:GetRegions()):Hide()
-	end
-	select(5, WarboardQuestChoiceFrame:GetRegions()):Show()
-	WarboardQuestChoiceFrame.GarrCorners:Hide()
-	WarboardQuestChoiceFrame.Background:Hide()
-	F.SetBD(WarboardQuestChoiceFrame)
 	F.ReskinClose(WarboardQuestChoiceFrame.CloseButton)
 
-	for _, option in pairs(WarboardQuestChoiceFrame.Options) do
-		F.Reskin(option.OptionButton)
-		option.Header.Text:SetTextColor(1, .8, 0)
-		option.OptionText:SetTextColor(1, 1, 1)
-	end
+	hooksecurefunc(WarboardQuestChoiceFrame, "Update", function(self)
+		for i = 1, self:GetNumOptions() do
+			local option = self.Options[i]
+			if not option.styled then
+				F.Reskin(option.OptionButtonsContainer.OptionButton1)
+				for i = 1, option.WidgetContainer:GetNumChildren() do
+					local child = select(i, option.WidgetContainer:GetChildren())
+					if child.Text then
+						child.Text:SetTextColor(1, 1, 1)
+						child.Text.SetTextColor = F.dummy
+					end
+					option.Header.Text:SetTextColor(1, 1, 1)
+					option.Header.SetTextColor = F.dummy
+					option.OptionText:SetTextColor(1, .8, 0)
+					option.OptionText.SetTextColor = F.dummy
+				end
+
+				option.styled = true
+			end
+		end
+	end)
 end
