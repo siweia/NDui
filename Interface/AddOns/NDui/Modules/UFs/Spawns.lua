40d645e887bf59add74d5412fc48501ffbcf3357
siweia <siwei1990@outlook.com>
2018-06-30 02:10:01 +0800

    Override oUF core, in case of UF disabled

--- Interface/AddOns/NDui/Modules/UFs/Spawns.lua
+++ Interface/AddOns/NDui/Modules/UFs/Spawns.lua
@@ -177,8 +177,8 @@ function UF:OnLogin()
 
 		if NDuiDB["Nameplate"]["ShowPlayerPlate"] then
 			oUF:SetActiveStyle("PlayerPlate")
-			local plate = oUF:Spawn("player", "oUF_PlayerPlate")
-			B.Mover(plate, L["PlayerNP"], "PlayerPlate", C.UFs.PlayerPlate, 180, 20)
+			local plate = oUF:Spawn("player", "oUF_PlayerPlate", true)
+			B.Mover(plate, L["PlayerNP"], "PlayerPlate", C.UFs.PlayerPlate, plate:GetWidth(), 20)
 		end
 	end
 
