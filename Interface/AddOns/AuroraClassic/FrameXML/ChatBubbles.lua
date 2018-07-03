df576e0bf7a2d13fb9169d1f187e6b45d0163d9f
siweia <siwei1990@outlook.com>
2018-07-03 16:23:10 +0800

    Fix chatbubble margin

--- Interface/AddOns/AuroraClassic/FrameXML/ChatBubbles.lua
+++ Interface/AddOns/AuroraClassic/FrameXML/ChatBubbles.lua
@@ -19,53 +19,51 @@ tinsert(C.themes["AuroraClassic"], function()
 		frame:SetScale(UIParent:GetScale())
 	end
 
-    local function findChatBubble(msg)
-        local chatbubbles = C_ChatBubbles.GetAllChatBubbles()
-        for index = 1, #chatbubbles do
-            local chatbubble = chatbubbles[index]
-            for i = 1, chatbubble:GetNumRegions() do
-                local region = select(i, chatbubble:GetRegions())
-                if region:GetObjectType() == "FontString" and region:GetText() == msg then
-                    return chatbubble
-                end
-            end
-        end
-    end
+	local function findChatBubble(msg)
+		local chatbubbles = C_ChatBubbles.GetAllChatBubbles()
+		for index = 1, #chatbubbles do
+			local chatbubble = chatbubbles[index]
+			for i = 1, chatbubble:GetNumRegions() do
+				local region = select(i, chatbubble:GetRegions())
+				if region:GetObjectType() == "FontString" and region:GetText() == msg then
+					return chatbubble
+				end
+			end
+		end
+	end
 
-    local events = {
-        CHAT_MSG_SAY = "chatBubbles",
-        CHAT_MSG_YELL = "chatBubbles",
-        CHAT_MSG_MONSTER_SAY = "chatBubbles",
-        CHAT_MSG_MONSTER_YELL = "chatBubbles",
-        CHAT_MSG_PARTY = "chatBubblesParty",
-        CHAT_MSG_PARTY_LEADER = "chatBubblesParty",
-        CHAT_MSG_MONSTER_PARTY = "chatBubblesParty",
-    }
+	local events = {
+		CHAT_MSG_SAY = "chatBubbles",
+		CHAT_MSG_YELL = "chatBubbles",
+		CHAT_MSG_MONSTER_SAY = "chatBubbles",
+		CHAT_MSG_MONSTER_YELL = "chatBubbles",
+		CHAT_MSG_PARTY = "chatBubblesParty",
+		CHAT_MSG_PARTY_LEADER = "chatBubblesParty",
+		CHAT_MSG_MONSTER_PARTY = "chatBubblesParty",
+	}
 
 	local bubbleHook = CreateFrame("Frame")
 	for event in next, events do
 		bubbleHook:RegisterEvent(event)
 	end
-    bubbleHook:SetScript("OnEvent", function(self, event, msg)
-        if GetCVarBool(events[event]) then
-            self.elapsed = 0
-            self.msg = msg
-            self:Show()
-        end
-    end)
+	bubbleHook:SetScript("OnEvent", function(self, event, msg)
+		if GetCVarBool(events[event]) then
+			self.elapsed = 0
+			self.msg = msg
+			self:Show()
+		end
+	end)
 
 	bubbleHook:SetScript("OnUpdate", function(self, elapsed)
-        self.elapsed = self.elapsed + elapsed
-        local chatbubble = findChatBubble(self.msg)
-        if chatbubble or self.elapsed > .3 then
-            self:Hide()
-            if chatbubble then
-                if not chatbubble.styled then
-                    styleBubble(chatbubble)
-					chatbubble.styled = true
-                end
-            end
-        end
+		self.elapsed = self.elapsed + elapsed
+		local chatbubble = findChatBubble(self.msg)
+		if chatbubble or self.elapsed > .3 then
+			self:Hide()
+			if chatbubble and not chatbubble.styled then
+				styleBubble(chatbubble)
+				chatbubble.styled = true
+			end
+		end
 	end)
-    bubbleHook:Hide()
+	bubbleHook:Hide()
 end)
