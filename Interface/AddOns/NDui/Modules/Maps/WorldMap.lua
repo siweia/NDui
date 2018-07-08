9312cfb5513502913e366c83d3de6456312cfde6
siweia <siwei1990@outlook.com>
2018-07-08 13:56:27 +0800

    Update worldmap coords

--- Interface/AddOns/NDui/Modules/Maps/WorldMap.lua
+++ Interface/AddOns/NDui/Modules/Maps/WorldMap.lua
@@ -2,36 +2,7 @@
 local B, C, L, DB = unpack(ns)
 local module = B:RegisterModule("Maps")
 
-local mapRects = {}
-local tempVec2D = CreateVector2D(0, 0)
-
-function module:GetPlayerMapPos(mapID)
-	tempVec2D.x, tempVec2D.y = UnitPosition("player")
-	if not tempVec2D.x then return end
-
-	local mapRect = mapRects[mapID]
-	if not mapRect then
-		mapRect = {}
-		mapRect[1] = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0)))
-		mapRect[2] = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1)))
-		mapRect[2]:Subtract(mapRect[1])
-
-		mapRects[mapID] = mapRect
-	end
-	tempVec2D:Subtract(mapRect[1])
-
-	return tempVec2D.y/mapRect[2].y, tempVec2D.x/mapRect[2].x
-end
-
 function module:OnLogin()
-	local WorldMapFrame = _G.WorldMapFrame
-	local BorderFrame = WorldMapFrame.BorderFrame
-	local mapBody = WorldMapFrame:GetCanvasContainer()
-	local formatText = DB.MyColor..": %.1f, %.1f"
-	local scale = mapBody:GetEffectiveScale()
-	local width, height, mapID = mapBody:GetWidth(), mapBody:GetHeight()
-	BorderFrame.Tutorial:SetPoint("TOPLEFT", WorldMapFrame, "TOPLEFT", -12, -12)
-
 	-- Scaling
 	if not WorldMapFrame.isMaximized then WorldMapFrame:SetScale(NDuiDB["Map"]["MapScale"]) end
 	hooksecurefunc(WorldMapFrame, "Minimize", function(self)
@@ -45,18 +16,15 @@ function module:OnLogin()
 
 	-- Generate Coords
 	if not NDuiDB["Map"]["Coord"] then return end
-	local player = B.CreateFS(BorderFrame, 14, "", false, "TOPLEFT", 60, -6)
-	local cursor = B.CreateFS(BorderFrame, 14, "", false, "TOPLEFT", 180, -6)
 
+	local player = B.CreateFS(WorldMapFrame.BorderFrame, 14, "", false, "TOPLEFT", 60, -6)
+	local cursor = B.CreateFS(WorldMapFrame.BorderFrame, 14, "", false, "TOPLEFT", 180, -6)
+	WorldMapFrame.BorderFrame.Tutorial:SetPoint("TOPLEFT", WorldMapFrame, "TOPLEFT", -12, -12)
+
+	local mapBody = WorldMapFrame:GetCanvasContainer()
+	local scale, width, height = mapBody:GetEffectiveScale(), mapBody:GetWidth(), mapBody:GetHeight()
 	hooksecurefunc(WorldMapFrame, "OnFrameSizeChanged", function()
 		width, height = mapBody:GetWidth(), mapBody:GetHeight()
-	end)
-	hooksecurefunc(WorldMapFrame, "OnMapChanged", function(self)
-		if self:GetMapID() == C_Map.GetBestMapForUnit("player") then
-			mapID = self:GetMapID()
-		else
-			mapID = nil
-		end
 	end)
 
 	local function CursorCoords()
@@ -68,30 +36,28 @@ function module:OnLogin()
 		return cx, cy
 	end
 
-	local function CoordsUpdate(player, cursor)
-		local cx, cy = CursorCoords()
-		if cx and cy then
-			cursor:SetFormattedText(L["Mouse"]..formatText, 100 * cx, 100 * cy)
-		else
-			cursor:SetText(L["Mouse"]..DB.MyColor..": --, --")
-		end
-
-		if not mapID then
-			player:SetText(PLAYER..DB.MyColor..": --, --")
-		else
-			local x, y = self:GetPlayerMapPos(mapID)
-			if not x or (x == 0 and y == 0) then
-				player:SetText(PLAYER..DB.MyColor..": --, --")
-			else
-				player:SetFormattedText(PLAYER..formatText, 100 * x, 100 * y)
-			end
-		end
+	local function CoordsFormat(owner, none)
+		local text = none and ": --, --" or ": %.1f, %.1f"
+		return owner..DB.MyColor..text
 	end
 
 	local function UpdateCoords(self, elapsed)
 		self.elapsed = (self.elapsed or 0) + elapsed
 		if self.elapsed > .1 then
-			CoordsUpdate(player, cursor)
+			local cx, cy = CursorCoords()
+			if cx and cy then
+				cursor:SetFormattedText(CoordsFormat(L["Mouse"]), 100 * cx, 100 * cy)
+			else
+				cursor:SetText(CoordsFormat(L["Mouse"], true))
+			end
+
+			RunScript("NDuiMapCoords = C_Map.GetPlayerMapPosition(0, 'player')")
+			if NDuiMapCoords then
+				player:SetFormattedText(CoordsFormat(PLAYER), 100 * NDuiMapCoords.x, 100 * NDuiMapCoords.y)
+			else
+				player:SetText(CoordsFormat(PLAYER, true))
+			end
+
 			self.elapsed = 0
 		end
 	end
