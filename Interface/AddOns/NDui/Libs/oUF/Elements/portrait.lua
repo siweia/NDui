38fced0c67eb5dc34217ef919c8d3cb143bef51c
siweia <siwei1990@outlook.com>
2018-07-11 11:19:58 +0800

    Update oUF libs

--- Interface/AddOns/NDui/Libs/oUF/Elements/portrait.lua
+++ Interface/AddOns/NDui/Libs/oUF/Elements/portrait.lua
@@ -107,6 +107,7 @@ local function Enable(self, unit)
 
 		self:RegisterEvent('UNIT_MODEL_CHANGED', Path)
 		self:RegisterEvent('UNIT_PORTRAIT_UPDATE', Path)
+		self:RegisterEvent('PORTRAITS_UPDATED', Path)
 		self:RegisterEvent('UNIT_CONNECTION', Path)
 
 		-- The quest log uses PARTY_MEMBER_{ENABLE,DISABLE} to handle updating of
@@ -132,6 +133,7 @@ local function Disable(self)
 
 		self:UnregisterEvent('UNIT_MODEL_CHANGED', Path)
 		self:UnregisterEvent('UNIT_PORTRAIT_UPDATE', Path)
+		self:UnregisterEvent('PORTRAITS_UPDATED', Path)
 		self:UnregisterEvent('PARTY_MEMBER_ENABLE', Path)
 		self:UnregisterEvent('UNIT_CONNECTION', Path)
 	end
