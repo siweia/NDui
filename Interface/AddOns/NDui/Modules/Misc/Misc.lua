a71621a19de05a725e2139692cce232b38bad6be
siweia <siwei1990@outlook.com>
2018-07-20 16:51:32 +0800

    Blizz fixed itself, need reviewed

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -415,26 +415,6 @@ do
 	InterfaceOptionsFrameCancel:SetScript("OnClick", function()
 		InterfaceOptionsFrameOkay:Click()
 	end)
-
-	-- https://www.townlong-yak.com/bugs/afKy4k-HonorFrameLoadTaint
-	if (UIDROPDOWNMENU_VALUE_PATCH_VERSION or 0) < 2 then
-		UIDROPDOWNMENU_VALUE_PATCH_VERSION = 2
-		hooksecurefunc("UIDropDownMenu_InitializeHelper", function()
-			if UIDROPDOWNMENU_VALUE_PATCH_VERSION ~= 2 then return end
-
-			for i = 1, UIDROPDOWNMENU_MAXLEVELS do
-				for j = 1, UIDROPDOWNMENU_MAXBUTTONS do
-					local b = _G["DropDownList"..i.."Button"..j]
-					if not (issecurevariable(b, "value") or b:IsShown()) then
-						b.value = nil
-						repeat
-							j, b["fx" .. j] = j+1
-						until issecurevariable(b, "value")
-					end
-				end
-			end
-		end)
-	end
 end
 
 -- Roll Gold
