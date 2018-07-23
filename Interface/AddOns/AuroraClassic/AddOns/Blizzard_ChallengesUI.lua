947e1455ac5a9d36a30f807d57cf239f38b6381a
siweia <siwei1990@outlook.com>
2018-07-24 00:46:22 +0800

    Reskin ChallengesFrame affixes

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_ChallengesUI.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_ChallengesUI.lua
@@ -1,51 +1,6 @@
 local F, C = unpack(select(2, ...))
 
 C.themes["Blizzard_ChallengesUI"] = function()
-	local ChallengesFrame = ChallengesFrame
-
-	ChallengesFrameInset:DisableDrawLayer("BORDER")
-	ChallengesFrameInsetBg:Hide()
-	for i = 1, 2 do
-		select(i, ChallengesFrame:GetRegions()):Hide()
-	end
-
-	local angryStyle
-	local function UpdateIcons(self)
-		for i = 1, #self.maps do
-			local bu = self.DungeonIcons[i]
-			if bu and not bu.styled then
-				bu:GetRegions():SetAlpha(0)
-				F.ReskinIcon(bu.Icon)
-				bu.styled = true
-			end
-		end
-
-		if IsAddOnLoaded("AngryKeystones") and not angryStyle then
-			local scheduel = select(6, self:GetChildren())
-			scheduel:GetRegions():SetAlpha(0)
-			select(3, scheduel:GetRegions()):SetAlpha(0)
-			F.CreateBD(scheduel, .3)
-
-			if scheduel.Entries then
-				for i = 1, 4 do
-					AffixesSetup(scheduel.Entries[i].Affixes)
-				end
-			end
-			angryStyle = true
-		end
-	end
-	hooksecurefunc("ChallengesFrame_Update", UpdateIcons)
-
-	local keystone = ChallengesKeystoneFrame
-	F.SetBD(keystone)
-	F.ReskinClose(keystone.CloseButton)
-	F.Reskin(keystone.StartButton)
-
-	hooksecurefunc(keystone, "Reset", function(self)
-		self:GetRegions():SetAlpha(0)
-		self.InstructionBackground:SetAlpha(0)
-	end)
-
 	local function AffixesSetup(self)
 		for _, frame in ipairs(self.Affixes) do
 			frame.Border:SetTexture(nil)
@@ -62,5 +17,58 @@ C.themes["Blizzard_ChallengesUI"] = function()
 			end
 		end
 	end
+
+	ChallengesFrameInset:DisableDrawLayer("BORDER")
+	ChallengesFrameInsetBg:Hide()
+	for i = 1, 2 do
+		select(i, ChallengesFrame:GetRegions()):Hide()
+	end
+
+	local angryStyle
+	local function UpdateIcons(self)
+		for i = 1, #self.maps do
+			local bu = self.DungeonIcons[i]
+			if bu and not bu.styled then
+				bu:GetRegions():SetAlpha(0)
+				bu.Icon:SetTexCoord(.08, .92, .08, .92)
+				F.CreateBDFrame(bu.Icon)
+
+				bu.styled = true
+			end
+		end
+
+		if IsAddOnLoaded("AngryKeystones") and not angryStyle then
+			local scheduel = select(6, self:GetChildren())
+			scheduel:GetRegions():SetAlpha(0)
+			select(3, scheduel:GetRegions()):SetAlpha(0)
+			F.CreateBD(scheduel, .3)
+			if scheduel.Entries then
+				for i = 1, 4 do
+					AffixesSetup(scheduel.Entries[i].Affixes)
+				end
+			end
+
+			angryStyle = true
+		end
+	end
+	hooksecurefunc("ChallengesFrame_Update", UpdateIcons)
+
+	hooksecurefunc(ChallengesFrame.WeeklyInfo, "SetUp", function(self)
+		local affixes = C_MythicPlus.GetCurrentAffixes()
+		if affixes then
+			AffixesSetup(self.Child)
+		end
+	end)
+
+	local keystone = ChallengesKeystoneFrame
+	F.SetBD(keystone)
+	F.ReskinClose(keystone.CloseButton)
+	F.Reskin(keystone.StartButton)
+
+	hooksecurefunc(keystone, "Reset", function(self)
+		self:GetRegions():SetAlpha(0)
+		self.InstructionBackground:SetAlpha(0)
+	end)
+
 	hooksecurefunc(keystone, "OnKeystoneSlotted", AffixesSetup)
 end
