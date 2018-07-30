6ac6c43f2a3912e56c1d72d19df46bf58b783e97
siweia <siwei1990@outlook.com>
2018-07-30 17:11:21 +0800

    Shadow the StopwatchFrame

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_TimeManager.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_TimeManager.lua
@@ -2,12 +2,6 @@ local F, C = unpack(select(2, ...))
 
 C.themes["Blizzard_TimeManager"] = function()
 	TimeManagerGlobe:Hide()
-	StopwatchFrameBackgroundLeft:Hide()
-	select(2, StopwatchFrame:GetRegions()):Hide()
-	StopwatchTabFrameLeft:Hide()
-	StopwatchTabFrameMiddle:Hide()
-	StopwatchTabFrameRight:Hide()
-
 	TimeManagerStopwatchCheck:GetNormalTexture():SetTexCoord(.08, .92, .08, .92)
 	TimeManagerStopwatchCheck:SetCheckedTexture(C.media.checked)
 	F.CreateBG(TimeManagerStopwatchCheck)
@@ -18,7 +12,9 @@ C.themes["Blizzard_TimeManager"] = function()
 
 	F.ReskinPortraitFrame(TimeManagerFrame, true)
 
-	F.CreateBD(StopwatchFrame)
+	F.StripTextures(StopwatchFrame)
+	F.StripTextures(StopwatchTabFrame)
+	F.SetBD(StopwatchFrame)
 	F.ReskinDropDown(TimeManagerAlarmHourDropDown)
 	F.ReskinDropDown(TimeManagerAlarmMinuteDropDown)
 	F.ReskinDropDown(TimeManagerAlarmAMPMDropDown)
