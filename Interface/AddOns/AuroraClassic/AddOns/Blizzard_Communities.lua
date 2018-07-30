b9e84a1ab6135367961efaba30e1dcaf7e55b259
siweia <siwei1990@outlook.com>
2018-07-30 17:08:03 +0800

    Reskin TicketFrame on communities

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_Communities.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_Communities.lua
@@ -14,15 +14,16 @@ C.themes["Blizzard_Communities"] = function()
 	CommunitiesFrameInset:Hide()
 	F.ReskinArrow(CommunitiesFrame.AddToChatButton, "down")
 	F.ReskinDropDown(CommunitiesFrame.CommunitiesListDropDownMenu)
-	CommunitiesFrame.GuildFinderFrame:GetRegions():Hide()
-	CommunitiesFrame.GuildFinderFrame.InsetFrame:Hide()
-	F.Reskin(CommunitiesFrame.GuildFinderFrame.FindAGuildButton)
 
-	F.StripTextures(CommunitiesFrame.InvitationFrame)
-	F.CreateBD(CommunitiesFrame.InvitationFrame, .25)
-	CommunitiesFrame.InvitationFrame.InsetFrame:Hide()
-	F.Reskin(CommunitiesFrame.InvitationFrame.AcceptButton)
-	F.Reskin(CommunitiesFrame.InvitationFrame.DeclineButton)
+	for _, name in next, {"GuildFinderFrame", "InvitationFrame", "TicketFrame"} do
+		local frame = CommunitiesFrame[name]
+		F.StripTextures(frame)
+		F.CreateBD(frame, .25)
+		frame.InsetFrame:Hide()
+		if frame.FindAGuildButton then F.Reskin(frame.FindAGuildButton) end
+		if frame.AcceptButton then F.Reskin(frame.AcceptButton) end
+		if frame.DeclineButton then F.Reskin(frame.DeclineButton) end
+	end
 
 	F.StripTextures(CommunitiesFrameCommunitiesList)
 	CommunitiesFrameCommunitiesList.InsetFrame:Hide()
@@ -288,12 +289,8 @@ C.themes["Blizzard_Communities"] = function()
 
 	-- Benefits
 	CommunitiesFrame.GuildBenefitsFrame.Perks:GetRegions():SetAlpha(0)
-	CommunitiesFrame.GuildBenefitsFrame.InsetBorderLeft:SetAlpha(0)
-	CommunitiesFrame.GuildBenefitsFrame.InsetBorderLeft2:SetAlpha(0)
-	CommunitiesFrame.GuildBenefitsFrame.InsetBorderTopLeft2:SetAlpha(0)
-	CommunitiesFrame.GuildBenefitsFrame.InsetBorderBottomLeft2:SetAlpha(0)
-	CommunitiesFrame.GuildBenefitsFrame.InsetBorderRight:SetAlpha(0)
 	CommunitiesFrame.GuildBenefitsFrame.Rewards.Bg:SetAlpha(0)
+	F.StripTextures(CommunitiesFrame.GuildBenefitsFrame)
 	F.ReskinScroll(CommunitiesFrameRewards.scrollBar)
 
 	local factionFrameBar = CommunitiesFrame.GuildBenefitsFrame.FactionFrame.Bar
@@ -360,15 +357,7 @@ C.themes["Blizzard_Communities"] = function()
 	F.ReskinScroll(CommunitiesFrameGuildDetailsFrameInfoScrollBar)
 	F.CreateBDFrame(CommunitiesFrameGuildDetailsFrameInfo.DetailsFrame, .25)
 	F.ReskinScroll(CommunitiesFrameGuildDetailsFrameNewsContainer.ScrollBar)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderLeft:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderLeft2:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderRight:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderTopLeft:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderTopLeft2:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderTopRight:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderBottomLeft:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderBottomLeft2:SetAlpha(0)
-	CommunitiesFrameGuildDetailsFrame.InsetBorderBottomRight:SetAlpha(0)
+	F.StripTextures(CommunitiesFrameGuildDetailsFrame)
 
 	hooksecurefunc("CommunitiesGuildNewsButton_SetNews", function(button)
 		if button.header:IsShown() then
