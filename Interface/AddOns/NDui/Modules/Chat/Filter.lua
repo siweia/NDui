ac7b169ffd3200b080612a5feca48dc9e6329061
siweia <siwei1990@outlook.com>
2018-08-09 03:40:27 +0800

    Update chatfilter block list

--- Interface/AddOns/NDui/Modules/Chat/Filter.lua
+++ Interface/AddOns/NDui/Modules/Chat/Filter.lua
@@ -98,7 +98,7 @@ end
 
 local addonBlockList = {
 	"任务进度提示%s?[:：]", "%[接受任务%]", "%(任务完成%)", "<大脚组队提示>", "<大脚团队提示>", "【网%.易%.有%.爱】", "EUI:", "EUI_RaidCD", "打断:.+|Hspell", "PS 死亡: .+>", "%*%*.+%*%*",
-	"<iLvl>", ("%-"):rep(30), "<小队物品等级:.+>", "<LFG>"
+	"<iLvl>", ("%-"):rep(30), "<小队物品等级:.+>", "<LFG>", "wowcdk"
 }
 
 local function genAddonBlock(_, _, msg, author)
