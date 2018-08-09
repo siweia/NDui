49209c9ff50fb9b3356314b14ed1a7ae35bd0eb0
siweia <siwei1990@outlook.com>
2018-08-09 15:04:49 +0800

    Post-merge clean up

--- Interface/AddOns/NDui/Core/GUI.lua
+++ Interface/AddOns/NDui/Core/GUI.lua
@@ -73,7 +73,6 @@ local defaultSettings = {
 		PetCombatText = true,
 		RaidClickSets = false,
 		ShowTeamIndex = false,
-		ShowPlayerPlate = false,
 		SortRunes = true,
 		AddPower = true,
 	},
@@ -123,6 +122,7 @@ local defaultSettings = {
 		ShowUnitPower = true,
 		ShowPowerList = "",
 		VerticalSpacing = .7,
+		ShowPlayerPlate = false,
 	},
 	Skins = {
 		DBM = true,
@@ -314,7 +314,6 @@ local optionList = {		-- type, key, value, name, horizon, doubleline
 	},
 	[5] = {
 		{1, "Nameplate", "Enable", L["Enable Nameplate"]},
-		{1, "Nameplate", "ShowPlayerPlate", "|cff00cc4c"..L["Enable PlayerPlate"], true},
 		{},--blank
 		{1, "Nameplate", "ColorBorder", L["Auras Border"]},
 		{1, "Nameplate", "AllAuras", L["Show All Auras"], true},
@@ -341,12 +340,13 @@ local optionList = {		-- type, key, value, name, horizon, doubleline
 		{1, "AuraWatch", "Enable", L["Enable AuraWatch"]},
 		{1, "AuraWatch", "ClickThrough", L["AuraWatch ClickThrough"]},
 		{},--blank
-		{1, "Auras", "Reminder", L["Enable Reminder"]},
 		{1, "Auras", "BloodyHell", L["Enable BloodyHell"]},
-		{1, "Auras", "Stagger", L["Enable Stagger"]},
+		{1, "Auras", "Stagger", L["Enable Stagger"], true},
 		{1, "Auras", "Statue", L["Enable Statue"]},
-		{1, "Auras", "Totems", L["Enable Totems"]},
-		{1, "Auras", "DestroyTotems", L["Destroy Totems"], true},
+		{1, "Auras", "Totems", L["Enable Totems"], true},
+		{1, "Auras", "Reminder", L["Enable Reminder"]},
+		{},--blank
+		{1, "Nameplate", "ShowPlayerPlate", "|cff00cc4c"..L["Enable PlayerPlate"]},
 		{1, "Auras", "HunterTool", L["Enable HunterTool"]},
 	},
 	[7] = {
