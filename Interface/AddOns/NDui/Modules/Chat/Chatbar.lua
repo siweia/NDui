4de6d8619a6d3a1eeeca288eab606a5b3d331df3
siweia <siwei1990@outlook.com>
2018-08-11 01:43:18 +0800

    Delay worldchannel check

--- Interface/AddOns/NDui/Modules/Chat/Chatbar.lua
+++ Interface/AddOns/NDui/Modules/Chat/Chatbar.lua
@@ -84,20 +84,21 @@ function module:Chatbar()
 		local wc = AddButton(0, .8, 1, L["World Channel"])
 
 		local function isInChannel(event)
-			channels = {GetChannelList()}
-			for i = 1, #channels do
-				if channels[i] == channelName then
-					wc.inChannel = true
-					channelID = channels[i-1]
-					break
+			C_Timer.After(.1, function()
+				channels = {GetChannelList()}
+				for i = 1, #channels do
+					if channels[i] == channelName then
+						wc.inChannel = true
+						channelID = channels[i-1]
+						break
+					end
 				end
-			end
-
-			if wc.inChannel then
-				wc.Icon:SetVertexColor(0, .8, 1)
-			else
-				wc.Icon:SetVertexColor(1, .1, .1)
-			end
+				if wc.inChannel then
+					wc.Icon:SetVertexColor(0, .8, 1)
+				else
+					wc.Icon:SetVertexColor(1, .1, .1)
+				end
+			end)
 
 			if event == "PLAYER_ENTERING_WORLD" then
 				B:UnregisterEvent(event, isInChannel)
