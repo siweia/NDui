e1bcc4f2162c6970208878e93f9400fe083a7cf1
siweia <siwei1990@outlook.com>
2018-08-16 18:08:54 +0800

    Reskin the islands rewardframe

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_IslandsPartyPoseUI.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_IslandsPartyPoseUI.lua
@@ -7,6 +7,13 @@ C.themes["Blizzard_IslandsPartyPoseUI"] = function()
 	F.StripTextures(IslandsPartyPoseFrame.ModelScene)
 	F.CreateBDFrame(IslandsPartyPoseFrame.ModelScene, .25)
 
-	IslandsPartyPoseFrame.RewardAnimations.RewardFrame.NameFrame:SetAlpha(0)
-	F.CreateBDFrame(IslandsPartyPoseFrame.RewardAnimations.RewardFrame.NameFrame, .25)
+	local rewardFrame = IslandsPartyPoseFrame.RewardAnimations.RewardFrame
+	local bg = F.CreateBDFrame(rewardFrame)
+	bg:SetPoint("TOPLEFT", -5, 5)
+	bg:SetPoint("BOTTOMRIGHT", rewardFrame.NameFrame, 0, -5)
+	F.CreateSD(bg)
+	rewardFrame.NameFrame:SetAlpha(0)
+	rewardFrame.IconBorder:SetAlpha(0)
+	rewardFrame.Icon:SetTexCoord(.08, .92, .08, .92)
+	F.CreateBDFrame(rewardFrame.Icon)
 end
