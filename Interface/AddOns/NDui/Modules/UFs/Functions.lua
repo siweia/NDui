4f5f376f0ff8bd66e5c3e5363fc3d8774da258cc
siweia <siwei1990@outlook.com>
2018-08-23 12:50:50 +0800

    Update playerplate classpower height

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -628,7 +628,7 @@ end
 
 function UF:CreateClassPower(self)
 	if self.mystyle == "PlayerPlate" then
-		width, height = self:GetWidth(), 13
+		width, height = self:GetWidth(), (self:GetHeight()+self.Power:GetHeight())
 		C.UFs.BarPoint = {"BOTTOMLEFT", self, "TOPLEFT", 0, 3}
 	end
 
