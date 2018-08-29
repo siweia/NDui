dd39a310b2f6b69af3773f41e35fcbc4dc65e7c8
siweia <siwei1990@outlook.com>
2018-08-29 15:04:15 +0800

    Update skin for BaudErrorFrame

--- Interface/AddOns/!BaudErrorFrame/Reskin.lua
+++ Interface/AddOns/!BaudErrorFrame/Reskin.lua
@@ -2,25 +2,24 @@ local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function()
 	f:UnregisterEvent("PLAYER_ENTERING_WORLD")
-	if IsAddOnLoaded("Aurora") then
-		local F = unpack(Aurora)
+	if IsAddOnLoaded("AuroraClassic") then
+		local F = unpack(AuroraClassic)
 		F.ReskinScroll(BaudErrorFrameListScrollBoxScrollBarScrollBar)
 		F.ReskinScroll(BaudErrorFrameDetailScrollFrameScrollBar)
 	end
 	if IsAddOnLoaded("NDui") then
 		local B = unpack(NDui)
 		B.CreateBD(BaudErrorFrame)
+		B.CreateSD(BaudErrorFrame)
 		B.CreateTex(BaudErrorFrame)
-		B.CreateSD(BaudErrorFrame, 1, 4)
+		B.StripTextures(BaudErrorFrameDetailScrollBox)
 		local BG2 = CreateFrame("Frame", nil, BaudErrorFrame)
 		BG2:SetPoint("CENTER", BaudErrorFrame, "CENTER", 0, -81)
-		BG2:SetSize(BaudErrorFrameEditBox:GetWidth() + 60, BaudErrorFrameEditBox:GetHeight() + 15)
+		BG2:SetSize(BaudErrorFrameEditBox:GetWidth() + 56, BaudErrorFrameEditBox:GetHeight() + 10)
 		B.CreateBD(BG2)
-		B.CreateBD(BaudErrorFrameClearButton)
-		B.CreateBC(BaudErrorFrameClearButton)
-		B.CreateBD(BaudErrorFrameCloseButton)
-		B.CreateBC(BaudErrorFrameCloseButton)
-		B.CreateBD(BaudErrorFrameReloadUIButton)
-		B.CreateBC(BaudErrorFrameReloadUIButton)
+		for _, button in next, {BaudErrorFrameClearButton, BaudErrorFrameCloseButton, BaudErrorFrameReloadUIButton} do
+			B.CreateBD(button)
+			B.CreateBC(button)
+		end
 	end
 end)
