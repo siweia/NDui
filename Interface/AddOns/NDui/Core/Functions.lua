2c6b822a5290e173bcecf86d222e4013ab96017b
siweia <siwei1990@outlook.com>
2018-08-29 15:05:25 +0800

    Update skin api

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -13,10 +13,9 @@ function B:CreateGF(w, h, o, r, g, b, a1, a2)
 end
 
 -- Create Backdrop
-function B:CreateBD(a, s)
+function B:CreateBD(a)
 	self:SetBackdrop({
-		bgFile = DB.bdTex, edgeFile = DB.glowTex, edgeSize = s or 3,
-		insets = {left = s or 3, right = s or 3, top = s or 3, bottom = s or 3},
+		bgFile = DB.bdTex, edgeFile = DB.bdTex, edgeSize = 1.2,
 	})
 	self:SetBackdropColor(0, 0, 0, a or .5)
 	self:SetBackdropBorderColor(0, 0, 0)
@@ -29,13 +28,14 @@ function B:CreateSD(m, s)
 	local frame = self
 	if self:GetObjectType() == "Texture" then frame = self:GetParent() end
 	local lvl = frame:GetFrameLevel()
+	if not m then m, s = 2, 3 end
 
 	self.Shadow = CreateFrame("Frame", nil, frame)
 	self.Shadow:SetPoint("TOPLEFT", self, -m, m)
 	self.Shadow:SetPoint("BOTTOMRIGHT", self, m, -m)
 	self.Shadow:SetBackdrop({edgeFile = DB.glowTex, edgeSize = s})
 	self.Shadow:SetBackdropBorderColor(0, 0, 0, 1)
-	self.Shadow:SetFrameLevel(lvl == 0 and 1 or lvl - 1)
+	self.Shadow:SetFrameLevel(lvl == 0 and 0 or lvl - 1)
 
 	return self.Shadow
 end
@@ -50,7 +50,7 @@ function B:CreateBG(offset)
 	local bg = CreateFrame("Frame", nil, frame)
 	bg:SetPoint("TOPLEFT", self, -offset, offset)
 	bg:SetPoint("BOTTOMRIGHT", self, offset, -offset)
-	bg:SetFrameLevel(lvl == 0 and 1 or lvl - 1)
+	bg:SetFrameLevel(lvl == 0 and 0 or lvl - 1)
 	return bg
 end
 
@@ -139,12 +139,12 @@ function B:CreateCB(a)
 	self:SetPushedTexture("")
 	self:SetHighlightTexture(DB.bdTex)
 	local hl = self:GetHighlightTexture()
-	hl:SetPoint("TOPLEFT", 6, -6)
-	hl:SetPoint("BOTTOMRIGHT", -6, 6)
+	hl:SetPoint("TOPLEFT", 5, -5)
+	hl:SetPoint("BOTTOMRIGHT", -5, 5)
 	hl:SetVertexColor(cr, cg, cb, .25)
 
 	local bd = B.CreateBG(self, -4)
-	B.CreateBD(bd, a, 2)
+	B.CreateBD(bd, a)
 
 	local ch = self:GetCheckedTexture()
 	ch:SetDesaturated(true)
@@ -449,7 +449,7 @@ function B:CreateDropDown(width, height, data)
 	bu:GetHighlightTexture():SetTexCoord(0, .5, 0, .5)
 	local list = CreateFrame("Frame", nil, dd)
 	list:SetPoint("TOP", dd, "BOTTOM")
-	B.CreateBD(list, .7)
+	B.CreateBD(list, 1)
 	bu:SetScript("OnShow", function() list:Hide() end)
 	bu:SetScript("OnClick", function()
 		PlaySound(SOUNDKIT.GS_TITLE_OPTION_OK)
@@ -474,7 +474,7 @@ function B:CreateDropDown(width, height, data)
 	end
 	local function optOnEnter(self)
 		if self.selected then return end
-		self:SetBackdropColor(1, 1, 1, .3)
+		self:SetBackdropColor(1, 1, 1, .25)
 	end
 	local function optOnLeave(self)
 		if self.selected then return end
@@ -483,9 +483,10 @@ function B:CreateDropDown(width, height, data)
 
 	for i, j in pairs(data) do
 		opt[i] = CreateFrame("Button", nil, list)
-		opt[i]:SetPoint("TOPLEFT", 5, -5 - (i-1)*height)
-		opt[i]:SetSize(width - 10, height)
+		opt[i]:SetPoint("TOPLEFT", 4, -4 - (i-1)*(height+2))
+		opt[i]:SetSize(width - 8, height)
 		B.CreateBD(opt[i], .3)
+		opt[i]:SetBackdropBorderColor(1, 1, 1, .2)
 		B.CreateFS(opt[i], 14, j, false, "LEFT", 5, 0)
 		opt[i].text = j
 		opt[i]:SetScript("OnClick", optOnClick)
@@ -495,7 +496,7 @@ function B:CreateDropDown(width, height, data)
 		dd.options[i] = opt[i]
 		index = index + 1
 	end
-	list:SetSize(width, index*height + 10)
+	list:SetSize(width, index*(height+2) + 6)
 
 	dd.Type = "DropDown"
 	return dd
