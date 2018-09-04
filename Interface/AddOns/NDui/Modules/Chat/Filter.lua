fae00d060eefac659595f78383e33771100466f7
siweia <siwei1990@outlook.com>
2018-09-04 16:56:19 +0800

    Update WQT blocker

--- Interface/AddOns/NDui/Modules/Chat/Filter.lua
+++ Interface/AddOns/NDui/Modules/Chat/Filter.lua
@@ -173,7 +173,7 @@ end
 
 local function blockWhisperString(_, _, msg, author)
 	local name = Ambiguate(author, "none")
-	if msg:find("%[World Quest Tracker%]") or msg:find("一起做世界任务吧：") then
+	if msg:find("%[World Quest Tracker%]") or msg:find("一起做世界任务吧：") or msg:find("一起来做世界任务<") then
 		if not WQTUsers[name] then
 			WQTUsers[name] = true
 		end
