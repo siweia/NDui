a3612372a71c9ac874819fe6d03ba59539a14e0d
siweia <siwei1990@outlook.com>
2018-09-17 02:51:17 +0800

    Update azerite armor info

--- Interface/AddOns/NDui/Modules/Tooltip/AzeriteArmor.lua
+++ Interface/AddOns/NDui/Modules/Tooltip/AzeriteArmor.lua
@@ -6,62 +6,88 @@ local module = B:GetModule("Tooltip")
 function module:AzeriteArmor()
 	if not NDuiDB["Tooltip"]["AzeriteArmor"] then return end
 
+	local tipList = {}
+	local function scanTooltip(tooltip)
+		for i = 9, tooltip:NumLines() do
+			local line = _G[tooltip:GetName().."TextLeft"..i]
+			local text = line:GetText()
+			if text and text:find("%-") then
+				tinsert(tipList, i)
+			end
+		end
+	end
+
 	local iconString = "|T%s:18:22:0:0:64:64:5:59:5:59"
 	local function getIconString(icon, known)
 		if known then
 			return format(iconString..":255:255:255|t", icon)
 		else
-			return format(iconString..":120:120:120|t", icon)
+			return format(iconString..":130:130:130|t", icon)
 		end
 	end
 
-	local function getAzeritePowerInfo(id)
-		local powerInfo = C_AzeriteEmpoweredItem.GetPowerInfo(id)
-		if powerInfo and powerInfo.spellID then
-			local name, _, icon = GetSpellInfo(powerInfo.spellID)
-			return name, icon
+	local powerData = {}
+	local function convertPowerID(id)
+		local spellID = powerData[id]
+		if not spellID then
+			local powerInfo = C_AzeriteEmpoweredItem.GetPowerInfo(id)
+			if powerInfo and powerInfo.spellID then
+				spellID = powerInfo.spellID
+				powerData[id] = spellID
+			end
 		end
+		return spellID
 	end
 
-	local function scanAzeriteTooltip(tooltip, powerName)
+	local function lookingForAzerite(tooltip, powerName)
 		for i = 8, tooltip:NumLines() do
 			local line = _G[tooltip:GetName().."TextLeft"..i]
 			local text = line:GetText()
 			if text and text:find("%- "..powerName) then
-				return i
+				return true
 			end
 		end
 	end
 
+	local cache = {}
 	local function updateAzeriteArmor(self)
 		local link = select(2, self:GetItem())
 		if not link then return end
 		if not C_AzeriteEmpoweredItem.IsAzeriteEmpoweredItemByID(link) then return end
+		local allTierInfo = cache[link]
+		if not allTierInfo then
+			allTierInfo = C_AzeriteEmpoweredItem.GetAllTierInfoByItemID(link)
+			cache[link] = allTierInfo
+		end
 
-		local allTierInfo = C_AzeriteEmpoweredItem.GetAllTierInfoByItemID(link)
-		if not allTierInfo then return end
+		scanTooltip(self)
 
+		local index = 1
 		for i = 1, #allTierInfo do
 			local powerIDs = allTierInfo[i].azeritePowerIDs
 			if powerIDs[1] == 13 then break end
 
-			local tooltipText, lineIndex = ""
-			for index, id in ipairs(powerIDs) do
-				local name, icon = getAzeritePowerInfo(id)
-				local index = scanAzeriteTooltip(self, name)
-				if index then
+			local tooltipText = ""
+			for _, id in ipairs(powerIDs) do
+				local spellID = convertPowerID(id)
+				if not spellID then break end
+				local name, _, icon = GetSpellInfo(spellID)
+				local found = lookingForAzerite(self, name)
+				if found then
 					tooltipText = tooltipText.." "..getIconString(icon, true)
-					lineIndex = index
 				else
 					tooltipText = tooltipText.." "..getIconString(icon)
 				end
 			end
 
-			if lineIndex then
-				local line = _G[self:GetName().."TextLeft"..lineIndex]
-				line:SetText(line:GetText()..tooltipText)
+			if #tipList > 0 then
+				local line = _G[self:GetName().."TextLeft"..tipList[index]]
+				line:SetText(line:GetText().."\n "..tooltipText)
+				index = index + 1
 			end
 		end
+
+		wipe(tipList)
 	end
 
 	GameTooltip:HookScript("OnTooltipSetItem", updateAzeriteArmor)
