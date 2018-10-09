886c60076fa6b5aded1b715161f97782dd8e7962
siweia <siwei1990@outlook.com>
2018-10-09 14:50:13 +0800

    Update brokenspell list

--- Interface/AddOns/NDui/Modules/Misc/Alerts.lua
+++ Interface/AddOns/NDui/Modules/Misc/Alerts.lua
@@ -125,6 +125,8 @@ function module:InterruptAlert()
 		[157997] = true,	-- 寒冰新星
 		[102359] = true,	-- 群体缠绕
 		[226943] = true,	-- 心灵炸弹
+		[105421] = true,	-- 盲目之光
+		[207167] = true,	-- 致盲冰雨
 	}
 
 	local function msgChannel()
