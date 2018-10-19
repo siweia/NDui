e8149699d87901357a33cf13b7f264ee697c638b
siweia <siwei1990@outlook.com>
2018-10-19 11:54:45 +0800

    Update button range, taint control

--- Interface/AddOns/NDui/Modules/ActionBar/ButtonRange.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/ButtonRange.lua
@@ -17,7 +17,7 @@ end
 function Bar:RangeUpdate()
 	if NDuiDB["Actionbar"]["Enable"] then
 		local bar = self:GetParent():GetParent()
-		if bar and bar:GetAlpha() == 0 then return end
+		if bar and bar:GetAlpha() < 1 then return end
 	end
 
 	local icon = self.icon
