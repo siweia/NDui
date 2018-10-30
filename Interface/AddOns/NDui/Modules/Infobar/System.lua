db2f627148500aec384015ec54a46f5b7cada4bf
siweia <siwei1990@outlook.com>
2018-10-30 11:19:28 +0800

    Fix a nil value in some cases

--- Interface/AddOns/NDui/Modules/Infobar/System.lua
+++ Interface/AddOns/NDui/Modules/Infobar/System.lua
@@ -41,7 +41,7 @@ info.onUpdate = function(self, elapsed)
 	end
 end
 
-local usageTable, startTime = {}
+local usageTable, startTime = {}, 0
 local function retrieveUsage()
 	wipe(usageTable)
 	UpdateAddOnCPUUsage()
