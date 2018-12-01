66a272f714cc9b51ccf501c8e2f597200c20dea6
siweia <siwei1990@outlook.com>
2018-12-02 00:32:12 +0800

    Update custom units

--- Interface/AddOns/NDui/Modules/UFs/NPs.lua
+++ Interface/AddOns/NDui/Modules/UFs/NPs.lua
@@ -78,6 +78,7 @@ local CustomUnits = {
 	[GetSectionInfo(18078)] = true,	-- 蛛魔编织者
 	[GetSectionInfo(18007)] = true,	-- 瘟疫聚合体
 	[GetSectionInfo(18053)] = true,	-- 灵魂荆棘
+	[GetSectionInfo(18312)] = true,	-- 血面兽
 	[120651] = true, -- 爆炸物
 	[141851] = true, -- 戈霍恩之嗣
 }
