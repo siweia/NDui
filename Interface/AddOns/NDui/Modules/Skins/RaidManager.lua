5d97398d51fb78a81b0013abc070360b18a77dd2
siweia <siwei1990@outlook.com>
2018-12-08 02:15:09 +0800

    Only count number on raid buffs

--- Interface/AddOns/NDui/Modules/Skins/RaidManager.lua
+++ Interface/AddOns/NDui/Modules/Skins/RaidManager.lua
@@ -5,7 +5,7 @@ local module = B:GetModule("Skins")
 function module:CreateRM()
 	if not NDuiDB["Skins"]["RM"] then return end
 
-	local tinsert, strsplit = table.insert, string.split
+	local tinsert, strsplit, format = table.insert, string.split, string.format
 	local next, pairs, mod = next, pairs, mod
 
 	local header = CreateFrame("Button", nil, UIParent)
@@ -237,12 +237,15 @@ function module:CreateRM()
 	end
 
 	local function sendResult(i)
-		if #NoBuff[i] > 0 then
-			if #NoBuff[i] >= numPlayer then
+		local count = #NoBuff[i]
+		if count > 0 then
+			if count >= numPlayer then
 				sendMsg(L["Lack"]..BuffName[i]..": "..ALL..PLAYER)
+			elseif count >= 5 and i > 2 then
+				sendMsg(L["Lack"]..BuffName[i]..": "..format(L["Player Count"], count))
 			else
 				local str = L["Lack"]..BuffName[i]..": "
-				for j = 1, #NoBuff[i] do
+				for j = 1, count do
 					str = str..NoBuff[i][j]..(j < #NoBuff[i] and ", " or "")
 					if #str > 230 then
 						sendMsg(str)
