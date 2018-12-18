49db2a70a0a593348457d3454997ce492682e711
siweia <siwei1990@outlook.com>
2018-12-19 01:21:11 +0800

    AuroraClassic update to 2.2.2

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_AlliedRacesUI.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_AlliedRacesUI.lua
@@ -1,17 +1,18 @@
 local F, C = unpack(select(2, ...))
 
 C.themes["Blizzard_AlliedRacesUI"] = function()
-	for i = 1, 19 do
-		select(i, AlliedRacesFrame:GetRegions()):Hide()
-	end
-	select(8, AlliedRacesFrame:GetRegions()):Show()
-	F.SetBD(AlliedRacesFrame)
-	F.ReskinClose(AlliedRacesFrameCloseButton)
-	F.ReskinScroll(AlliedRacesFrame.RaceInfoFrame.ScrollFrame.ScrollBar)
+	local AlliedRacesFrame = AlliedRacesFrame
+	F.ReskinPortraitFrame(AlliedRacesFrame, true)
 	select(2, AlliedRacesFrame.ModelFrame:GetRegions()):Hide()
 
+	local scrollFrame = AlliedRacesFrame.RaceInfoFrame.ScrollFrame
+	F.ReskinScroll(scrollFrame.ScrollBar)
+	AlliedRacesFrame.RaceInfoFrame.AlliedRacesRaceName:SetTextColor(1, .8, 0)
+	scrollFrame.Child.RaceDescriptionText:SetTextColor(1, 1, 1)
+	scrollFrame.Child.RacialTraitsLabel:SetTextColor(1, .8, 0)
+
 	AlliedRacesFrame:HookScript("OnShow", function(self)
-		local parent = self.RaceInfoFrame.ScrollFrame.Child
+		local parent = scrollFrame.Child
 		for i = 1, parent:GetNumChildren() do
 			local bu = select(i, parent:GetChildren())
 
@@ -19,12 +20,7 @@ C.themes["Blizzard_AlliedRacesUI"] = function()
 				bu.Icon:SetTexCoord(.08, .92, .08, .92)
 				select(3, bu:GetRegions()):Hide()
 				F.CreateBG(bu.Icon)
-
-				bu.styled = true
-			end
-
-			if bu.Bullet and not bu.styled then
-				bu.Text:SetTextColor(1, .8, 0)
+				bu.Text:SetTextColor(1, 1, 1)
 
 				bu.styled = true
 			end
