2a48348fe4a1016e26e745d60abb0f4982cd093a
siweia <siwei1990@outlook.com>
2018-12-24 14:04:40 +0800

    Buff indicator anchor tweaks

--- Interface/AddOns/NDui/Modules/UFs/Raid.lua
+++ Interface/AddOns/NDui/Modules/UFs/Raid.lua
@@ -366,8 +366,8 @@ function UF:CreateBuffIndicator(self)
 		icon.count = B.CreateFS(icon, 12, "")
 		icon.count:ClearAllPoints()
 		if NDuiDB["UFs"]["BuffTimerIndicator"] then
-			icon.timer = B.CreateFS(icon, 12, "")
 			local point, anchorPoint, x, y = unpack(counterOffsets[anchor][2])
+			icon.timer = B.CreateFS(icon, 12, "", false, "CENTER", -x, 0)
 			icon.count:SetPoint(point, icon.timer, anchorPoint, x, y)
 		else
 			icon.bg = icon:CreateTexture(nil, "BACKGROUND")
