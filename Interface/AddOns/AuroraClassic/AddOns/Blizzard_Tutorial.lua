dd04565a1ac917d457b9b2d7ebf40fe2b3fe62d4
siweia <siwei1990@outlook.com>
2019-01-08 18:45:40 +0800

    Reskin TutorialUI

---
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_Tutorial.lua
@@ -0,0 +1,11 @@
+local F, C = unpack(select(2, ...))
+
+C.themes["Blizzard_Tutorial"] = function()
+	local tutorialFrame = NPE_TutorialKeyboardMouseFrame_Frame
+	F.RemoveSlice(tutorialFrame)
+	F.SetBD(tutorialFrame)
+	tutorialFrame.TitleBg:Hide()
+	tutorialFrame.portrait:SetAlpha(0)
+	F.ReskinClose(tutorialFrame.CloseButton)
+	NPE_TutorialKeyString:SetTextColor(1, 1, 1)
+end
