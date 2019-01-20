abbfa051549c91e238b6b1726e01f78f72611839
siweia <siwei1990@outlook.com>
2019-01-20 15:10:42 +0800

    Clean up PetFilterTab

--- Interface/AddOns/NDui/Plugins/PetFilterTab.lua
+++ Interface/AddOns/NDui/Plugins/PetFilterTab.lua
@@ -12,7 +12,7 @@ local function loadPetFilter()
 		PetJournalEnhancedListScrollFrame:SetPoint("TOPLEFT", PetJournalLeftInset, 3, -60)
 	end
 
-	local QuickFilter_Function = function(self, button)
+	local function QuickFilter_Function(self, button)
 		local activeCount = 0
 		for petType, _ in ipairs(PET_TYPE_SUFFIX) do
 			local btn = _G["PetJournalQuickFilterButton"..petType]
@@ -27,10 +27,10 @@ local function loadPetFilter()
 			end
 
 			if btn.isActive then
-				btn.Shadow:SetBackdropBorderColor(1, 1, 0)
+				btn:SetBackdropBorderColor(1, 1, 1)
 				activeCount = activeCount + 1
 			else
-				btn.Shadow:SetBackdropBorderColor(0, 0, 0)
+				btn:SetBackdropBorderColor(0, 0, 0)
 			end
 			C_PetJournal.SetPetTypeFilter(btn.petType, btn.isActive)
 		end
@@ -57,27 +57,16 @@ local function loadPetFilter()
 		local btn = CreateFrame("Button", "PetJournalQuickFilterButton"..petIndex, PetJournal)
 		btn:SetSize(24, 24)
 		btn:SetPoint("TOPLEFT", PetJournalLeftInset, 6 + 25 * (petIndex-1), -33)
+		B.PixelIcon(btn, "Interface\\ICONS\\Pet_Type_"..PET_TYPE_SUFFIX[petType], true)
 
-		local icon = btn:CreateTexture(nil, "ARTWORK")
-		icon:SetTexture("Interface\\ICONS\\Pet_Type_"..PET_TYPE_SUFFIX[petType])
-		icon:SetPoint("TOPLEFT", -1, 1)
-		icon:SetPoint("BOTTOMRIGHT", 1, -1)
-		B.CreateSD(btn, 1, 3)
 		if C_PetJournal.IsPetTypeChecked(petType) then
 			btn.isActive = true
-			btn.Shadow:SetBackdropBorderColor(1, 1, 0)
+			btn:SetBackdropBorderColor(1, 1, 1)
 			activeCount = activeCount + 1
 		else
 			btn.isActive = false
 		end
 		btn.petType = petType
-
-		btn:SetHighlightTexture(DB.bdTex)
-		local hl = btn:GetHighlightTexture()
-		hl:SetVertexColor(1, 1, 1, .25)
-		hl:SetPoint("TOPLEFT", 2, -2)
-		hl:SetPoint("BOTTOMRIGHT", -2, 2)
-
 		btn:SetScript("OnMouseUp", QuickFilter_Function)
 	end
 
@@ -85,10 +74,11 @@ local function loadPetFilter()
 		for petIndex, _ in ipairs(PET_TYPE_SUFFIX) do
 			local btn = _G["PetJournalQuickFilterButton"..petIndex]
 			btn.isActive = false
-			btn.Shadow:SetBackdropBorderColor(0, 0, 0)
+			btn:SetBackdropBorderColor(0, 0, 0)
 		end
 	end
 end
+
 local function setupPetFilter(event, addon)
 	if not NDuiDB["Misc"]["PetFilter"] then
 		B:UnregisterEvent(event, setupPetFilter)
