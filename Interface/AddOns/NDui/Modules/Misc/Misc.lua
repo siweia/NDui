bc734c3dcbf582258d6a21b9a7cc8573c782a62d
siweia <siwei1990@outlook.com>
2019-01-26 00:50:45 +0800

    Cleanup

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -421,26 +421,6 @@ do
 			end
 		end)
 	end
-
-	-- https://www.townlong-yak.com/bugs/afKy4k-HonorFrameLoadTaint
-	if (UIDROPDOWNMENU_VALUE_PATCH_VERSION or 0) < 2 then
-		UIDROPDOWNMENU_VALUE_PATCH_VERSION = 2
-		hooksecurefunc("UIDropDownMenu_InitializeHelper", function()
-			if UIDROPDOWNMENU_VALUE_PATCH_VERSION ~= 2 then return end
-
-			for i = 1, UIDROPDOWNMENU_MAXLEVELS do
-				for j = 1, UIDROPDOWNMENU_MAXBUTTONS do
-					local b = _G["DropDownList"..i.."Button"..j]
-					if not (issecurevariable(b, "value") or b:IsShown()) then
-						b.value = nil
-						repeat
-							j, b["fx" .. j] = j+1
-						until issecurevariable(b, "value")
-					end
-				end
-			end
-		end)
-	end
 end
 
 -- Roll Gold
