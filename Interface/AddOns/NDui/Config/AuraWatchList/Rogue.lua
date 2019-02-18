3fd99a465871156d5b2a0b1d1f6f9442ccdfc6a1
siweia <siwei1990@outlook.com>
2019-02-18 17:31:12 +0800

    Update auras for Rogue

--- Interface/AddOns/NDui/Config/AuraWatchList/Rogue.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/Rogue.lua
@@ -56,12 +56,12 @@ local list = {
 		{AuraID = 185422, UnitID = "player"},	-- 影舞
 		{AuraID = 212283, UnitID = "player"},	-- 死亡标记
 		{AuraID = 202754, UnitID = "player"},	-- 隐秘刀刃
-		{AuraID = 193356, UnitID = "player"},	-- 强势连击
-		{AuraID = 193357, UnitID = "player"},	-- 暗鲨涌动
-		{AuraID = 193358, UnitID = "player"},	-- 大乱斗
-		{AuraID = 193359, UnitID = "player"},	-- 双巧手
-		{AuraID = 199603, UnitID = "player"},	-- 骷髅黑帆
-		{AuraID = 199600, UnitID = "player"},	-- 埋藏的宝藏
+		{AuraID = 193356, UnitID = "player", Text = L["Combo"]},	-- 强势连击，骰子
+		{AuraID = 193357, UnitID = "player", Text = L["Crit"]},	-- 暗鲨涌动，骰子
+		{AuraID = 193358, UnitID = "player", Text = L["AttackSpeed"]},	-- 大乱斗，骰子
+		{AuraID = 193359, UnitID = "player", Text = L["CD"]},	-- 双巧手，骰子
+		{AuraID = 199603, UnitID = "player", Text = L["Strike"]},	-- 骷髅黑帆，骰子
+		{AuraID = 199600, UnitID = "player", Text = L["Power"]},	-- 埋藏的宝藏，骰子
 		{AuraID = 202665, UnitID = "player"},	-- 恐惧之刃诅咒
 		{AuraID = 199754, UnitID = "player"},	-- 还击
 		{AuraID = 195627, UnitID = "player"},	-- 可乘之机
