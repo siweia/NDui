c71e9b6c8840486ef84bb4d0f6dec56042881d96
siweia <siwei1990@outlook.com>
2019-03-05 13:43:57 +0800

    Replace deprecate API

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -532,6 +532,8 @@ end
 
 -- TradeFrame hook
 do
+	local IsGuildMember, BNGetGameAccountInfoByGUID, C_FriendList_IsFriend = IsGuildMember, BNGetGameAccountInfoByGUID, C_FriendList.IsFriend
+
 	local infoText = B.CreateFS(TradeFrame, 16, "")
 	infoText:ClearAllPoints()
 	infoText:SetPoint("TOP", TradeFrameRecipientNameText, "BOTTOM", 0, -5)
@@ -543,7 +545,7 @@ do
 		local guid = UnitGUID("NPC")
 		if not guid then return end
 		local text = "|cffff0000"..L["Stranger"]
-		if BNGetGameAccountInfoByGUID(guid) or IsCharacterFriend(guid) then
+		if BNGetGameAccountInfoByGUID(guid) or C_FriendList_IsFriend(guid) then
 			text = "|cffffff00"..FRIEND
 		elseif IsGuildMember(guid) then
 			text = "|cff00ff00"..GUILD
