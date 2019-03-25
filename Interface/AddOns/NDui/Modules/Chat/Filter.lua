2e3164ed77fb1c6d37f7ba97a80e55c9b5b26232
siweia <siwei1990@outlook.com>
2019-03-26 03:01:35 +0800

    Update chatfilter

--- Interface/AddOns/NDui/Modules/Chat/Filter.lua
+++ Interface/AddOns/NDui/Modules/Chat/Filter.lua
@@ -75,7 +75,6 @@ local function getFilterResult(msg, name)
 		local line = chatLines[i]
 		if line[1] == msgTable[1] and ((msgTable[3] - line[3] < .6) or strDiff(line[2], msgTable[2]) <= .1) then
 			tremove(chatLines, i)
-			C.BadBoys[name] = (C.BadBoys[name] or 0) + 1
 			return true
 		end
 	end
@@ -89,10 +88,12 @@ local function genChatFilter(_, event, msg, author, _, _, _, flag, _, _, _, _, l
 		local name = Ambiguate(author, "none")
 		if UnitIsUnit(name, "player") or (event == "CHAT_MSG_WHISPER" and flag == "GM") or flag == "DEV" then
 			return
-		elseif guid and (IsGuildMember(guid) or BNGetGameAccountInfoByGUID(guid) or C_FriendList_IsFriend(guid) or IsGUIDInGroup(guid)) then
+		elseif guid and (IsGuildMember(guid) or BNGetGameAccountInfoByGUID(guid) or C_FriendList_IsFriend(guid) or (IsInInstance() and IsGUIDInGroup(guid))) then
 			return
 		end
 		filterResult = getFilterResult(msg, name)
+
+		if filterResult then C.BadBoys[name] = (C.BadBoys[name] or 0) + 1 end
 	end
 
 	return filterResult
@@ -200,6 +201,8 @@ function module:ChatFilter()
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", genChatFilter)
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_EMOTE", genChatFilter)
 		ChatFrame_AddMessageEventFilter("CHAT_MSG_TEXT_EMOTE", genChatFilter)
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", genChatFilter)
+		ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", genChatFilter)
 	end
 
 	if NDuiDB["Chat"]["BlockAddonAlert"] then
