1398e6febb548346779ca8647bfb14db7881b139
siweia <siwei1990@outlook.com>
2019-03-28 20:33:31 +0800

    Typo

--- Interface/AddOns/NDui/Modules/Chat/Filter.lua
+++ Interface/AddOns/NDui/Modules/Chat/Filter.lua
@@ -37,7 +37,7 @@ end
 
 C.BadBoys = {} -- debug
 local chatLines, prevLineID, filterResult = {}, 0, false
-local function getFilterResult(msg, name)
+local function getFilterResult(event, msg, name, flag, guid)
 	if name == DB.MyName or (event == "CHAT_MSG_WHISPER" and flag == "GM") or flag == "DEV" then
 		return
 	elseif guid and (IsGuildMember(guid) or BNGetGameAccountInfoByGUID(guid) or C_FriendList_IsFriend(guid) or (IsInInstance() and IsGUIDInGroup(guid))) then
@@ -92,7 +92,7 @@ local function genChatFilter(_, event, msg, author, _, _, _, flag, _, _, _, _, l
 		prevLineID = lineID
 
 		local name = Ambiguate(author, "none")
-		filterResult = getFilterResult(msg, name)
+		filterResult = getFilterResult(event, msg, name, flag, guid)
 		if filterResult then C.BadBoys[name] = (C.BadBoys[name] or 0) + 1 end
 	end
 
