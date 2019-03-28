c9c76b297e3804cd1f8ae827e1d9fb67a39b7a8d
siweia <siwei1990@outlook.com>
2019-03-28 15:54:42 +0800

    Fix itemlevel shows on ignore slot

--- Interface/AddOns/NDui/Modules/Misc/ItemLevel.lua
+++ Interface/AddOns/NDui/Modules/Misc/ItemLevel.lua
@@ -93,8 +93,7 @@ function module:ShowItemLevel()
 
 	hooksecurefunc("EquipmentFlyout_DisplayButton", function(button)
 		local location = button.location
-		if not location or location < 0 then return end
-		if location == EQUIPMENTFLYOUT_PLACEINBAGS_LOCATION then
+		if not location or location >= EQUIPMENTFLYOUT_FIRST_SPECIAL_LOCATION then
 			if button.iLvl then button.iLvl:SetText("") end
 			return
 		end
