364d83f87e8f7b14866ebead0ab0c0867f91d266
siweia <siwei1990@outlook.com>
2019-04-05 18:01:42 +0800

    Update oUF core

--- Interface/AddOns/NDui/Libs/oUF/Elements/runes.lua
+++ Interface/AddOns/NDui/Libs/oUF/Elements/runes.lua
@@ -19,7 +19,8 @@ A default texture will be applied if the sub-widgets are StatusBars and don't ha
 
 .colorSpec - Use `self.colors.runes[specID]` to color the bar based on player's spec. `specID` is defined by the return
              value of [GetSpecialization](http://wowprogramming.com/docs/api/GetSpecialization.html) (boolean)
-.sortOrder - Sorting order (string?)['asc', 'desc']
+.sortOrder - Sorting order. Sorts by the remaining cooldown time, 'asc' - from the least cooldown time remaining (fully
+             charged) to the most (fully depleted), 'desc' - the opposite (string?)['asc', 'desc']
 
 ## Sub-Widgets Options
 
