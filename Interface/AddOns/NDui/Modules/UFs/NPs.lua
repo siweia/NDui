45a8bcf6fbeb61d4d1edda6505cf55766f63e250
siweia <siwei1990@outlook.com>
2019-04-14 12:32:31 +0800

    Update unit classification type

--- Interface/AddOns/NDui/Modules/UFs/NPs.lua
+++ Interface/AddOns/NDui/Modules/UFs/NPs.lua
@@ -270,8 +270,8 @@ local function UpdateDungeonProgress(self, unit)
 end
 
 local classify = {
-	rare = {.7, .7, .7},
-	elite = {1, 1, 0},
+	rare = {1, 1, 1, true},
+	elite = {1, 1, 1},
 	rareelite = {1, .1, .1},
 	worldboss = {0, 1, 0},
 }
@@ -280,8 +280,9 @@ local function UpdateUnitClassify(self, unit)
 	local class = UnitClassification(unit)
 	if self.creatureIcon then
 		if class and classify[class] then
-			local r, g, b = unpack(classify[class])
+			local r, g, b, desature = unpack(classify[class])
 			self.creatureIcon:SetVertexColor(r, g, b)
+			self.creatureIcon:SetDesaturated(desature)
 			self.creatureIcon:SetAlpha(1)
 		else
 			self.creatureIcon:SetAlpha(0)
@@ -440,11 +441,13 @@ function UF:CreatePlates(unit)
 		self.tarMark = mark
 		self:RegisterEvent("PLAYER_TARGET_CHANGED", UpdateTargetMark, true)
 
-		local cicon = self:CreateTexture(nil, "OVERLAY")
-		cicon:SetPoint("LEFT", self, 1, 5)
-		cicon:SetSize(12, 12)
-		cicon:SetTexture("Interface\\MINIMAP\\ObjectIcons")
-		cicon:SetTexCoord(.391, .487, .644, .74)
+		local iconFrame = CreateFrame("Frame", nil, self)
+		iconFrame:SetAllPoints()
+		iconFrame:SetFrameLevel(self:GetFrameLevel() + 2)
+		local cicon = iconFrame:CreateTexture(nil, "ARTWORK")
+		cicon:SetAtlas("VignetteKill")
+		cicon:SetPoint("BOTTOMLEFT", self, "LEFT", 0, -4)
+		cicon:SetSize(18, 18)
 		cicon:SetAlpha(0)
 		self.creatureIcon = cicon
 
