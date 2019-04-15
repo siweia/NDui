5a72878ea6de5b9daa54c8690cc4e6b4f42e4550
siweia <siwei1990@outlook.com>
2019-04-16 02:30:04 +0800

    AuroraClassic 2.7.5

--- Interface/AddOns/AuroraClassic/FrameXML/ChatFrame.lua
+++ Interface/AddOns/AuroraClassic/FrameXML/ChatFrame.lua
@@ -1,6 +1,7 @@
 local F, C = unpack(select(2, ...))
 
 tinsert(C.themes["AuroraClassic"], function()
+	local r, g, b = C.r, C.g, C.b
 	local friendTex = "Interface\\HELPFRAME\\ReportLagIcon-Chat"
 	local queueTex = "Interface\\HELPFRAME\\HelpIcon-ItemRestoration"
 	local homeTex = "Interface\\Buttons\\UI-HomeButton"
@@ -43,6 +44,16 @@ tinsert(C.themes["AuroraClassic"], function()
 	ChatFrameMenuButton:SetNormalTexture(homeTex)
 	ChatFrameMenuButton:SetPushedTexture(homeTex)
 
+	local function scrollOnEnter(self)
+		self.thumbBG:SetBackdropColor(r, g, b, .25)
+		self.thumbBG:SetBackdropBorderColor(r, g, b)
+	end
+
+	local function scrollOnLeave(self)
+		self.thumbBG:SetBackdropColor(0, 0, 0, 0)
+		self.thumbBG:SetBackdropBorderColor(0, 0, 0)
+	end
+
 	local function reskinScroll(self)
 		local bu = _G[self:GetName().."ThumbTexture"]
 		bu:SetAlpha(0)
@@ -51,7 +62,11 @@ tinsert(C.themes["AuroraClassic"], function()
 		F.CreateGradient(bg)
 		local down = self.ScrollToBottomButton
 		F.ReskinArrow(down, "down")
-		down:SetPoint("BOTTOMRIGHT", _G[self:GetName().."ResizeButton"], "TOPRIGHT", -3, -2)
+		down:SetPoint("BOTTOMRIGHT", _G[self:GetName().."ResizeButton"], "TOPRIGHT", -4, -2)
+
+		self.ScrollBar.thumbBG = bg
+		self.ScrollBar:HookScript("OnEnter", scrollOnEnter)
+		self.ScrollBar:HookScript("OnLeave", scrollOnLeave)
 	end
 
 	for i = 1, NUM_CHAT_WINDOWS do
