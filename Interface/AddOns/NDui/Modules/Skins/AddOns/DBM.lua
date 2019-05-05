4c7540725e874a59a26cb6d5fe0681e1ce3517a5
siweia <siwei1990@outlook.com>
2019-05-06 00:52:00 +0800

    Make sure the default raid notice reskinned

--- Interface/AddOns/NDui/Modules/Skins/AddOns/DBM.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/DBM.lua
@@ -3,6 +3,23 @@ local B, C, L, DB = unpack(ns)
 local module = B:GetModule("Skins")
 
 function module:DBMSkin()
+	-- Default notice message
+	local RaidNotice_AddMessage_ = RaidNotice_AddMessage
+	RaidNotice_AddMessage = function(noticeFrame, textString, colorInfo)
+		if strfind(textString, "|T") then
+            if strmatch(textString, ":(%d+):(%d+)") then
+                local size1, size2 = strmatch(textString, ":(%d+):(%d+)")
+                size1, size2 = size1 + 3, size2 + 3
+                textString = gsub(textString,":(%d+):(%d+)",":"..size1..":"..size2..":0:0:64:64:5:59:5:59")
+            elseif strmatch(textString, ":(%d+)|t") then
+                local size = strmatch(textString, ":(%d+)|t")
+                size = size + 3
+                textString = gsub(textString,":(%d+)|t",":"..size..":"..size..":0:0:64:64:5:59:5:59|t")
+            end
+		end
+		return RaidNotice_AddMessage_(noticeFrame, textString, colorInfo)
+	end
+
 	if not IsAddOnLoaded("DBM-Core") then return end
 	if not NDuiDB["Skins"]["DBM"] then return end
 
@@ -168,22 +185,6 @@ function module:DBMSkin()
 		DBM.InfoFrame:Show(5, "test")
 		DBM.InfoFrame:Hide()
 		DBMInfoFrame:HookScript("OnShow", B.ReskinTooltip)
-	end
-
-	local RaidNotice_AddMessage_ = RaidNotice_AddMessage
-	RaidNotice_AddMessage = function(noticeFrame, textString, colorInfo)
-		if strfind(textString, "|T") then
-            if strmatch(textString, ":(%d+):(%d+)") then
-                local size1, size2 = strmatch(textString, ":(%d+):(%d+)")
-                size1, size2 = size1 + 3, size2 + 3
-                textString = gsub(textString,":(%d+):(%d+)",":"..size1..":"..size2..":0:0:64:64:5:59:5:59")
-            elseif strmatch(textString, ":(%d+)|t") then
-                local size = strmatch(textString, ":(%d+)|t")
-                size = size + 3
-                textString = gsub(textString,":(%d+)|t",":"..size..":"..size..":0:0:64:64:5:59:5:59|t")
-            end
-		end
-		return RaidNotice_AddMessage_(noticeFrame, textString, colorInfo)
 	end
 
 	-- Force Settings
