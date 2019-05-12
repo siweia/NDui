60579c6986e66ea2a1eda2748e59c9dbbc102381
siweia <siwei1990@outlook.com>
2019-05-12 20:43:50 +0800

    Dragable ModelPreviewFrame

--- Interface/AddOns/NDui/Plugins/DragEmAll.lua
+++ Interface/AddOns/NDui/Plugins/DragEmAll.lua
@@ -28,6 +28,7 @@ local frames = {
 	["LootFrame"] = false,
 	["MailFrame"] = false,
 	["MerchantFrame"] = false,
+	["ModelPreviewFrame"] = false,
 	["OpenMailFrame"] = false,
 	["PaperDollFrame"] = true,
 	["PetitionFrame"] = false,
