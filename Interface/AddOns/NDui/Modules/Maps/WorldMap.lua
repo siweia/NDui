c3bf2c7b5b293100c58a1c2f845b9ce3b620746f
siweia <siwei1990@outlook.com>
2019-06-17 13:34:34 +0800

    Revamp worldmap coords

--- Interface/AddOns/NDui/Modules/Maps/WorldMap.lua
+++ Interface/AddOns/NDui/Modules/Maps/WorldMap.lua
@@ -2,8 +2,17 @@
 local B, C, L, DB = unpack(ns)
 local module = B:RegisterModule("Maps")
 
+local select = select
+local CreateVector2D = CreateVector2D
+local UnitPosition, GetCursorPosition = UnitPosition, GetCursorPosition
+local C_Map_GetWorldPosFromMapPos = C_Map.GetWorldPosFromMapPos
+local C_Map_GetBestMapForUnit = C_Map.GetBestMapForUnit
+
 local mapRects = {}
 local tempVec2D = CreateVector2D(0, 0)
+local mapID
+local mapBody = WorldMapFrame:GetCanvasContainer()
+local scale, width, height = mapBody:GetEffectiveScale(), mapBody:GetWidth(), mapBody:GetHeight()
 
 function module:GetPlayerMapPos(mapID)
 	tempVec2D.x, tempVec2D.y = UnitPosition("player")
@@ -12,8 +21,8 @@ function module:GetPlayerMapPos(mapID)
 	local mapRect = mapRects[mapID]
 	if not mapRect then
 		mapRect = {}
-		mapRect[1] = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0)))
-		mapRect[2] = select(2, C_Map.GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1)))
+		mapRect[1] = select(2, C_Map_GetWorldPosFromMapPos(mapID, CreateVector2D(0, 0)))
+		mapRect[2] = select(2, C_Map_GetWorldPosFromMapPos(mapID, CreateVector2D(1, 1)))
 		mapRect[2]:Subtract(mapRect[1])
 
 		mapRects[mapID] = mapRect
@@ -23,80 +32,82 @@ function module:GetPlayerMapPos(mapID)
 	return tempVec2D.y/mapRect[2].y, tempVec2D.x/mapRect[2].x
 end
 
-function module:UpdateCoords()
+function module:GetCursorCoords()
+	local left, top = mapBody:GetLeft() or 0, mapBody:GetTop() or 0
+	local x, y = GetCursorPosition()
+	local cx = (x/scale - left) / width
+	local cy = (top - y/scale) / height
+	if cx < 0 or cx > 1 or cy < 0 or cy > 1 then return end
+	return cx, cy
+end
+
+local function CoordsFormat(owner, none)
+	local text = none and ": --, --" or ": %.1f, %.1f"
+	return owner..DB.MyColor..text
+end
+
+function module:UpdateCoords(elapsed)
+	self.elapsed = (self.elapsed or 0) + elapsed
+	if self.elapsed > .1 then
+		local cx, cy = module:GetCursorCoords()
+		if cx and cy then
+			cursor:SetFormattedText(CoordsFormat(L["Mouse"]), 100 * cx, 100 * cy)
+		else
+			cursor:SetText(CoordsFormat(L["Mouse"], true))
+		end
+
+		if not mapID then
+			player:SetText(CoordsFormat(PLAYER, true))
+		else
+			local x, y = module:GetPlayerMapPos(mapID)
+			if not x or (x == 0 and y == 0) then
+				player:SetText(CoordsFormat(PLAYER, true))
+			else
+				player:SetFormattedText(CoordsFormat(PLAYER), 100 * x, 100 * y)
+			end
+		end
+
+		self.elapsed = 0
+	end
+end
+
+function module:SetupCoords()
 	if not NDuiDB["Map"]["Coord"] then return end
 
 	local player = B.CreateFS(WorldMapFrame.BorderFrame, 14, "", false, "TOPLEFT", 60, -6)
 	local cursor = B.CreateFS(WorldMapFrame.BorderFrame, 14, "", false, "TOPLEFT", 180, -6)
 	WorldMapFrame.BorderFrame.Tutorial:SetPoint("TOPLEFT", WorldMapFrame, "TOPLEFT", -12, -12)
 
-	local mapBody = WorldMapFrame:GetCanvasContainer()
-	local scale, width, height = mapBody:GetEffectiveScale(), mapBody:GetWidth(), mapBody:GetHeight()
 	hooksecurefunc(WorldMapFrame, "OnFrameSizeChanged", function()
 		width, height = mapBody:GetWidth(), mapBody:GetHeight()
 	end)
 
-	local mapID
 	hooksecurefunc(WorldMapFrame, "OnMapChanged", function(self)
-		if self:GetMapID() == C_Map.GetBestMapForUnit("player") then
+		if self:GetMapID() == C_Map_GetBestMapForUnit("player") then
 			mapID = self:GetMapID()
 		else
 			mapID = nil
 		end
 	end)
 
-	local function CursorCoords()
-		local left, top = mapBody:GetLeft() or 0, mapBody:GetTop() or 0
-		local x, y = GetCursorPosition()
-		local cx = (x/scale - left) / width
-		local cy = (top - y/scale) / height
-		if cx < 0 or cx > 1 or cy < 0 or cy > 1 then return end
-		return cx, cy
-	end
-
-	local function CoordsFormat(owner, none)
-		local text = none and ": --, --" or ": %.1f, %.1f"
-		return owner..DB.MyColor..text
-	end
-
-	local function UpdateCoords(self, elapsed)
-		self.elapsed = (self.elapsed or 0) + elapsed
-		if self.elapsed > .1 then
-			local cx, cy = CursorCoords()
-			if cx and cy then
-				cursor:SetFormattedText(CoordsFormat(L["Mouse"]), 100 * cx, 100 * cy)
-			else
-				cursor:SetText(CoordsFormat(L["Mouse"], true))
-			end
-
-			if not mapID then
-				player:SetText(CoordsFormat(PLAYER, true))
-			else
-				local x, y = module:GetPlayerMapPos(mapID)
-				if not x or (x == 0 and y == 0) then
-					player:SetText(CoordsFormat(PLAYER, true))
-				else
-					player:SetFormattedText(CoordsFormat(PLAYER), 100 * x, 100 * y)
-				end
-			end
-
-			self.elapsed = 0
-		end
-	end
-
 	local CoordsUpdater = CreateFrame("Frame", nil, WorldMapFrame.BorderFrame)
-	CoordsUpdater:SetScript("OnUpdate", UpdateCoords)
+	CoordsUpdater:SetScript("OnUpdate", module.UpdateCoords)
+end
+
+function module:UpdateMapScale()
+	if self.isMaximized and self:GetScale() ~= 1 then
+		self:SetScale(1)
+	elseif not self.isMaximized and self:GetScale() ~= NDuiDB["Map"]["MapScale"] then
+		self:SetScale(NDuiDB["Map"]["MapScale"])
+	end
+end
+
+function module:UpdateMapAnchor()
+	module.UpdateMapScale(self)
+	if not self.isMaximized then B.RestoreMF(self) end
 end
 
 function module:WorldMapScale()
-	local function setupScale(self)
-		if self.isMaximized and self:GetScale() ~= 1 then
-			self:SetScale(1)
-		elseif not self.isMaximized and self:GetScale() ~= NDuiDB["Map"]["MapScale"] then
-			self:SetScale(NDuiDB["Map"]["MapScale"])
-		end
-	end
-
 	if NDuiDB["Map"]["MapScale"] > 1 then
 		WorldMapFrame.ScrollContainer.GetCursorPosition = function(f)
 			local x, y = MapCanvasScrollControllerMixin.GetCursorPosition(f)
@@ -105,17 +116,13 @@ function module:WorldMapScale()
 		end
 	end
 
-	local function updateMapAnchor(self)
-		setupScale(self)
-		if not self.isMaximized then B.RestoreMF(self) end
-	end
 	B.CreateMF(WorldMapFrame, nil, true)
-	hooksecurefunc(WorldMapFrame, "SynchronizeDisplayState", updateMapAnchor)
+	hooksecurefunc(WorldMapFrame, "SynchronizeDisplayState", self.UpdateMapAnchor)
 end
 
 function module:OnLogin()
 	self:WorldMapScale()
-	self:UpdateCoords()
+	self:SetupCoords()
 	self:SetupMinimap()
 	self:MapReveal()
 end
