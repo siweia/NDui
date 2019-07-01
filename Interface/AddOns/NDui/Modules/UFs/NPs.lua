f351c1a7fc7df29390beb41b652bb5dd3a46ed48
siweia <siwei1990@outlook.com>
2019-07-02 02:09:58 +0800

    Add follower progressbar string on nameplate

--- Interface/AddOns/NDui/Modules/UFs/NPs.lua
+++ Interface/AddOns/NDui/Modules/UFs/NPs.lua
@@ -392,6 +392,16 @@ local function AddMouseoverIndicator(self)
 	self.HighlightIndicator = f
 end
 
+function UF:AddFollowerXP(self)
+	local bar = CreateFrame("StatusBar", nil, self)
+	bar:SetSize(NDuiDB["Nameplate"]["Width"], NDuiDB["Nameplate"]["Height"])
+	bar:SetPoint("TOP", self.Castbar, "BOTTOM", 0, -5)
+	B.CreateSB(bar, false, .3, .7, 1)
+	bar.progressText = B.CreateFS(bar, 9)
+
+	self.NazjatarFollowerXP = bar
+end
+
 -- Create Nameplates
 function UF:CreatePlates(unit)
 	self.mystyle = "nameplate"
@@ -414,6 +424,7 @@ function UF:CreatePlates(unit)
 		UF:CreatePrediction(self)
 		UF:CreateAuras(self)
 		UF:CreatePVPClassify(self)
+		UF:AddFollowerXP(self)
 
 		self.powerText = B.CreateFS(self, 15, "", false, "LEFT", 0, 0)
 		self.powerText:SetPoint("LEFT", self, "RIGHT", 2, 0)
