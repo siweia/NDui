5ca5e7bd0c8532420aa7af7856ec6fca13fb562d
siweia <siwei1990@outlook.com>
2019-07-27 01:44:57 +0800

    AuroraClassic 2.12.6

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_WarboardUI.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_WarboardUI.lua
@@ -26,6 +26,8 @@ C.themes["Blizzard_WarboardUI"] = function()
 			self.bg = F.SetBD(self)
 		end
 
+		self.CloseButton:SetPoint("TOPRIGHT", -2, -2)
+
 		for i = 1, self:GetNumOptions() do
 			local option = self.Options[i]
 			if reskinFont then
