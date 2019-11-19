037b559a249fc4dcfadfd61c82692889b923d1ff
siweia <siwei1990@gmail.com>
2019-11-19 19:43:49 +0800

    This bug minimap dropdown

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -554,33 +554,6 @@ do
 			end
 		end)
 	end
-
-	-- https://www.townlong-yak.com/bugs/Mx7CWN-RefreshOverread
-	if (UIDD_REFRESH_OVERREAD_PATCH_VERSION or 0) < 1 then
-		UIDD_REFRESH_OVERREAD_PATCH_VERSION = 1
-		local function drop(t, k)
-			local c = 42
-			t[k] = nil
-			while not issecurevariable(t, k) do
-				if t[c] == nil then
-					t[c] = nil
-				end
-				c = c + 1
-			end
-		end
-		hooksecurefunc("UIDropDownMenu_InitializeHelper", function()
-			if UIDD_REFRESH_OVERREAD_PATCH_VERSION ~= 1 then
-				return
-			end
-			for i = 1,UIDROPDOWNMENU_MAXLEVELS do
-				for j = 1,UIDROPDOWNMENU_MAXBUTTONS do
-					local b, _ = _G["DropDownList"..i.."Button"..j]
-					_ = issecurevariable(b, "checked") or drop(b, "checked")
-					_ = issecurevariable(b, "notCheckable") or drop(b, "notCheckable")
-				end
-			end
-		end)
-	end
 end
 
 -- Select target when click on raid units
