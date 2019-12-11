3bf67fb65d0329dd8ccd8052730ef5354913c7dd
siweia <siwei1990@gmail.com>
2019-12-12 07:24:11 +0800

    Fix issue for empty main container

--- Interface/AddOns/NDui/Modules/Bags/Core.lua
+++ Interface/AddOns/NDui/Modules/Bags/Core.lua
@@ -582,7 +582,8 @@ function module:OnLogin()
 		local spacing = 5
 		local xOffset = 5
 		local yOffset = -offset + spacing
-		local width, height = self:LayoutButtons("grid", columns, spacing, xOffset, yOffset)
+		local _, height = self:LayoutButtons("grid", columns, spacing, xOffset, yOffset)
+		local width = columns * (iconSize+spacing)-spacing
 		if self.freeSlot then
 			if NDuiDB["Bags"]["GatherEmpty"] then
 				local numSlots = #self.buttons + 1
@@ -597,7 +598,7 @@ function module:OnLogin()
 				self.freeSlot:Show()
 
 				if height < 0 then
-					width, height = columns * (iconSize+spacing)-spacing, iconSize
+					height = iconSize
 				elseif col == 1 then
 					height = height + iconSize + spacing
 				end
