6ccf16821fe990394f4915dbc6e9969a9633c6b8
siweia <siwei1990@gmail.com>
2019-12-22 21:08:22 +0800

    AuroraClassic update to 3.2.6

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_ArchaeologyUI.lua
+++ Interface/AddOns/AuroraClassic/AddOns/Blizzard_ArchaeologyUI.lua
@@ -7,7 +7,7 @@ C.themes["Blizzard_ArchaeologyUI"] = function()
 	F.Reskin(ArchaeologyFrameArtifactPageBackButton)
 
 	ArchaeologyFrameSummaryPageTitle:SetTextColor(1, 1, 1)
-	ArchaeologyFrameArtifactPageHistoryTitle:SetTextColor(1, 1, 1)
+	ArchaeologyFrameArtifactPageHistoryTitle:SetTextColor(1, .8, 0)
 	ArchaeologyFrameArtifactPageHistoryScrollChildText:SetTextColor(1, 1, 1)
 	ArchaeologyFrameHelpPageTitle:SetTextColor(1, 1, 1)
 	ArchaeologyFrameHelpPageDigTitle:SetTextColor(1, 1, 1)
@@ -19,25 +19,26 @@ C.themes["Blizzard_ArchaeologyUI"] = function()
 	ArchaeologyFrameCompletedPagePageText:SetTextColor(1, 1, 1)
 	ArchaeologyFrameSummaryPagePageText:SetTextColor(1, 1, 1)
 	for i = 1, ARCHAEOLOGY_MAX_RACES do
-		_G["ArchaeologyFrameSummaryPageRace"..i]:GetRegions():SetTextColor(1, 1, 1)
+		local bu = _G["ArchaeologyFrameSummaryPageRace"..i]
+		bu.raceName:SetTextColor(1, 1, 1)
 	end
 
 	for i = 1, ARCHAEOLOGY_MAX_COMPLETED_SHOWN do
-		local bu = _G["ArchaeologyFrameCompletedPageArtifact"..i]
-		bu:GetRegions():Hide()
-		select(2, bu:GetRegions()):Hide()
-		select(3, bu:GetRegions()):SetTexCoord(.08, .92, .08, .92)
-		select(4, bu:GetRegions()):SetTextColor(1, 1, 1)
-		select(5, bu:GetRegions()):SetTextColor(1, 1, 1)
-		F.CreateBDFrame(bu, .25)
-		local vline = CreateFrame("Frame", nil, bu)
-		vline:SetPoint("LEFT", 44, 0)
-		vline:SetSize(1, 44)
-		F.CreateBD(vline)
+		local buttonName = "ArchaeologyFrameCompletedPageArtifact"..i
+		local button = _G[buttonName]
+		local icon = _G[buttonName.."Icon"]
+		local name = _G[buttonName.."ArtifactName"]
+		local subText = _G[buttonName.."ArtifactSubText"]
+		F.StripTextures(button)
+		F.ReskinIcon(icon)
+		name:SetTextColor(1, .8, 0)
+		subText:SetTextColor(1, 1, 1)
+		local bg = F.CreateBDFrame(button, .25)
+		bg:SetPoint("TOPLEFT", -4, 4)
+		bg:SetPoint("BOTTOMRIGHT", 4, -4)
 	end
 
 	ArchaeologyFrameInfoButton:SetPoint("TOPLEFT", 3, -3)
-
 	ArchaeologyFrameSummarytButton:SetPoint("TOPLEFT", ArchaeologyFrame, "TOPRIGHT", 1, -50)
 	ArchaeologyFrameSummarytButton:SetFrameLevel(ArchaeologyFrame:GetFrameLevel() - 1)
 	ArchaeologyFrameCompletedButton:SetPoint("TOPLEFT", ArchaeologyFrame, "TOPRIGHT", 1, -120)
@@ -54,19 +55,22 @@ C.themes["Blizzard_ArchaeologyUI"] = function()
 	ArchaeologyFrameSummaryPagePrevPageButtonIcon:Hide()
 	ArchaeologyFrameSummaryPageNextPageButtonIcon:Hide()
 
-	ArchaeologyFrameRankBarBorder:Hide()
-	ArchaeologyFrameRankBarBackground:Hide()
+	F.StripTextures(ArchaeologyFrameRankBar)
 	ArchaeologyFrameRankBarBar:SetTexture(C.media.backdrop)
 	ArchaeologyFrameRankBarBar:SetGradient("VERTICAL", 0, .65, 0, 0, .75, 0)
 	ArchaeologyFrameRankBar:SetHeight(14)
 	F.CreateBD(ArchaeologyFrameRankBar, .25)
+	F.ReskinIcon(ArchaeologyFrameArtifactPageIcon)
 
-	ArchaeologyFrameArtifactPageSolveFrameStatusBarBarBG:Hide()
-	local bar = select(3, ArchaeologyFrameArtifactPageSolveFrameStatusBar:GetRegions())
-	bar:SetTexture(C.media.backdrop)
-	bar:SetGradient("VERTICAL", .65, .25, 0, .75, .35, .1)
+	F.StripTextures(ArchaeologyFrameArtifactPageSolveFrameStatusBar)
 	F.CreateBDFrame(ArchaeologyFrameArtifactPageSolveFrameStatusBar, .25)
+	local barTexture = ArchaeologyFrameArtifactPageSolveFrameStatusBar:GetStatusBarTexture()
+	barTexture:SetTexture(C.media.backdrop)
+	barTexture:SetGradient("VERTICAL", .65, .25, 0, .75, .35, .1)
 
-	ArchaeologyFrameArtifactPageIcon:SetTexCoord(.08, .92, .08, .92)
-	F.CreateBG(ArchaeologyFrameArtifactPageIcon)
+	-- ArcheologyDigsiteProgressBar
+	F.StripTextures(ArcheologyDigsiteProgressBar)
+	F.SetBD(ArcheologyDigsiteProgressBar.FillBar)
+	ArcheologyDigsiteProgressBar.FillBar:SetStatusBarTexture(C.media.backdrop)
+	ArcheologyDigsiteProgressBar.FillBar:SetStatusBarColor(.7, .3, .2)
 end
