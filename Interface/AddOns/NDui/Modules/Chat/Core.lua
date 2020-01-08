ba6a2cd1b5ffeed7f38bfd4bead0b80f73db8eb0
siweia <siwei1990@gmail.com>
2020-01-08 14:37:19 +0800

    Cleanup chatframe

--- Interface/AddOns/NDui/Modules/Chat/Core.lua
+++ Interface/AddOns/NDui/Modules/Chat/Core.lua
@@ -21,8 +21,11 @@ function module:TabSetAlpha(alpha)
 	end
 end
 
+local isScaling = false
 function module:UpdateChatSize()
 	if not NDuiDB["Chat"]["Lock"] then return end
+	if isScaling then return end
+	isScaling = true
 
 	ChatFrame1:ClearAllPoints()
 	ChatFrame1:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 0, 28)
@@ -32,6 +35,7 @@ function module:UpdateChatSize()
 	if bg then
 		bg:SetHeight(NDuiDB["Chat"]["ChatHeight"] + 30)
 	end
+	isScaling = false
 end
 
 function module:SkinChat()
@@ -83,6 +87,8 @@ function module:SkinChat()
 	B.HideObject(self.buttonFrame)
 	B.HideObject(self.ScrollBar)
 	B.HideObject(self.ScrollToBottomButton)
+
+	self.oldAlpha = self.oldAlpha or 0 -- fix blizz error, need reviewed
 
 	self.styled = true
 end
@@ -225,18 +231,6 @@ function module:ChatWhisperSticky()
 	end
 end
 
-local isScaling = false
-function module:FixChatFrameAnchor()
-	if isScaling then return end
-	isScaling = true
-
-	local x, y = select(4, ChatFrame1:GetPoint())
-	if x ~= 0 or y ~= 28 then
-		ChatFrame1:SetPoint("BOTTOMLEFT", UIParent, "BOTTOMLEFT", 0, 28)
-	end
-	isScaling = false
-end
-
 function module:UpdateTabColors(selected)
 	if selected then
 		self:GetFontString():SetTextColor(1, .8, 0)
@@ -298,11 +292,6 @@ function module:OnLogin()
 	SetCVar("chatStyle", "classic")
 	B.HideOption(InterfaceOptionsSocialPanelChatStyle)
 	CombatLogQuickButtonFrame_CustomTexture:SetTexture(nil)
-
-	-- Fix chatframe anchor after scaling
-	if NDuiDB["Chat"]["Lock"] then
-		B:RegisterEvent("UI_SCALE_CHANGED", self.FixChatFrameAnchor)
-	end
 
 	-- Add Elements
 	self:UpdateTimestamp()
