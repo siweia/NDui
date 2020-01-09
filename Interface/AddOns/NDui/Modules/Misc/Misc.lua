51490a9e91565dbc54ce481f869923a960d4f3c7
siweia <siwei1990@gmail.com>
2020-01-09 14:47:51 +0800

    Fix blizz addon list error

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -100,6 +100,14 @@ function M:OnLogin()
 			self.editBox:SetText(DELETE_ITEM_CONFIRM_STRING)
 		end
 	end)
+
+	-- Fix blizz bug in addon list
+	local _AddonTooltip_Update = AddonTooltip_Update
+	function AddonTooltip_Update(owner)
+		if not owner then return end
+		if owner:GetID() < 1 then return end
+		_AddonTooltip_Update(owner)
+	end
 end
 
 -- Get Naked
