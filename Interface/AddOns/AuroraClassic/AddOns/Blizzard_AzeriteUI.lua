b3f9f2d37d8bb7cf12f0b8d95bd07f7edb031783
siweia <siwei1990@gmail.com>
2020-01-11 23:56:42 +0800

    Remove AuroraClassic

--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_AzeriteUI.lua
+++
@@ -1,7 +0,0 @@
-local F, C = unpack(select(2, ...))
-
-C.themes["Blizzard_AzeriteUI"] = function()
-	F.ReskinPortraitFrame(AzeriteEmpoweredItemUI)
-	AzeriteEmpoweredItemUIBg:Hide()
-	AzeriteEmpoweredItemUI.ClipFrame.BackgroundFrame.Bg:Hide()
-end
--- Interface/AddOns/AuroraClassic/AddOns/Blizzard_FlightMap.lua
+++
@@ -1,7 +0,0 @@
-local F, C = unpack(select(2, ...))
-
-C.themes["Blizzard_FlightMap"] = function()
-	F.ReskinPortraitFrame(FlightMapFrame)
-	FlightMapFrameBg:Hide()
-	FlightMapFrame.ScrollContainer.Child.TiledBackground:Hide()
-end
