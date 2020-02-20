6729999065814827c890364a4b64a5108ab3c693
siweia <siwei1990@gmail.com>
2020-02-20 20:12:08 +0800

    Cleanup

--- Interface/AddOns/NDui/Modules/Tooltip/TooltipID.lua
+++ Interface/AddOns/NDui/Modules/Tooltip/TooltipID.lua
@@ -28,7 +28,7 @@ function TT:AddLineForID(id, linkType, noadd)
 
 	if linkType == types.item then
 		local bagCount = GetItemCount(id)
-		local bankCount = GetItemCount(id, true) - GetItemCount(id)
+		local bankCount = GetItemCount(id, true) - bagCount
 		local itemStackCount = select(8, GetItemInfo(id))
 		if bankCount > 0 then
 			self:AddDoubleLine(BAGSLOT.."/"..BANK..":", DB.InfoColor..bagCount.."/"..bankCount)
