5ed96f4bbb1dee4759a62f4c6bcdb29964e9281b
siweia <siwei1990@gmail.com>
2020-03-13 17:21:40 +0800

    Prevent taint from custom BAG_ITEM_QUALITY_COLORS, need reviewed

--- Interface/AddOns/NDui/Core/Database.lua
+++ Interface/AddOns/NDui/Core/Database.lua
@@ -33,9 +33,14 @@ DB.r, DB.g, DB.b = DB.ClassColors[DB.MyClass].r, DB.ClassColors[DB.MyClass].g, D
 DB.MyColor = format("|cff%02x%02x%02x", DB.r*255, DB.g*255, DB.b*255)
 DB.InfoColor = "|cff99ccff" --.6,.8,1
 DB.GreyColor = "|cff7b8489"
-BAG_ITEM_QUALITY_COLORS[-1] = {r = 0, g = 0, b = 0}
-BAG_ITEM_QUALITY_COLORS[LE_ITEM_QUALITY_POOR] = {r = .61, g = .61, b = .61}
-BAG_ITEM_QUALITY_COLORS[LE_ITEM_QUALITY_COMMON] = {r = 0, g = 0, b = 0}
+DB.QualityColors = {}
+local qualityColors = C.QualityColors
+for index, value in pairs(qualityColors) do
+	DB.QualityColors[index] = {r = value.r, g = value.g, b = value.b}
+end
+DB.QualityColors[-1] = {r = 0, g = 0, b = 0}
+DB.QualityColors[LE_ITEM_QUALITY_POOR] = {r = .61, g = .61, b = .61}
+DB.QualityColors[LE_ITEM_QUALITY_COMMON] = {r = 0, g = 0, b = 0}
 
 -- Fonts
 DB.Font = {STANDARD_TEXT_FONT, 12, "OUTLINE"}
