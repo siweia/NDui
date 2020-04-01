df8e830c5039de4996507999137f6b2ce3546526
siweia <siwei1990@gmail.com>
2020-04-01 19:29:37 +0800

    Don't ignore case #62

---
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/bar3.lua
@@ -0,0 +1,72 @@
+local _, ns = ...
+local B, C, L, DB = unpack(ns)
+local Bar = B:GetModule("Actionbar")
+local cfg = C.bars.bar3
+
+function Bar:CreateBar3()
+	local padding, margin = 2, 2
+	local num = NUM_ACTIONBAR_BUTTONS
+	local buttonList = {}
+	local layout = NDuiDB["Actionbar"]["Style"]
+	if layout > 3 then cfg = C.bars.bar2 end
+
+	--create the frame to hold the buttons
+	local frame = CreateFrame("Frame", "NDui_ActionBar3", UIParent, "SecureHandlerStateTemplate")
+	if layout == 4 then
+		frame:SetWidth(num*cfg.size + (num-1)*margin + 2*padding)
+		frame:SetHeight(cfg.size + 2*padding)
+		frame.Pos = {"BOTTOM", UIParent, "BOTTOM", 0, 96}
+	elseif layout == 5 then
+		frame:SetWidth(6*cfg.size + 5*margin + 2*padding)
+		frame:SetHeight(2*cfg.size + margin + 2*padding)
+		frame.Pos = {"BOTTOM", UIParent, "BOTTOM", 216, 24}
+	else
+		frame:SetWidth(19*cfg.size + 17*margin + 2*padding)
+		frame:SetHeight(2*cfg.size + margin + 2*padding)
+		frame.Pos = {"BOTTOM", UIParent, "BOTTOM", 0, 26}
+	end
+
+	--move the buttons into position and reparent them
+	MultiBarBottomRight:SetParent(frame)
+	MultiBarBottomRight:EnableMouse(false)
+
+	for i = 1, num do
+		local button = _G["MultiBarBottomRightButton"..i]
+		table.insert(buttonList, button) --add the button object to the list
+		button:SetSize(cfg.size, cfg.size)
+		button:ClearAllPoints()
+		if i == 1 then
+			if layout == 4 then
+				button:SetPoint("LEFT", frame, padding, 0)
+			else
+				button:SetPoint("TOPLEFT", frame, padding, -padding)
+			end
+		elseif (i == 4 and layout < 4) or (i == 7 and layout == 5) then
+			local previous = _G["MultiBarBottomRightButton1"]
+			button:SetPoint("TOP", previous, "BOTTOM", 0, -margin)
+		elseif i == 7 and layout < 4 then
+			local previous = _G["MultiBarBottomRightButton3"]
+			button:SetPoint("LEFT", previous, "RIGHT", 13*cfg.size+13*margin, 0)
+		elseif i == 10 and layout < 4 then
+			local previous = _G["MultiBarBottomRightButton7"]
+			button:SetPoint("TOP", previous, "BOTTOM", 0, -margin)
+		else
+			local previous = _G["MultiBarBottomRightButton"..i-1]
+			button:SetPoint("LEFT", previous, "RIGHT", margin, 0)
+		end
+	end
+
+	--show/hide the frame on a given state driver
+	frame.frameVisibility = "[petbattle][overridebar][vehicleui][possessbar,@vehicle,exists][shapeshift] hide; show"
+	RegisterStateDriver(frame, "visibility", frame.frameVisibility)
+
+	--create drag frame and drag functionality
+	if C.bars.userplaced then
+		frame.mover = B.Mover(frame, SHOW_MULTIBAR2_TEXT, "Bar3", frame.Pos)
+	end
+
+	--create the mouseover functionality
+	if cfg.fader then
+		Bar.CreateButtonFrameFader(frame, buttonList, cfg.fader)
+	end
+end
---
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/leave_vehicle.lua
@@ -0,0 +1,58 @@
+local _, ns = ...
+local B, C, L, DB = unpack(ns)
+local Bar = B:GetModule("Actionbar")
+local cfg = C.bars.leave_vehicle
+
+function Bar:CreateLeaveVehicle()
+	local padding, margin = 10, 5
+	local num = 1
+	local buttonList = {}
+
+	--create the frame to hold the buttons
+	local frame = CreateFrame("Frame", "NDui_ActionBarExit", UIParent, "SecureHandlerStateTemplate")
+	frame:SetWidth(num*cfg.size + (num-1)*margin + 2*padding)
+	frame:SetHeight(cfg.size + 2*padding)
+	if NDuiDB["Actionbar"]["Style"] == 3 then
+		frame.Pos = {"BOTTOM", UIParent, "BOTTOM", 0, 130}
+	else
+		frame.Pos = {"BOTTOM", UIParent, "BOTTOM", 320, 100}
+	end
+
+	--the button
+	local button = CreateFrame("CheckButton", "NDui_LeaveVehicleButton", frame, "ActionButtonTemplate, SecureHandlerClickTemplate")
+	table.insert(buttonList, button) --add the button object to the list
+	button:SetSize(cfg.size, cfg.size)
+	button:SetPoint("BOTTOMLEFT", frame, padding, padding)
+	button:RegisterForClicks("AnyUp")
+	button.icon:SetTexture("INTERFACE\\VEHICLES\\UI-Vehicles-Button-Exit-Up")
+	button.icon:SetTexCoord(.216, .784, .216, .784)
+	button:SetNormalTexture(nil)
+	button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+	button:GetPushedTexture():SetTexture(DB.textures.pushed)
+	B.CreateBDFrame(button, nil, true)
+
+	local function onClick(self)
+		if UnitOnTaxi("player") then TaxiRequestEarlyLanding() else VehicleExit() end
+		self:SetChecked(false)
+	end
+	button:SetScript("OnClick", onClick)
+	button:SetScript("OnEnter", MainMenuBarVehicleLeaveButton_OnEnter)
+	button:SetScript("OnLeave", B.HideTooltip)
+
+	--frame visibility
+	frame.frameVisibility = "[canexitvehicle]c;[mounted]m;n"
+	RegisterStateDriver(frame, "exit", frame.frameVisibility)
+
+	frame:SetAttribute("_onstate-exit", [[ if CanExitVehicle() then self:Show() else self:Hide() end ]])
+	if not CanExitVehicle() then frame:Hide() end
+
+	--create drag frame and drag functionality
+	if C.bars.userplaced then
+		frame.mover = B.Mover(frame, L["LeaveVehicle"], "LeaveVehicle", frame.Pos)
+	end
+
+	--create the mouseover functionality
+	if cfg.fader then
+		Bar.CreateButtonFrameFader(frame, buttonList, cfg.fader)
+	end
+end
