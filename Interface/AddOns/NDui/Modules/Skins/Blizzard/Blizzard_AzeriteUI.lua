b82ea9c920e4be7e0d35c276fb031bbacb63c820
siweia <siwei1990@gmail.com>
2020-04-01 19:44:06 +0800

    Test

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_AzeriteUI.lua
+++
@@ -1,8 +0,0 @@
-local _, ns = ...
-local B, C, L, DB = unpack(ns)
-
-C.themes["Blizzard_AzeriteUI"] = function()
-	B.ReskinPortraitFrame(AzeriteEmpoweredItemUI)
-	AzeriteEmpoweredItemUIBg:Hide()
-	AzeriteEmpoweredItemUI.ClipFrame.BackgroundFrame.Bg:Hide()
-end
