3b7eb25c212f7d47a1d51923124e0289506b7bce
siweia <siwei1990@outlook.com>
2020-05-08 19:58:15 +0800

    Revert

---
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/Hide_blizzart.lua
@@ -0,0 +1,81 @@
+local _, ns = ...
+local B, C, L, DB = unpack(ns)
+local Bar = B:GetModule("Actionbar")
+
+local _G = _G
+local next, tonumber = next, tonumber
+local ACTION_BUTTON_SHOW_GRID_REASON_CVAR = ACTION_BUTTON_SHOW_GRID_REASON_CVAR
+
+local scripts = {
+	"OnShow", "OnHide", "OnEvent", "OnEnter", "OnLeave", "OnUpdate", "OnValueChanged", "OnClick", "OnMouseDown", "OnMouseUp",
+}
+
+local framesToHide = {
+	MainMenuBar, OverrideActionBar,
+}
+
+local framesToDisable = {
+	MainMenuBar,
+	MicroButtonAndBagsBar, MainMenuBarArtFrame, StatusTrackingBarManager,
+	ActionBarDownButton, ActionBarUpButton, MainMenuBarVehicleLeaveButton,
+	OverrideActionBar,
+	OverrideActionBarExpBar, OverrideActionBarHealthBar, OverrideActionBarPowerBar, OverrideActionBarPitchFrame,
+}
+
+local function DisableAllScripts(frame)
+	for _, script in next, scripts do
+		if frame:HasScript(script) then
+			frame:SetScript(script, nil)
+		end
+	end
+end
+
+function Bar:HideBlizz()
+	MainMenuBar:SetMovable(true)
+	MainMenuBar:SetUserPlaced(true)
+	MainMenuBar.ignoreFramePositionManager = true
+	MainMenuBar:SetAttribute("ignoreFramePositionManager", true)
+
+	for _, frame in next, framesToHide do
+		frame:SetParent(B.HiddenFrame)
+	end
+
+	for _, frame in next, framesToDisable do
+		frame:UnregisterAllEvents()
+		DisableAllScripts(frame)
+	end
+
+	-- Update button grid
+	local function buttonShowGrid(name, showgrid)
+		for i = 1, 12 do
+			local button = _G[name..i]
+			button:SetAttribute("showgrid", showgrid)
+			ActionButton_ShowGrid(button, ACTION_BUTTON_SHOW_GRID_REASON_CVAR)
+		end
+	end
+
+	local updateAfterCombat
+	local function ToggleButtonGrid()
+		if InCombatLockdown() then
+			updateAfterCombat = true
+			B:RegisterEvent("PLAYER_REGEN_ENABLED", ToggleButtonGrid)
+		else
+			local showgrid = tonumber(GetCVar("alwaysShowActionBars"))
+			buttonShowGrid("ActionButton", showgrid)
+			buttonShowGrid("MultiBarBottomRightButton", showgrid)
+			if updateAfterCombat then
+				B:UnregisterEvent("PLAYER_REGEN_ENABLED", ToggleButtonGrid)
+				updateAfterCombat = false
+			end
+		end
+	end
+	hooksecurefunc("MultiActionBar_UpdateGridVisibility", ToggleButtonGrid)
+
+	-- Update token panel
+	local function updateToken()
+		TokenFrame_LoadUI()
+		TokenFrame_Update()
+		BackpackTokenFrame_Update()
+	end
+	B:RegisterEvent("CURRENCY_DISPLAY_UPDATE", updateToken)
+end
