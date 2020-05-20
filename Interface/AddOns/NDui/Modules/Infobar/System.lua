6f422d0f6e5215cffbcaafd4827b87e9088d8779
siweia <siwei1990@outlook.com>
2020-05-20 21:29:02 +0800

    Combine memory and cpu usage

--- Interface/AddOns/NDui/Modules/Infobar/System.lua
+++ Interface/AddOns/NDui/Modules/Infobar/System.lua
@@ -4,22 +4,44 @@ if not C.Infobar.System then return end
 
 local module = B:GetModule("Infobar")
 local info = module:RegisterInfobar("System", C.Infobar.SystemPos)
-local min, max, floor, mod, format, sort, select = math.min, math.max, math.floor, mod, string.format, table.sort, select
-local GetFramerate, GetNetStats, GetTime, GetCVarBool, SetCVar = GetFramerate, GetNetStats, GetTime, GetCVarBool, SetCVar
-local GetNumAddOns, GetAddOnInfo, IsShiftKeyDown, IsAddOnLoaded = GetNumAddOns, GetAddOnInfo, IsShiftKeyDown, IsAddOnLoaded
-local UpdateAddOnCPUUsage, GetAddOnCPUUsage, ResetCPUUsage = UpdateAddOnCPUUsage, GetAddOnCPUUsage, ResetCPUUsage
-local VIDEO_OPTIONS_ENABLED, VIDEO_OPTIONS_DISABLED, FRAMERATE_LABEL = VIDEO_OPTIONS_ENABLED, VIDEO_OPTIONS_DISABLED, FRAMERATE_LABEL
 
-local usageTable, showMode, entered = {}, 0
+local format, floor, wipe, select, sort, min, max = format, floor, wipe, select, sort, min, max
+local GetFramerate, GetTime = GetFramerate, GetTime
+local GetNumAddOns, GetAddOnInfo, GetCVarBool, SetCVar = GetNumAddOns, GetAddOnInfo, GetCVarBool, SetCVar
+local UpdateAddOnCPUUsage, GetAddOnCPUUsage = UpdateAddOnCPUUsage, GetAddOnCPUUsage
+local UpdateAddOnMemoryUsage, GetAddOnMemoryUsage = UpdateAddOnMemoryUsage, GetAddOnMemoryUsage
+local IsShiftKeyDown, IsAddOnLoaded = IsShiftKeyDown, IsAddOnLoaded
+local ResetCPUUsage, collectgarbage, gcinfo = ResetCPUUsage, collectgarbage, gcinfo
+local VIDEO_OPTIONS_ENABLED, VIDEO_OPTIONS_DISABLED = VIDEO_OPTIONS_ENABLED, VIDEO_OPTIONS_DISABLED
+
+local usageTable, memoryTable = {}, {}
 local usageString = "%.3f ms"
+local scriptProfileStatus = GetCVarBool("scriptProfile")
+local showMemoryUsage, entered
 
-local function colorLatency(latency)
-	if latency < 250 then
-		return "|cff0CD809"..latency
-	elseif latency < 500 then
-		return "|cffE8DA0F"..latency
+local function formatMemory(value)
+	if value > 1024 then
+		return format("%.1f mb", value / 1024)
 	else
-		return "|cffD80909"..latency
+		return format("%.0f kb", value)
+	end
+end
+
+local function memoryColor(value, times)
+	if not times then times = 1 end
+
+	if value <= 1024*times then
+		return 0, 1, 0
+	elseif value <= 2048*times then
+		return .75, 1, 0
+	elseif value <= 4096*times then
+		return 1, 1, 0
+	elseif value <= 8192*times then
+		return 1, .75, 0
+	elseif value <= 16384*times then
+		return 1, .5, 0
+	else
+		return 1, .1, 0
 	end
 end
 
@@ -38,40 +60,15 @@ local function setFrameRate(self)
 	self.text:SetText(L["FPS"]..": "..colorFPS(fps))
 end
 
-local function setLatency(self)
-	local _, _, latencyHome, latencyWorld = GetNetStats()
-	local latency = max(latencyHome, latencyWorld)
-	self.text:SetText(L["Latency"]..": "..colorLatency(latency))
-end
-
-info.onUpdate = function(self, elapsed)
-	self.timer = (self.timer or 0) + elapsed
-	if self.timer > 1 then
-		if NDuiADB["SystemInfoType"] == 1 then
-			setFrameRate(self)
-		elseif NDuiADB["SystemInfoType"] == 2 then
-			setLatency(self)
-		else
-			showMode = mod(showMode + 1, 10)
-			if showMode > 4 then
-				setFrameRate(self)
-			else
-				setLatency(self)
-			end
-		end
-		if entered then self:onEnter() end
-
-		self.timer = 0
-	end
-end
-
-local function updateUsageTable()
+local function updateAddOnTable()
 	local numAddons = GetNumAddOns()
 	if numAddons == #usageTable then return end
 
 	wipe(usageTable)
+	wipe(memoryTable)
 	for i = 1, numAddons do
 		usageTable[i] = {i, select(2, GetAddOnInfo(i)), 0}
+		memoryTable[i] = {i, select(2, GetAddOnInfo(i)), 0}
 	end
 end
 
@@ -95,26 +92,76 @@ local function updateUsage()
 	return total
 end
 
-local systemText = {
-	[0] = "|cff55ff55"..L["Rotation"],
-	[1] = "|cffff9900"..L["FPS"],
-	[2] = "|cffffff55"..L["Latency"]
-}
+local function updateMemory()
+	UpdateAddOnMemoryUsage()
+
+	local total = 0
+	for i = 1, #memoryTable do
+		local value = memoryTable[i]
+		value[3] = GetAddOnMemoryUsage(value[1])
+		total = total + value[3]
+	end
+	sort(memoryTable, sortUsage)
+
+	return total
+end
+
+info.onUpdate = function(self, elapsed)
+	-- Framerate
+	self.timer = (self.timer or 0) + elapsed
+	if self.timer > 1 then
+		setFrameRate(self)
+		if entered then self:onEnter() end
+		self.timer = 0
+	end
+
+	-- Rebuild addon list
+	self.elapsed = (self.elapsed or 10) + elapsed
+	if self.elapsed > 10 then
+		updateAddOnTable()
+		self.elapsed = 0
+	end
+end
 
 info.onEnter = function(self)
 	entered = true
+
+	local totalMemory = updateMemory()
+	local maxAddOns = C.Infobar.MaxAddOns
+	local isShiftKeyDown = IsShiftKeyDown()
+
 	GameTooltip:SetOwner(self, "ANCHOR_BOTTOM", 0, -15)
 	GameTooltip:ClearLines()
-	GameTooltip:AddLine(L["System"], 0,.6,1)
+	GameTooltip:AddDoubleLine(L["System"], formatMemory(totalMemory), 0,.6,1, .6,.8,1)
 	GameTooltip:AddLine(" ")
 
-	local scriptProfile = GetCVarBool("scriptProfile")
-	if scriptProfile then
-		updateUsageTable()
+	if showMemoryUsage or not scriptProfileStatus then
+		local maxShown = isShiftKeyDown and #memoryTable or min(maxAddOns, #memoryTable)
+		local numEnabled = 0
+		for i = 1, #memoryTable do
+			local value = memoryTable[i]
+			if value and IsAddOnLoaded(value[1]) then
+				numEnabled = numEnabled + 1
+				if numEnabled <= maxShown then
+					GameTooltip:AddDoubleLine(value[2], formatMemory(value[3]), 1,1,1, memoryColor(value[3], 5))
+				end
+			end
+		end
+	
+		if not isShiftKeyDown and (numEnabled > maxAddOns) then
+			local hiddenMemory = 0
+			for i = (maxAddOns + 1), numEnabled do
+				hiddenMemory = hiddenMemory + memoryTable[i][3]
+			end
+			GameTooltip:AddDoubleLine(format("%d %s (%s)", numEnabled - maxAddOns, L["Hidden"], L["Hold Shift"]), formatMemory(hiddenMemory), .6,.8,1, .6,.8,1)
+		end
+
+		GameTooltip:AddLine(" ")
+		GameTooltip:AddDoubleLine(L["Default UI Memory Usage:"], formatMemory(gcinfo() - totalMemory), .6,.8,1, 1,1,1)
+		GameTooltip:AddDoubleLine(L["Total Memory Usage:"], formatMemory(collectgarbage("count")), .6,.8,1, 1,1,1)
+	else
 		local totalCPU = updateUsage()
 		if totalCPU > 0 then
-			local maxAddOns = C.Infobar.MaxAddOns
-			local isShiftKeyDown = IsShiftKeyDown()
 			local maxShown = isShiftKeyDown and #usageTable or min(maxAddOns, #usageTable)
 			local numEnabled = 0
 			for i = 1, #usageTable do
@@ -136,21 +183,15 @@ info.onEnter = function(self)
 				end
 				GameTooltip:AddDoubleLine(format("%d %s (%s)", numEnabled - maxAddOns, L["Hidden"], L["Hold Shift"]), format(usageString, hiddenUsage), .6,.8,1, .6,.8,1)
 			end
-			GameTooltip:AddLine(" ")
 		end
 	end
 
-	local _, _, latencyHome, latencyWorld = GetNetStats()
-	local fps = floor(GetFramerate())
-	GameTooltip:AddDoubleLine(L["Home Latency"]..":", colorLatency(latencyHome).."|r MS", .6,.8,1, 1,1,1)
-	GameTooltip:AddDoubleLine(L["World Latency"]..":", colorLatency(latencyWorld).."|r MS", .6,.8,1, 1,1,1)
-	GameTooltip:AddDoubleLine(FRAMERATE_LABEL, colorFPS(fps).."|r FPS", .6,.8,1, 1,1,1)
 	GameTooltip:AddDoubleLine(" ", DB.LineString)
-	if scriptProfile then
-		GameTooltip:AddDoubleLine(" ", DB.LeftButton..L["ResetCPUUsage"].." ", 1,1,1, .6,.8,1)
+	GameTooltip:AddDoubleLine(" ", DB.LeftButton..L["Collect Memory"].." ", 1,1,1, .6,.8,1)
+	if scriptProfileStatus then
+		GameTooltip:AddDoubleLine(" ", DB.RightButton..L["SwitchSystemInfo"].." ", 1,1,1, .6,.8,1)
 	end
-	GameTooltip:AddDoubleLine(" ", DB.RightButton..L["SystemInfoType"]..": "..systemText[NDuiADB["SystemInfoType"]].." ", 1,1,1, .6,.8,1)
-	GameTooltip:AddDoubleLine(" ", DB.ScrollButton..L["CPU Usage"]..": "..(scriptProfile and "|cff55ff55"..VIDEO_OPTIONS_ENABLED or "|cffff5555"..VIDEO_OPTIONS_DISABLED).." ", 1,1,1, .6,.8,1)
+	GameTooltip:AddDoubleLine(" ", DB.ScrollButton..L["CPU Usage"]..": "..(GetCVarBool("scriptProfile") and "|cff55ff55"..VIDEO_OPTIONS_ENABLED or "|cffff5555"..VIDEO_OPTIONS_DISABLED).." ", 1,1,1, .6,.8,1)
 	GameTooltip:Show()
 end
 
@@ -167,14 +208,20 @@ StaticPopupDialogs["CPUUSAGE"] = {
 	whileDead = 1,
 }
 
-local status = GetCVarBool("scriptProfile")
 info.onMouseUp = function(self, btn)
 	local scriptProfile = GetCVarBool("scriptProfile")
-	if btn == "LeftButton" and scriptProfile then
-		ResetCPUUsage()
-		module.loginTime = GetTime()
-	elseif btn == "RightButton" then
-		NDuiADB["SystemInfoType"] = mod(NDuiADB["SystemInfoType"] + 1, 3)
+	if btn == "LeftButton" then
+		if scriptProfileStatus then
+			ResetCPUUsage()
+			module.loginTime = GetTime()
+		end
+		local before = gcinfo()
+		collectgarbage("collect")
+		print(format("|cff66C6FF%s:|r %s", L["Collect Memory"], formatMemory(before - gcinfo())))
+		self:onEnter()
+	elseif btn == "RightButton" and scriptProfileStatus then
+		showMemoryUsage = not showMemoryUsage
+		self:onEnter()
 	elseif btn == "MiddleButton" then
 		if scriptProfile then
 			SetCVar("scriptProfile", 0)
@@ -182,11 +229,11 @@ info.onMouseUp = function(self, btn)
 			SetCVar("scriptProfile", 1)
 		end
 
-		if GetCVarBool("scriptProfile") == status then
+		if GetCVarBool("scriptProfile") == scriptProfileStatus then
 			StaticPopup_Hide("CPUUSAGE")
 		else
 			StaticPopup_Show("CPUUSAGE")
 		end
+		self:onEnter()
 	end
-	self:onEnter()
 end
