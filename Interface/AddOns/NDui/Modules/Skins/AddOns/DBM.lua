c71801bb816d8e7b9b4be1f849be4df31c47d227
siweia <siwei1990@outlook.com>
2020-05-24 17:38:32 +0800

    DBM option nil check

--- Interface/AddOns/NDui/Modules/Skins/AddOns/DBM.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/DBM.lua
@@ -160,12 +160,13 @@ function S:DBMSkin()
 	if not DBM_AllSavedOptions["Default"] then DBM_AllSavedOptions["Default"] = {} end
 	DBM_AllSavedOptions["Default"]["BlockVersionUpdateNotice"] = true
 	DBM_AllSavedOptions["Default"]["EventSoundVictory"] = "None"
-	DBT_AllPersistentOptions["Default"]["DBM"].BarYOffset = 15
-	DBT_AllPersistentOptions["Default"]["DBM"].HugeBarYOffset = 15
-	DBT_AllPersistentOptions["Default"]["DBM"].ExpandUpwards = true
-	DBT_AllPersistentOptions["Default"]["DBM"].ExpandUpwardsLarge = true
 	if IsAddOnLoaded("DBM-VPYike") then
 		DBM_AllSavedOptions["Default"]["CountdownVoice"] = "VP:Yike"
 		DBM_AllSavedOptions["Default"]["ChosenVoicePack"] = "Yike"
 	end
+	if not DBT_AllPersistentOptions["Default"] then DBT_AllPersistentOptions["Default"] = {} end
+	DBT_AllPersistentOptions["Default"]["DBM"].BarYOffset = 15
+	DBT_AllPersistentOptions["Default"]["DBM"].HugeBarYOffset = 15
+	DBT_AllPersistentOptions["Default"]["DBM"].ExpandUpwards = true
+	DBT_AllPersistentOptions["Default"]["DBM"].ExpandUpwardsLarge = true
 end
