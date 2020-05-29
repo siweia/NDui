4b2b72a93c3dd4f36e484911e4b9a16f3a2f5a00
siweia <siwei1990@outlook.com>
2020-05-30 01:22:13 +0800

    Smooth on objective tracker

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
@@ -63,6 +63,7 @@ tinsert(C.defaultThemes, function()
 		bar:SetStatusBarTexture(DB.normTex)
 		bar:SetStatusBarColor(r, g, b)
 		bar.bg = B.SetBD(bar)
+		B:SmoothBar(bar)
 	end
 
 	local function reskinProgressbar(_, _, line)
