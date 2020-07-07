961b1558d851c16c96f051f92448e0bddec2b335
siweia <siwei1990@outlook.com>
2020-07-08 01:45:12 +0800

    Cleanup

--- Interface/AddOns/NDui/Core/ExtraGUI.lua
+++ Interface/AddOns/NDui/Core/ExtraGUI.lua
@@ -740,7 +740,7 @@ local function sliderValueChanged(self, v)
 	self.__update()
 end
 
-local function createOptionSlider(parent, title, minV, maxV, x, y, value, func, defaultV)
+local function createOptionSlider(parent, title, minV, maxV, defaultV, x, y, value, func)
 	local slider = B.CreateSlider(parent, title, minV, maxV, 1, x, y)
 	slider:SetValue(NDuiDB["UFs"][value])
 	slider.value:SetText(NDuiDB["UFs"][value])
@@ -787,11 +787,11 @@ function G:SetupUnitFrame(parent)
 
 	local function createOptionGroup(parent, title, offset, value, func)
 		createOptionTitle(parent, title, offset)
-		createOptionSlider(parent, L["Health Width"], sliderRange[value][1], sliderRange[value][2], 30, offset-60, value.."Width", func, defaultValue[value][1])
-		createOptionSlider(parent, L["Health Height"], 15, 50, 30, offset-130, value.."Height", func, defaultValue[value][2])
-		createOptionSlider(parent, L["Power Height"], 2, 30, 30, offset-200, value.."PowerHeight", func, defaultValue[value][3])
+		createOptionSlider(parent, L["Health Width"], sliderRange[value][1], sliderRange[value][2], defaultValue[value][1], 30, offset-60, value.."Width", func)
+		createOptionSlider(parent, L["Health Height"], 15, 50, defaultValue[value][2], 30, offset-130, value.."Height", func)
+		createOptionSlider(parent, L["Power Height"], 2, 30, defaultValue[value][3], 30, offset-200, value.."PowerHeight", func)
 		if defaultValue[value][4] then
-			createOptionSlider(parent, L["Power Offset"], -20, 20, 30, offset-270, value.."PowerOffset", func, defaultValue[value][4])
+			createOptionSlider(parent, L["Power Offset"], -20, 20, defaultValue[value][4], 30, offset-270, value.."PowerOffset", func)
 		end
 	end
 
@@ -851,9 +851,9 @@ function G:SetupRaidFrame(parent)
 
 	local function createOptionGroup(parent, title, offset, value, func)
 		createOptionTitle(parent, title, offset)
-		createOptionSlider(parent, L["Health Width"], minRange[value][1], 200, 30, offset-60, value.."Width", func, defaultValue[value][1])
-		createOptionSlider(parent, L["Health Height"], minRange[value][2], 60, 30, offset-130, value.."Height", func, defaultValue[value][2])
-		createOptionSlider(parent, L["Power Height"], 2, 30, 30, offset-200, value.."PowerHeight", func, defaultValue[value][3])
+		createOptionSlider(parent, L["Health Width"], minRange[value][1], 200, defaultValue[value][1], 30, offset-60, value.."Width", func)
+		createOptionSlider(parent, L["Health Height"], minRange[value][2], 60, defaultValue[value][2], 30, offset-130, value.."Height", func)
+		createOptionSlider(parent, L["Power Height"], 2, 30, defaultValue[value][3], 30, offset-200, value.."PowerHeight", func)
 	end
 
 	local function resizeRaidFrame()
@@ -875,7 +875,7 @@ function G:SetupRaidFrame(parent)
 		end
 	end
 	createOptionGroup(scroll.child, L["RaidFrame"], -10, "Raid", resizeRaidFrame)
-	createOptionSlider(scroll.child, "|cff00cc4c"..L["SimpleMode Scale"], 8, 15, 30, -280, "SimpleRaidScale", resizeRaidFrame, 10)
+	createOptionSlider(scroll.child, "|cff00cc4c"..L["SimpleMode Scale"], 8, 15, 10, 30, -280, "SimpleRaidScale", resizeRaidFrame)
 
 	local function resizePartyFrame()
 		for _, frame in pairs(ns.oUF.objects) do
@@ -922,8 +922,8 @@ function G:SetupCastbar(parent)
 
 	local function createOptionGroup(parent, title, offset, value, func)
 		createOptionTitle(parent, title, offset)
-		createOptionSlider(parent, L["Castbar Width"], 200, 400, 30, offset-60, value.."CBWidth", func, defaultValue[value][1])
-		createOptionSlider(parent, L["Castbar Height"], 10, 50, 30, offset-130, value.."CBHeight", func, defaultValue[value][2])
+		createOptionSlider(parent, L["Castbar Width"], 200, 400, defaultValue[value][1], 30, offset-60, value.."CBWidth", func)
+		createOptionSlider(parent, L["Castbar Height"], 10, 50, defaultValue[value][2], 30, offset-130, value.."CBHeight", func)
 	end
 
 	local function updatePlayerCastbar()
