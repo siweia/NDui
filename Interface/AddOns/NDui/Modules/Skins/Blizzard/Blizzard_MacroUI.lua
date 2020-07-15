b2f9c23665abfa8d5d8325c9da3240f9589b0289
siweia <siwei1990@outlook.com>
2020-07-16 01:40:50 +0800

    Skins: revamp macro frame

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_MacroUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_MacroUI.lua
@@ -6,70 +6,50 @@ C.themes["Blizzard_MacroUI"] = function()
 	B.StripTextures(MacroFrameTab1)
 	B.StripTextures(MacroFrameTab2)
 
+	B.StripTextures(MacroPopupFrame)
 	B.StripTextures(MacroPopupFrame.BorderBox)
 	B.StripTextures(MacroPopupScrollFrame)
-	MacroPopupFrame:GetRegions():Hide()
-	MacroPopupNameLeft:Hide()
-	MacroPopupNameMiddle:Hide()
-	MacroPopupNameRight:Hide()
 	MacroFrameTextBackground:SetBackdrop(nil)
-	select(2, MacroFrameSelectedMacroButton:GetRegions()):Hide()
-	MacroFrameSelectedMacroBackground:SetAlpha(0)
-	MacroButtonScrollFrameTop:Hide()
-	MacroButtonScrollFrameBottom:Hide()
-	MacroButtonScrollFrameMiddle:Hide()
 
-	MacroFrameSelectedMacroButton:SetPoint("TOPLEFT", MacroFrameSelectedMacroBackground, "TOPLEFT", 12, -16)
-	MacroFrameSelectedMacroButtonIcon:SetInside()
-	MacroFrameSelectedMacroButtonIcon:SetTexCoord(unpack(DB.TexCoord))
-
-	MacroPopupFrame:HookScript("OnShow", function(self)
-		self:SetPoint("TOPLEFT", MacroFrame, "TOPRIGHT", 3, 0)
-	end)
 	MacroPopupFrame:SetHeight(525)
 	MacroNewButton:ClearAllPoints()
 	MacroNewButton:SetPoint("RIGHT", MacroExitButton, "LEFT", -1, 0)
 
-	for i = 1, MAX_ACCOUNT_MACROS do
-		local bu = _G["MacroButton"..i]
-		local ic = _G["MacroButton"..i.."Icon"]
+	local function reskinMacroButton(button)
+		if button.styled then return end
 
-		bu:SetCheckedTexture(DB.textures.pushed)
-		select(2, bu:GetRegions()):Hide()
-		local hl = bu:GetHighlightTexture()
+		button:DisableDrawLayer("BACKGROUND")
+		button:SetCheckedTexture(DB.textures.pushed)
+		local hl = button:GetHighlightTexture()
 		hl:SetColorTexture(1, 1, 1, .25)
+		hl:SetInside()
 
-		ic:SetTexCoord(unpack(DB.TexCoord))
-		ic:SetAllPoints()
-		B.CreateBDFrame(bu, .25)
+		local icon = _G[button:GetName().."Icon"]
+		icon:SetTexCoord(unpack(DB.TexCoord))
+		icon:SetInside()
+		B.CreateBDFrame(icon, .25)
+
+		button.styled = true
 	end
 
-	MacroPopupFrame:HookScript("OnShow", function()
+	reskinMacroButton(MacroFrameSelectedMacroButton)
+
+	for i = 1, MAX_ACCOUNT_MACROS do
+		reskinMacroButton(_G["MacroButton"..i])
+	end
+
+	MacroPopupFrame:HookScript("OnShow", function(self)
 		for i = 1, NUM_MACRO_ICONS_SHOWN do
-			local bu = _G["MacroPopupButton"..i]
-			local ic = _G["MacroPopupButton"..i.."Icon"]
-
-			if not bu.styled then
-				bu:SetCheckedTexture(DB.textures.pushed)
-				select(2, bu:GetRegions()):Hide()
-				local hl = bu:GetHighlightTexture()
-				hl:SetColorTexture(1, 1, 1, .25)
-				hl:SetAllPoints(ic)
-
-				ic:SetInside()
-				ic:SetTexCoord(unpack(DB.TexCoord))
-				B.CreateBD(bu, .25)
-
-				bu.styled = true
-			end
+			reskinMacroButton(_G["MacroPopupButton"..i])
 		end
+		self:SetPoint("TOPLEFT", MacroFrame, "TOPRIGHT", 3, 0)
 	end)
 
 	B.ReskinPortraitFrame(MacroFrame)
 	B.CreateBDFrame(MacroFrameScrollFrame, .25)
 	B.SetBD(MacroPopupFrame)
+	MacroPopupEditBox:DisableDrawLayer("BACKGROUND")
 	B.ReskinInput(MacroPopupEditBox)
-	B.CreateBD(MacroFrameSelectedMacroButton, .25)
 	B.Reskin(MacroDeleteButton)
 	B.Reskin(MacroNewButton)
 	B.Reskin(MacroExitButton)
