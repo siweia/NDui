44df1b425bf995f9b7c1b8fa3f52ffa851944829
siweia <siwei1990@outlook.com>
2020-07-19 19:23:52 +0800

    Skins: PlayerChoiceUI

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Contribution.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Contribution.lua
@@ -28,30 +28,3 @@ C.themes["Blizzard_Contribution"] = function()
 		end
 	end)
 end
-
-C.themes["Blizzard_PlayerChoiceUI"] = function()
-	B.ReskinClose(PlayerChoiceFrame.CloseButton)
-
-	local function reskinOptionButton(self)
-		if self.button1 and not self.button1.styled then
-			B.Reskin(self.button1)
-			self.button1.styled = true
-		end
-	end
-	for i = 1, 4 do
-		local option = PlayerChoiceFrame.Options[i]
-		hooksecurefunc(option.OptionButtonsContainer, "ConfigureButtons", reskinOptionButton)
-	end
-end
-
-C.themes["Blizzard_Soulbinds"] = function()
-	B.ReskinClose(SoulbindViewer.CloseButton)
-	B.Reskin(SoulbindViewer.ActivateButton)
-end
-
-C.themes["Blizzard_CovenantPreviewUI"] = function()
-	B.ReskinClose(CovenantPreviewFrame.CloseButton)
-	B.Reskin(CovenantPreviewFrame.SelectButton)
-	CovenantPreviewFrame.InfoPanel.Description:SetTextColor(1, 1, 1)
-	CovenantPreviewFrame.InfoPanel.AbilitiesLabel:SetTextColor(1, .8, 0)
-end
