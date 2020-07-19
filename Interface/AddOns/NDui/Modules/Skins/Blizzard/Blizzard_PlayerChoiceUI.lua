de493dc0533361804359275e7ca12d2deb4fbee7
siweia <siwei1990@outlook.com>
2020-07-19 19:23:52 +0800

    Skins: PlayerChoiceUI

---
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_PlayerChoiceUI.lua
@@ -0,0 +1,118 @@
+local _, ns = ...
+local B, C, L, DB = unpack(ns)
+
+C.themes["Blizzard_PlayerChoiceUI"] = function()
+	local function forceProgressText(self)
+		if self.styled then return end
+
+		self:SetTextColor(1, 1, 1)
+		self.SetTextColor = B.Dummy
+		self.styled = true
+	end
+
+	local function reskinOptionButton(self)
+		if not self or self.styled then return end
+
+		if self.Left then self.Left:SetAlpha(0) end
+		if self.Middle then self.Middle:SetAlpha(0) end
+		if self.Right then self.Right:SetAlpha(0) end
+		if self.SetPushedTexture then self:SetPushedTexture("") end
+		self:SetHighlightTexture(DB.bdTex)
+		local hl = self:GetHighlightTexture()
+		hl:SetVertexColor(1, 1, 1, .25)
+		hl:SetInside()
+		B.CreateBD(self, 0)
+		B.CreateGradient(self)
+
+		self.styled = true
+	end
+
+	hooksecurefunc(PlayerChoiceFrame, "Update", function(self)
+		if not self.bg then
+			self.BlackBackground:SetAlpha(0)
+			self.Background:SetAlpha(0)
+			self.NineSlice:SetAlpha(0)
+			self.Title:DisableDrawLayer("BACKGROUND")
+			self.Title.Text:SetTextColor(1, .8, 0)
+			self.Title.Text:SetFontObject(SystemFont_Huge2)
+			self.BorderFrame.Header:SetAlpha(0)
+			B.CreateBDFrame(self.Title, .25)
+			B.ReskinClose(self.CloseButton)
+			self.CloseButton.Border:SetAlpha(0)
+			self.bg = B.SetBD(self)
+		end
+
+		self.CloseButton:SetPoint("TOPRIGHT", -2, -2)
+
+		for i = 1, self:GetNumOptions() do
+			local option = self.Options[i]
+			option.Header.Text:SetTextColor(1, .8, 0)
+			option.OptionText:SetTextColor(1, 1, 1)
+
+			for i = 1, option.WidgetContainer:GetNumChildren() do
+				local child = select(i, option.WidgetContainer:GetChildren())
+				if child.Text then
+					child.Text:SetTextColor(1, 1, 1)
+				end
+
+				if child.Spell then
+					if not child.Spell.bg then
+						child.Spell.Border:SetTexture("")
+						child.Spell.IconMask:Hide()
+						child.Spell.bg = B.ReskinIcon(child.Spell.Icon)
+					end
+
+					child.Spell.Text:SetTextColor(1, 1, 1)
+				end
+
+				for j = 1, child:GetNumChildren() do
+					local child2 = select(j, child:GetChildren())
+					if child2 then
+						if child2.Text then forceProgressText(child2.Text) end
+						if child2.LeadingText then forceProgressText(child2.LeadingText) end
+						if child2.Icon and not child2.Icon.bg then
+							child2.Icon.bg = B.ReskinIcon(child2.Icon)
+						end
+					end
+				end
+			end
+
+			reskinOptionButton(option.OptionButtonsContainer.button1)
+			reskinOptionButton(option.OptionButtonsContainer.button2)
+		end
+	end)
+end
+
+C.themes["Blizzard_Soulbinds"] = function()
+	B.ReskinClose(SoulbindViewer.CloseButton)
+	B.Reskin(SoulbindViewer.ActivateButton)
+end
+
+C.themes["Blizzard_CovenantPreviewUI"] = function()
+	B.Reskin(CovenantPreviewFrame.SelectButton)
+	CovenantPreviewFrame.InfoPanel.Description:SetTextColor(1, 1, 1)
+	CovenantPreviewFrame.InfoPanel.AbilitiesLabel:SetTextColor(1, .8, 0)
+
+	hooksecurefunc(CovenantPreviewFrame, "TryShow", function(self)
+		if not self.bg then
+			self.Background:SetAlpha(0)
+			self.BorderFrame:SetAlpha(0)
+			self.Title:DisableDrawLayer("BACKGROUND")
+			self.Title.Text:SetTextColor(1, .8, 0)
+			self.Title.Text:SetFontObject(SystemFont_Huge2)
+			self.ModelSceneContainer.ModelSceneBorder:SetAlpha(0)
+			B.CreateBDFrame(self.Title, .25)
+			B.ReskinClose(self.CloseButton)
+			self.bg = B.SetBD(self)
+		end
+	end)
+
+	hooksecurefunc(CovenantPreviewFrame, "SetupTextureKits", function(self, button)
+		if button.IconBorder and not button.bg then
+			button.IconBorder:SetAlpha(0)
+			button.CircleMask:Hide()
+			button.Background:SetAlpha(0)
+			button.bg = B.ReskinIcon(button.Icon)
+		end
+	end)
+end
