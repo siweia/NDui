768de85aea3f81f7013715cd57088988e18e21ff
siweia <siwei1990@outlook.com>
2020-07-22 20:13:14 +0800

    Cooldown should be inside

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -641,7 +641,7 @@ do
 
 	function B:AuraIcon(highlight)
 		self.CD = CreateFrame("Cooldown", nil, self, "CooldownFrameTemplate")
-		self.CD:SetAllPoints()
+		self.CD:SetInside()
 		self.CD:SetReverse(true)
 		B.PixelIcon(self, nil, highlight)
 		B.CreateSD(self)
