f60583431acef131a7a7e76ff229404501f0703b
siweia <siwei1990@outlook.com>
2020-07-23 20:17:53 +0800

    Some default auras in mythic dungeons

--- Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/Party-Shadowlands.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/Party-Shadowlands.lua
@@ -6,18 +6,41 @@ local TIER = 9
 local INSTANCE -- 5人本
 
 INSTANCE = 1187 -- 伤逝剧场
---module:RegisterDebuff(TIER, INSTANCE, BOSS, 306015) -- 灼烧护甲
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1183 -- 凋魂之殇
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1184 -- 塞兹仙林的迷雾
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1188 -- 彼界
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1186 -- 晋升高塔
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1185 -- 赎罪大厅
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1189 -- 赤红深渊
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
 
 INSTANCE = 1182 -- 通灵战潮
+module:RegisterDebuff(TIER, INSTANCE, 0, 209858) -- 死疽
+module:RegisterDebuff(TIER, INSTANCE, 0, 240559) -- 重伤
+module:RegisterDebuff(TIER, INSTANCE, 0, 240443) -- 爆裂
