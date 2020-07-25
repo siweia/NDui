4ea7cd5555ec31d7eb9e1e37db180bed3eda24bf
siweia <siwei1990@outlook.com>
2020-07-26 03:03:28 +0800

    Skins: soulbind viewer

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Soulbinds.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Soulbinds.lua
@@ -2,6 +2,9 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
 C.themes["Blizzard_Soulbinds"] = function()
+	B.StripTextures(SoulbindViewer)
+	SoulbindViewer.Background:SetAlpha(0)
+	B.SetBD(SoulbindViewer)
 	B.ReskinClose(SoulbindViewer.CloseButton)
 	B.Reskin(SoulbindViewer.ActivateButton)
 end
