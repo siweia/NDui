3e164e404c23d6a0738e58170c439f34f195f182
siweia <siwei1990@outlook.com>
2020-07-26 18:39:44 +0800

    Update raid debuffs

--- Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/CastleNathria.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/CastleNathria.lua
@@ -7,17 +7,24 @@ local INSTANCE = 1190 -- 纳斯利亚堡
 local BOSS
 
 BOSS = 2393 -- 啸翼
---module:RegisterDebuff(TIER, INSTANCE, BOSS, 306015) -- 灼烧护甲
 
 BOSS = 2429 -- 猎手阿尔迪莫
 
 BOSS = 2428 -- 饥饿的毁灭者
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 334228) -- 不稳定的喷发
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 329298) -- 暴食瘴气
 
 BOSS = 2422 -- 太阳之王的救赎
 
 BOSS = 2418 -- 技师赛·墨克斯
 
 BOSS = 2420 -- 伊涅瓦·暗脉女勋爵
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 325936) -- 共享认知
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 335396) -- 隐秘欲望
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 324982) -- 共受苦难
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 324983) -- 共受苦难
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 332664) -- 浓缩心能
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 325382) -- 扭曲欲望
 
 BOSS = 2426 -- 猩红议会
 
