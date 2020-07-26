0b781515826531b682a731d51d8f8c257154b684
siweia <siwei1990@outlook.com>
2020-07-26 17:55:27 +0800

    Skins: merge gm chat and survey

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_GMChatUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_GMChatUI.lua
@@ -1,6 +1,7 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+-- /run LoadAddOn'Blizzard_GMChatUI' GMChatFrame:Show()
 C.themes["Blizzard_GMChatUI"] = function()
 	local frame = _G["GMChatFrame"]
 	frame:SetClampRectInsets(0, 0, 0, 0)
@@ -48,4 +49,33 @@ C.themes["Blizzard_GMChatUI"] = function()
 	B.HideObject(frame.buttonFrame)
 end
 
--- /run LoadAddOn'Blizzard_GMChatUI' GMChatFrame:Show()
+-- /run LoadAddOn'Blizzard_GMSurveyUI' GMSurveyFrame:Show()
+C.themes["Blizzard_GMSurveyUI"] = function()
+	B.SetBD(GMSurveyFrame, nil, 0, 0, -32, 4)
+	B.StripTextures(GMSurveyCommentFrame)
+	local bg = B.CreateBDFrame(GMSurveyCommentFrame, .25)
+	bg:SetInside()
+
+	for i = 1, 15 do
+		local button = _G["GMSurveyQuestion"..i]
+		if button then
+			B.StripTextures(button)
+			local bg  = B.CreateBDFrame(button, .25)
+			bg:SetInside()
+			for _, radio in next, button.radioButtons do
+				B.ReskinRadio(radio)
+				radio:SetSize(20, 20)
+			end
+		end
+	end
+
+	B.StripTextures(GMSurveyFrame)
+	B.StripTextures(GMSurveyFrame.Header)
+	GMSurveyScrollFrameTop:SetAlpha(0)
+	GMSurveyScrollFrameMiddle:SetAlpha(0)
+	GMSurveyScrollFrameBottom:SetAlpha(0)
+	B.Reskin(GMSurveySubmitButton)
+	B.Reskin(GMSurveyCancelButton)
+	B.ReskinClose(GMSurveyCloseButton, "TOPRIGHT", GMSurveyFrame, "TOPRIGHT", -36, -4)
+	B.ReskinScroll(GMSurveyScrollFrameScrollBar)
+end
