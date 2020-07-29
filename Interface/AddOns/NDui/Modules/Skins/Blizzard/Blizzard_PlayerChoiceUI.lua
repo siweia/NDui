37c7b3927fbc9fad66862ed80648edf95e3dd141
siweia <siwei1990@outlook.com>
2020-07-30 01:55:27 +0800

    Skins: update playerchoice in differ situations

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_PlayerChoiceUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_PlayerChoiceUI.lua
@@ -1,31 +1,28 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local function WhitenProgressText(self)
+	if self.styled then return end
+
+	self:SetTextColor(1, 1, 1)
+	self.SetTextColor = B.Dummy
+	self.styled = true
+end
+
+local function ReskinFirstOptionButton(self)
+	if not self or self.__bg then return end
+
+	B.StripTextures(self, true)
+	B.Reskin(self)
+end
+
+local function ReskinSecondOptionButton(self)
+	if not self or self.__bg then return end
+
+	B.Reskin(self, nil, true)
+end
+
 C.themes["Blizzard_PlayerChoiceUI"] = function()
-	local function forceProgressText(self)
-		if self.styled then return end
-
-		self:SetTextColor(1, 1, 1)
-		self.SetTextColor = B.Dummy
-		self.styled = true
-	end
-
-	local function reskinOptionButton(self)
-		if not self or self.styled then return end
-
-		if self.Left then self.Left:SetAlpha(0) end
-		if self.Middle then self.Middle:SetAlpha(0) end
-		if self.Right then self.Right:SetAlpha(0) end
-		if self.SetPushedTexture then self:SetPushedTexture("") end
-		local bg = B.CreateBDFrame(self, 0, true)
-		self:SetHighlightTexture(DB.bdTex)
-		local hl = self:GetHighlightTexture()
-		hl:SetVertexColor(1, 1, 1, .25)
-		hl:SetInside(bg)
-
-		self.styled = true
-	end
-
 	hooksecurefunc(PlayerChoiceFrame, "Update", function(self)
 		if not self.bg then
 			self.BlackBackground:SetAlpha(0)
@@ -41,7 +38,8 @@ C.themes["Blizzard_PlayerChoiceUI"] = function()
 			self.bg = B.SetBD(self)
 		end
 
-		self.CloseButton:SetPoint("TOPRIGHT", -2, -2)
+		self.CloseButton:SetPoint("TOPRIGHT", self.bg, -2, -2)
+		self.bg:SetShown(not IsInJailersTower())
 
 		for i = 1, self:GetNumOptions() do
 			local option = self.Options[i]
@@ -67,8 +65,12 @@ C.themes["Blizzard_PlayerChoiceUI"] = function()
 				for j = 1, child:GetNumChildren() do
 					local child2 = select(j, child:GetChildren())
 					if child2 then
-						if child2.Text then forceProgressText(child2.Text) end
-						if child2.LeadingText then forceProgressText(child2.LeadingText) end
+						if child2.Text then
+							WhitenProgressText(child2.Text)
+						end
+						if child2.LeadingText then
+							WhitenProgressText(child2.LeadingText)
+						end
 						if child2.Icon and not child2.Icon.bg then
 							child2.Icon.bg = B.ReskinIcon(child2.Icon)
 						end
@@ -76,8 +78,8 @@ C.themes["Blizzard_PlayerChoiceUI"] = function()
 				end
 			end
 
-			reskinOptionButton(option.OptionButtonsContainer.button1)
-			reskinOptionButton(option.OptionButtonsContainer.button2)
+			ReskinFirstOptionButton(option.OptionButtonsContainer.button1)
+			ReskinSecondOptionButton(option.OptionButtonsContainer.button2)
 		end
 	end)
 end
