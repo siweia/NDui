3986c2d782e291c40239f22232eb9b44fe545255
siweia <siwei1990@outlook.com>
2020-08-01 01:02:09 +0800

    Update raid debuffs

--- Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/CastleNathria.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/CastleNathria.lua
@@ -9,6 +9,12 @@ local BOSS
 BOSS = 2393 -- 啸翼
 
 BOSS = 2429 -- 猎手阿尔迪莫
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 335304) -- 寻罪箭
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 334971) -- 锯齿利爪
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 335111) -- 猎手印记
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 335112) -- 猎手印记
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 335113) -- 猎手印记
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 334945) -- 深红痛击
 
 BOSS = 2428 -- 饥饿的毁灭者
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 334228) -- 不稳定的喷发
@@ -30,6 +36,13 @@ module:RegisterDebuff(TIER, INSTANCE, BOSS, 332664) -- 浓缩心能
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 325382) -- 扭曲欲望
 
 BOSS = 2426 -- 猩红议会
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 327773) -- 吸取精华
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 327052) -- 吸取精华
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 328334) -- 战术冲锋
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 330848) -- 跳错了
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 331706) -- 红字
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 331636) -- 黑暗伴舞
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 331637) -- 黑暗伴舞
 
 BOSS = 2394 -- 泥拳
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 335470) -- 锁链猛击
