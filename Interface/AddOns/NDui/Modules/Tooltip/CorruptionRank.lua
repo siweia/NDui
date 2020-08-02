8044661307ae54427fbd858e0f1fe9a692eb9e26
siweia <siwei1990@outlook.com>
2020-08-02 18:37:53 +0800

    Rename scantip

--- Interface/AddOns/NDui/Modules/Tooltip/CorruptionRank.lua
+++ Interface/AddOns/NDui/Modules/Tooltip/CorruptionRank.lua
@@ -188,7 +188,7 @@ function TT:Corruption_PlayerSummary()
 	TT:Corruption_AddSummary()
 end
 
-local tip = _G.NDui_iLvlTooltip
+local tip = _G.NDui_ScanTooltip
 local cloakResString = "(%d+)%s?"..ITEM_MOD_CORRUPTION_RESISTANCE
 
 local essenceTextureIDs = {
