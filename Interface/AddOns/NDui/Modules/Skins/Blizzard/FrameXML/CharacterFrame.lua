2e1fedae6e93311ff75647f24ccb8213afdabe07
siweia <siwei1990@outlook.com>
2020-08-07 01:26:52 +0800

    Skins: replace arrow textures

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/CharacterFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/CharacterFrame.lua
@@ -92,13 +92,12 @@ tinsert(C.defaultThemes, function()
 		popout:SetHighlightTexture("")
 
 		local arrow = popout:CreateTexture(nil, "OVERLAY")
+		arrow:SetSize(14, 14)
 		if slot.verticalFlyout then
-			arrow:SetSize(13, 8)
-			arrow:SetTexture(DB.arrowDown)
+			B.SetupArrow(arrow, "down")
 			arrow:SetPoint("TOP", slot, "BOTTOM", 0, 1)
 		else
-			arrow:SetSize(8, 14)
-			arrow:SetTexture(DB.arrowRight)
+			B.SetupArrow(arrow, "right")
 			arrow:SetPoint("LEFT", slot, "RIGHT", -1, 0)
 		end
 		popout.arrow = arrow
