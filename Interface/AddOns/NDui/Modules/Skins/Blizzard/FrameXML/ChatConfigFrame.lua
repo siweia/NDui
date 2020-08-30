339077264ca93e5c0fd736471054abfb3c135c2e
siweia <siwei1990@outlook.com>
2020-08-31 00:23:45 +0800

    Skins: update ChatConfigFrame

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ChatConfigFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ChatConfigFrame.lua
@@ -17,7 +17,8 @@ tinsert(C.defaultThemes, function()
 			local checkbox = _G[checkBoxName]
 			if checkbox and not checkbox.styled then
 				checkbox:SetBackdrop(nil)
-				B.CreateBDFrame(checkbox, .25)
+				local bg = B.CreateBDFrame(checkbox, .25)
+				bg:SetInside()
 				B.ReskinCheck(_G[checkBoxName.."Check"])
 
 				checkbox.styled = true
