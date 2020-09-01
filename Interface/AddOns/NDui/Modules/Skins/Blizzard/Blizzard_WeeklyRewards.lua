77db0d82c7bbafca13cdc059555615045f4081dd
siweia <siwei1990@outlook.com>
2020-09-02 00:38:58 +0800

    Skins: update WeeklyRewards

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_WeeklyRewards.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_WeeklyRewards.lua
@@ -1,11 +1,38 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
+local r, g, b = DB.r, DB.g, DB.b
 
 -- /run UIParent_OnEvent({}, "WEEKLY_REWARDS_SHOW")
+
+local function updateSelection(frame, state)
+	if not frame.bg then return end
+
+	if frame.SelectedTexture:IsShown() then
+		frame.bg:SetBackdropBorderColor(1, .8, 0)
+	else
+		frame.bg:SetBackdropBorderColor(0, 0, 0)
+	end
+end
+
+local iconColor = DB.QualityColors[LE_ITEM_QUALITY_EPIC or 4] -- epic color only
+local function reskinRewardIcon(itemFrame)
+	if not itemFrame.bg then
+		itemFrame:DisableDrawLayer("BORDER")
+		itemFrame.Icon:SetPoint("LEFT", 6, 0)
+		itemFrame.bg = B.ReskinIcon(itemFrame.Icon)
+		itemFrame.bg:SetBackdropBorderColor(iconColor.r, iconColor.g, iconColor.b)
+		B.CreateBDFrame(itemFrame, .25)
+	end
+end
+
 local function ReskinActivityFrame(frame, isObject)
 	if frame.Border then
 		if isObject then
 			frame.Border:SetAlpha(0)
+			frame.SelectedTexture:SetAlpha(0)
+			frame.LockIcon:SetVertexColor(r, g, b)
+			hooksecurefunc(frame, "SetSelectionState", updateSelection)
+			hooksecurefunc(frame.ItemFrame, "SetDisplayedItem", reskinRewardIcon)
 		else
 			frame.Border:SetTexCoord(.926, 1, 0, 1)
 			frame.Border:SetSize(25, 137)
@@ -14,7 +41,7 @@ local function ReskinActivityFrame(frame, isObject)
 	end
 
 	if frame.Background then
-		B.CreateBDFrame(frame.Background, 1)
+		frame.bg = B.CreateBDFrame(frame.Background, 1)
 	end
 end
 
@@ -24,11 +51,14 @@ C.themes["Blizzard_WeeklyRewards"] = function()
 	B.StripTextures(WeeklyRewardsFrame)
 	B.SetBD(WeeklyRewardsFrame)
 	B.ReskinClose(WeeklyRewardsFrame.CloseButton)
+	B.StripTextures(WeeklyRewardsFrame.SelectRewardButton)
+	B.Reskin(WeeklyRewardsFrame.SelectRewardButton)
 
 	local headerFrame = WeeklyRewardsFrame.HeaderFrame
 	B.StripTextures(headerFrame)
 	B.CreateBDFrame(headerFrame, .25)
 	headerFrame:SetPoint("TOP", 1, -42)
+	headerFrame.Text:SetFontObject(SystemFont_Huge1)
 
 	ReskinActivityFrame(WeeklyRewardsFrame.RaidFrame)
 	ReskinActivityFrame(WeeklyRewardsFrame.MythicFrame)
@@ -37,4 +67,9 @@ C.themes["Blizzard_WeeklyRewards"] = function()
 	for _, frame in pairs(WeeklyRewardsFrame.Activities) do
 		ReskinActivityFrame(frame, true)
 	end
+
+	local confirmItemFrame = WeeklyRewardConfirmSelectionFrame.ItemFrame
+	confirmItemFrame.bg = B.ReskinIcon(confirmItemFrame.Icon)
+	B.ReskinIconBorder(confirmItemFrame.IconBorder)
+	WeeklyRewardConfirmSelectionFrameNameFrame:Hide()
 end
