d875d571aa28051914e6cc65738d985e9cd2ba77
siweia <siwei1990@outlook.com>
2020-09-04 02:28:41 +0800

    Strip QuickKeybindGlow

--- Interface/AddOns/NDui/Modules/ActionBar/Bars/Bar3.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/Bar3.lua
@@ -58,6 +58,7 @@ function Bar:CreateBar3()
 
 	MultiBarBottomRight:SetParent(frame)
 	MultiBarBottomRight:EnableMouse(false)
+	MultiBarBottomRight.QuickKeybindGlow:SetTexture("")
 
 	for i = 1, num do
 		local button = _G["MultiBarBottomRightButton"..i]
