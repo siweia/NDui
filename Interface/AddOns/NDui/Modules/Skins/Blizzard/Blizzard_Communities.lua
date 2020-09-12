55e85590d48892b701cd63e96e4279b0c0c84e4f
siweia <siwei1990@outlook.com>
2020-09-12 18:58:39 +0800

    Skins: update communites list buttons

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Communities.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Communities.lua
@@ -1,6 +1,89 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local function reskinCommunityTab(tab)
+	tab:GetRegions():Hide()
+	B.ReskinIcon(tab.Icon)
+	tab:SetCheckedTexture(DB.textures.pushed)
+	local hl = tab:GetHighlightTexture()
+	hl:SetColorTexture(1, 1, 1, .25)
+	hl:SetAllPoints(tab.Icon)
+end
+
+local function reskinGuildCards(cards)
+	for _, name in pairs({"First", "Second", "Third"}) do
+		local guildCard = cards[name.."Card"]
+		B.StripTextures(guildCard)
+		B.CreateBDFrame(guildCard, .25)
+		B.Reskin(guildCard.RequestJoin)
+	end
+	B.ReskinArrow(cards.PreviousPage, "left")
+	B.ReskinArrow(cards.NextPage, "right")
+end
+
+local function reskinCommunityCards(frame)
+	for _, button in next, frame.ListScrollFrame.buttons do
+		button.CircleMask:Hide()
+		button.LogoBorder:Hide()
+		button.Background:Hide()
+		B.ReskinIcon(button.CommunityLogo)
+		B.Reskin(button)
+	end
+	B.ReskinScroll(frame.ListScrollFrame.scrollBar)
+end
+
+local function reskinRequestCheckbox(self)
+	for button in self.SpecsPool:EnumerateActive() do
+		if button.CheckBox then
+			B.ReskinCheck(button.CheckBox)
+			button.CheckBox:SetSize(26, 26)
+		end
+	end
+end
+
+local function updateCommunitiesSelection(texture, show)
+	local button = texture:GetParent()
+	if show then
+		if texture:GetTexCoord() == 0 then
+			button.bg:SetBackdropColor(0, 1, 0, .25)
+		else
+			button.bg:SetBackdropColor(.51, .773, 1, .25)
+		end
+	else
+		button.bg:SetBackdropColor(0, 0, 0, 0)
+	end
+end
+
+local function updateNameFrame(self)
+	if not self.expanded then return end
+	if not self.bg then
+		self.bg = B.CreateBDFrame(self.Class)
+	end
+	local memberInfo = self:GetMemberInfo()
+	if memberInfo and memberInfo.classID then
+		local classInfo = C_CreatureInfo.GetClassInfo(memberInfo.classID)
+		if classInfo then
+			local tcoords = CLASS_ICON_TCOORDS[classInfo.classFile]
+			self.Class:SetTexCoord(tcoords[1] + .022, tcoords[2] - .025, tcoords[3] + .022, tcoords[4] - .025)
+		end
+	end
+end
+
+local function updateMemberName(self, info)
+	if not info then return end
+
+	local class = self.Class
+	if not class.bg then
+		class.bg = B.CreateBDFrame(class)
+	end
+
+	local classTag = select(2, GetClassInfo(info.classID))
+	if classTag then
+		local tcoords = CLASS_ICON_TCOORDS[classTag]
+		class:SetTexCoord(tcoords[1] + .022, tcoords[2] - .025, tcoords[3] + .022, tcoords[4] - .025)
+	end
+end
+
 C.themes["Blizzard_Communities"] = function()
 	local r, g, b = DB.r, DB.g, DB.b
 	local CommunitiesFrame = CommunitiesFrame
@@ -12,46 +95,6 @@ C.themes["Blizzard_Communities"] = function()
 	B.ReskinMinMax(CommunitiesFrame.MaximizeMinimizeFrame)
 	B.ReskinArrow(CommunitiesFrame.AddToChatButton, "down")
 	B.ReskinDropDown(CommunitiesFrame.CommunitiesListDropDownMenu)
-
-	local function reskinCommunityTab(tab)
-		tab:GetRegions():Hide()
-		B.ReskinIcon(tab.Icon)
-		tab:SetCheckedTexture(DB.textures.pushed)
-		local hl = tab:GetHighlightTexture()
-		hl:SetColorTexture(1, 1, 1, .25)
-		hl:SetAllPoints(tab.Icon)
-	end
-
-	local function reskinGuildCards(cards)
-		for _, name in pairs({"First", "Second", "Third"}) do
-			local guildCard = cards[name.."Card"]
-			B.StripTextures(guildCard)
-			B.CreateBDFrame(guildCard, .25)
-			B.Reskin(guildCard.RequestJoin)
-		end
-		B.ReskinArrow(cards.PreviousPage, "left")
-		B.ReskinArrow(cards.NextPage, "right")
-	end
-
-	local function reskinCommunityCards(frame)
-		for _, button in next, frame.ListScrollFrame.buttons do
-			button.CircleMask:Hide()
-			button.LogoBorder:Hide()
-			button.Background:Hide()
-			B.ReskinIcon(button.CommunityLogo)
-			B.Reskin(button)
-		end
-		B.ReskinScroll(frame.ListScrollFrame.scrollBar)
-	end
-
-	local function reskinRequestCheckbox(self)
-		for button in self.SpecsPool:EnumerateActive() do
-			if button.CheckBox then
-				B.ReskinCheck(button.CheckBox)
-				button.CheckBox:SetSize(26, 26)
-			end
-		end
-	end
 
 	for _, name in next, {"GuildFinderFrame", "InvitationFrame", "TicketFrame", "CommunityFinderFrame", "ClubFinderInvitationFrame"} do
 		local frame = CommunitiesFrame[name]
@@ -115,19 +158,20 @@ C.themes["Blizzard_Communities"] = function()
 		for i = 1, #buttons do
 			local button = buttons[i]
 			if not button.bg then
-				button:GetRegions():Hide()
-				button.Selection:SetAlpha(0)
-				button:SetHighlightTexture("")
 				button.bg = B.CreateBDFrame(button, 0, true)
 				button.bg:SetPoint("TOPLEFT", 5, -5)
 				button.bg:SetPoint("BOTTOMRIGHT", -10, 5)
+
+				button:SetHighlightTexture("")
+				button.IconRing:SetAlpha(0)
+				button.__iconBorder = B.ReskinIcon(button.Icon)
+				button.Background:Hide()
+				button.Selection:SetAlpha(0)
+				hooksecurefunc(button.Selection, "SetShown", updateCommunitiesSelection)
 			end
 
-			if button.Selection:IsShown() then
-				button.bg:SetBackdropColor(r, g, b, .25)
-			else
-				button.bg:SetBackdropColor(0, 0, 0, 0)
-			end
+			button.CircleMask:Hide()
+			button.__iconBorder:SetShown(button.IconRing:IsShown())
 		end
 	end)
 
@@ -321,21 +365,6 @@ C.themes["Blizzard_Communities"] = function()
 		end)
 	end
 
-	local function updateNameFrame(self)
-		if not self.expanded then return end
-		if not self.bg then
-			self.bg = B.CreateBDFrame(self.Class)
-		end
-		local memberInfo = self:GetMemberInfo()
-		if memberInfo and memberInfo.classID then
-			local classInfo = C_CreatureInfo.GetClassInfo(memberInfo.classID)
-			if classInfo then
-				local tcoords = CLASS_ICON_TCOORDS[classInfo.classFile]
-				self.Class:SetTexCoord(tcoords[1] + .022, tcoords[2] - .025, tcoords[3] + .022, tcoords[4] - .025)
-			end
-		end
-	end
-
 	hooksecurefunc(CommunitiesFrame.MemberList, "RefreshListDisplay", function(self)
 		for i = 1, self.ColumnDisplay:GetNumChildren() do
 			local child = select(i, self.ColumnDisplay:GetChildren())
@@ -387,12 +416,12 @@ C.themes["Blizzard_Communities"] = function()
 			local button = buttons[i]
 			if button and button:IsShown() and not button.bg then
 				B.ReskinIcon(button.Icon)
-				for i = 1, 4 do
-					select(i, button:GetRegions()):SetAlpha(0)
-				end
+				B.StripTextures(button)
 				button.bg = B.CreateBDFrame(button, .25)
-				button.bg:SetPoint("TOPLEFT", button.Icon)
-				button.bg:SetPoint("BOTTOMRIGHT", button.Right)
+				button.bg:ClearAllPoints()
+				button.bg:SetPoint("TOPLEFT", button.Icon, 0, C.mult)
+				button.bg:SetPoint("BOTTOMLEFT", button.Icon, 0, -C.mult)
+				button.bg:SetWidth(button:GetWidth())
 			end
 		end
 	end)
@@ -503,21 +532,6 @@ C.themes["Blizzard_Communities"] = function()
 	local listBG = B.CreateBDFrame(applicantList, .25)
 	listBG:SetPoint("TOPLEFT", 0, 0)
 	listBG:SetPoint("BOTTOMRIGHT", -15, 0)
-
-	local function updateMemberName(self, info)
-		if not info then return end
-
-		local class = self.Class
-		if not class.bg then
-			class.bg = B.CreateBDFrame(class)
-		end
-
-		local classTag = select(2, GetClassInfo(info.classID))
-		if classTag then
-			local tcoords = CLASS_ICON_TCOORDS[classTag]
-			class:SetTexCoord(tcoords[1] + .022, tcoords[2] - .025, tcoords[3] + .022, tcoords[4] - .025)
-		end
-	end
 
 	hooksecurefunc(applicantList, "BuildList", function(self)
 		local columnDisplay = self.ColumnDisplay
