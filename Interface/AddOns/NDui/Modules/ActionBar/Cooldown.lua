09fffd7069cc3f15bb90989fe77b117b6d3b59d0
siweia <siwei1990@outlook.com>
2020-09-13 22:52:23 +0800

    Update actionbar cooldown

--- Interface/AddOns/NDui/Modules/ActionBar/Cooldown.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/Cooldown.lua
@@ -20,8 +20,8 @@ function module:ForceUpdate()
 	self:Show()
 end
 
-function module:OnSizeChanged(width)
-	local fontScale = floor(width + 0.5) / ICON_SIZE
+function module:OnSizeChanged(width, height)
+	local fontScale = B:Round((width+height)/2) / ICON_SIZE
 	if fontScale == self.fontScale then return end
 	self.fontScale = fontScale
 
@@ -77,11 +77,11 @@ end
 
 function module:StartTimer(start, duration)
 	if self:IsForbidden() then return end
-	if self.noOCC or hideNumbers[self] then return end
+	if self.noCooldownCount or hideNumbers[self] then return end
 
-	local frameName = self.GetName and self:GetName() or ""
-	if NDuiDB["Actionbar"]["OverrideWA"] and strfind(frameName, "WeakAuras") then
-		self.noOCC = true
+	local frameName = self.GetName and self:GetName()
+	if NDuiDB["Actionbar"]["OverrideWA"] and frameName and strfind(frameName, "WeakAuras") then
+		self.noCooldownCount = true
 		return
 	end
 
