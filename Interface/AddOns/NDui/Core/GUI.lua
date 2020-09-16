134aab81f0c5a1a8834aa10c19bcd9ef31caa146
siweia <siwei1990@outlook.com>
2020-09-17 00:27:09 +0800

    Option to toggle chatframe background

--- Interface/AddOns/NDui/Core/GUI.lua
+++ Interface/AddOns/NDui/Core/GUI.lua
@@ -175,6 +175,7 @@ local defaultSettings = {
 	Chat = {
 		Sticky = false,
 		Lock = true,
+		ShowBG = true,
 		Invite = true,
 		Freedom = true,
 		Keyword = "raid",
@@ -520,6 +521,10 @@ end
 
 local function updateChatSize()
 	B:GetModule("Chat"):UpdateChatSize()
+end
+
+local function toggleChatBackground()
+	B:GetModule("Chat"):ToggleChatBackground()
 end
 
 local function updateToggleDirection()
@@ -893,6 +898,7 @@ local optionList = { -- type, key, value, name, horizon, doubleline
 	},
 	[9] = {
 		{1, "Chat", "Lock", "|cff00cc4c"..L["Lock Chat"]},
+		{1, "Chat", "ShowBG", L["ShowChatBackground"].."*", true, nil, toggleChatBackground},
 		{3, "Chat", "ChatWidth", L["LockChatWidth"].."*", nil, {200, 600, 1}, updateChatSize},
 		{3, "Chat", "ChatHeight", L["LockChatHeight"].."*", true, {100, 500, 1}, updateChatSize},
 		{},--blank
--- Interface/AddOns/NDui/Locales/ruRU.lua
+++ Interface/AddOns/NDui/Locales/ruRU.lua
@@ -666,3 +666,4 @@ L["CustomBarButtonSize"] = "Button Size" -- need translation
 L["CustomBarNumButtons"] = "Max Buttons" -- need translation
 L["CustomBarNumPerRow"] = "Buttons Per Row" -- need translation
 L["HideBossEmote"] = "Hide RaidBossEmoteFrame" -- need translation
+L["ShowChatBackground"] = "Show chat background" -- need translation
