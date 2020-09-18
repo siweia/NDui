7f0fc2687724658379f820a6ea715c18bb16e006
siweia <siwei1990@outlook.com>
2020-09-19 00:29:06 +0800

    Skins: Blizzard_GMSurveyUI is removed as well

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_GMChatUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_GMChatUI.lua
@@ -48,34 +48,3 @@ C.themes["Blizzard_GMChatUI"] = function()
 
 	B.HideObject(frame.buttonFrame)
 end
-
--- /run LoadAddOn'Blizzard_GMSurveyUI' GMSurveyFrame:Show()
-C.themes["Blizzard_GMSurveyUI"] = function()
-	B.SetBD(GMSurveyFrame, nil, 0, 0, -32, 4)
-	B.StripTextures(GMSurveyCommentFrame)
-	local bg = B.CreateBDFrame(GMSurveyCommentFrame, .25)
-	bg:SetInside()
-
-	for i = 1, 15 do
-		local button = _G["GMSurveyQuestion"..i]
-		if button then
-			B.StripTextures(button)
-			local bg  = B.CreateBDFrame(button, .25)
-			bg:SetInside()
-			for _, radio in next, button.radioButtons do
-				B.ReskinRadio(radio)
-				radio:SetSize(20, 20)
-			end
-		end
-	end
-
-	B.StripTextures(GMSurveyFrame)
-	B.StripTextures(GMSurveyFrame.Header)
-	GMSurveyScrollFrameTop:SetAlpha(0)
-	GMSurveyScrollFrameMiddle:SetAlpha(0)
-	GMSurveyScrollFrameBottom:SetAlpha(0)
-	B.Reskin(GMSurveySubmitButton)
-	B.Reskin(GMSurveyCancelButton)
-	B.ReskinClose(GMSurveyCloseButton, GMSurveyFrame, -36, -4)
-	B.ReskinScroll(GMSurveyScrollFrameScrollBar)
-end
