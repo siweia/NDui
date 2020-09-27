892c7a92774c410f306f1d709672d249f00593fe
siweia <siwei1990@outlook.com>
2020-09-27 23:17:25 +0800

    Option to show stancebar

--- Interface/AddOns/NDui/Core/GUI.lua
+++ Interface/AddOns/NDui/Core/GUI.lua
@@ -38,6 +38,7 @@ G.DefaultSettings = {
 		CustomBarButtonSize = 34,
 		CustomBarNumButtons = 12,
 		CustomBarNumPerRow = 12,
+		ShowStance = true,
 	},
 	Bags = {
 		Enable = true,
@@ -709,7 +710,9 @@ G.TabList = {
 G.OptionList = { -- type, key, value, name, horizon, doubleline
 	[1] = {
 		{1, "Actionbar", "Enable", "|cff00cc4c"..L["Enable Actionbar"]},
-		{1, "Actionbar", "MicroMenu", L["Micromenu"], true},
+		{},--blank
+		{1, "Actionbar", "MicroMenu", L["Micromenu"]},
+		{1, "Actionbar", "ShowStance", L["ShowStanceBar"], true},
 		{1, "Actionbar", "Bar4Fade", L["Bar4 Fade"]},
 		{1, "Actionbar", "Bar5Fade", L["Bar5 Fade"], true},
 		{4, "Actionbar", "Style", L["Actionbar Style"], false, {L["BarStyle1"], L["BarStyle2"], L["BarStyle3"], L["BarStyle4"], L["BarStyle5"]}},
