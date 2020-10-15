3ddfe6a1ffc0122da6c48a72acb3c78d420a2bdb
siweia <siwei1990@outlook.com>
2020-10-16 00:01:19 +0800

    Skins: update follower portrait

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -1203,6 +1203,7 @@ do
 			local roleIcon = self.HealthBar.RoleIcon
 			roleIcon:ClearAllPoints()
 			roleIcon:SetPoint("CENTER", self.squareBG, "TOPRIGHT")
+			replaceFollowerRole(roleIcon, roleIcon:GetAtlas())
 			hooksecurefunc(roleIcon, "SetAtlas", replaceFollowerRole)
 
 			local background = self.HealthBar.Background
