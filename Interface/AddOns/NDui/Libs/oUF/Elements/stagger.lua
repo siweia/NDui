7a91badb67e10bc11327d627d527f24d691e5048
siweia <siwei1990@outlook.com>
2020-10-17 02:37:26 +0800

    Fix oUF stagger core

--- Interface/AddOns/NDui/Libs/oUF/Elements/stagger.lua
+++ Interface/AddOns/NDui/Libs/oUF/Elements/stagger.lua
@@ -198,6 +198,8 @@ local function Enable(self, unit)
 		MonkStaggerBar:UnregisterEvent('UNIT_EXITED_VEHICLE')
 		MonkStaggerBar:UnregisterEvent('UPDATE_VEHICLE_ACTIONBAR')
 
+		element:Hide()
+
 		return true
 	end
 end
