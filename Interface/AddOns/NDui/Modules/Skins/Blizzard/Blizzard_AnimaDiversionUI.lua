974ca841ebdce6a21009042765596f5b5fe2b9b3
siweia <siwei1990@outlook.com>
2020-10-23 01:55:43 +0800

    Skins: update AnimaDiversionUI for 36294

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_AnimaDiversionUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_AnimaDiversionUI.lua
@@ -1,6 +1,14 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local function replaceIconString(self, text)
+	if not text then text = self:GetText() end
+	if not text or text == "" then return end
+
+	local newText, count = gsub(text, "|T([^:]-):[%d+:]+|t", "|T%1:14:14:0:0:64:64:5:59:5:59|t")
+	if count > 0 then self:SetFormattedText("%s", newText) end
+end
+
 C.themes["Blizzard_AnimaDiversionUI"] = function()
 	local frame = AnimaDiversionFrame
 
@@ -8,7 +16,9 @@ C.themes["Blizzard_AnimaDiversionUI"] = function()
 	B.SetBD(frame)
 	B.ReskinClose(frame.CloseButton)
 	frame.AnimaDiversionCurrencyFrame.Background:SetAlpha(0)
-	B.ReskinIcon(frame.AnimaDiversionCurrencyFrame.CurrencyFrame.CurrencyIcon)
+	if not DB.isNewPatch then
+		B.ReskinIcon(frame.AnimaDiversionCurrencyFrame.CurrencyFrame.CurrencyIcon)
+	end
 
 	local infoFrame = frame.SelectPinInfoFrame
 	B.StripTextures(infoFrame)
@@ -17,13 +27,26 @@ C.themes["Blizzard_AnimaDiversionUI"] = function()
 	B.Reskin(infoFrame.SelectButton)
 	B.ReskinClose(infoFrame.CloseButton)
 
-	hooksecurefunc(infoFrame, "SetupCosts", function(self)
-		for currency in self.currencyPool:EnumerateActive() do
-			if not currency.bg then
-				currency.bg = B.ReskinIcon(currency.CurrencyIcon)
+	if DB.isNewPatch then
+		hooksecurefunc(infoFrame, "SetupCosts", function(self)
+			for currency in self.currencyPool:EnumerateActive() do
+				if not currency.hooked then
+					replaceIconString(currency.Quantity)
+					hooksecurefunc(currency.Quantity, "SetText", replaceIconString)
+
+					currency.hooked = true
+				end
 			end
-		end
-	end)
+		end)
+	else
+		hooksecurefunc(infoFrame, "SetupCosts", function(self)
+			for currency in self.currencyPool:EnumerateActive() do
+				if not currency.bg then
+					currency.bg = B.ReskinIcon(currency.CurrencyIcon)
+				end
+			end
+		end)
+	end
 
 	B.Reskin(frame.ReinforceInfoFrame.AnimaNodeReinforceButton)
 end
