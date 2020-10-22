bc0628593fa43d9f6504cc97de45ec9a40e6b4b8
siweia <siwei1990@outlook.com>
2020-10-23 01:55:32 +0800

    Skins: add cosmetic texture on CharacterFrame for 36294

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/CharacterFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/CharacterFrame.lua
@@ -60,6 +60,13 @@ tinsert(C.defaultThemes, function()
 		highlight:SetInside()
 	end
 
+	local function UpdateCosmetic(self)
+		if not IsCosmeticItem then return end
+
+		local itemLink = GetInventoryItemLink("player", self:GetID())
+		self.IconOverlay:SetShown(itemLink and IsCosmeticItem(itemLink))
+	end
+
 	local slots = {
 		"Head", "Neck", "Shoulder", "Shirt", "Chest", "Waist", "Legs", "Feet", "Wrist",
 		"Hands", "Finger0", "Finger1", "Trinket0", "Trinket1", "Back", "MainHand",
@@ -77,6 +84,9 @@ tinsert(C.defaultThemes, function()
 		cooldown:SetInside()
 
 		slot.ignoreTexture:SetTexture("Interface\\PaperDollInfoFrame\\UI-GearManager-LeaveItem-Transparent")
+		if IsCosmeticItem then
+			slot.IconOverlay:SetAtlas("CosmeticIconFrame")
+		end
 		slot.IconOverlay:SetInside()
 		B.ReskinIconBorder(slot.IconBorder)
 
@@ -108,6 +118,7 @@ tinsert(C.defaultThemes, function()
 			button.icon:SetShown(GetInventoryItemTexture("player", button:GetID()) ~= nil)
 			colourPopout(button.popoutButton)
 		end
+		UpdateCosmetic(button)
 		UpdateHighlight(button)
 	end)
 
