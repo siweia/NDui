868f1b52c65f6961732b7481b00310648092d82d
siweia <siwei1990@outlook.com>
2020-10-22 21:40:04 +0800

    Chat init tweaks

--- Interface/AddOns/NDui/Modules/Chat/Core.lua
+++ Interface/AddOns/NDui/Modules/Chat/Core.lua
@@ -329,8 +329,8 @@ function module:OnLogin()
 	-- Lock chatframe
 	if NDuiDB["Chat"]["Lock"] then
 		hooksecurefunc("FCF_SavePositionAndDimensions", self.UpdateChatSize)
-		self:UpdateChatSize()
 		B:RegisterEvent("UI_SCALE_CHANGED", self.UpdateChatSize)
+		self:UpdateChatSize()
 	end
 
 	-- ProfanityFilter
