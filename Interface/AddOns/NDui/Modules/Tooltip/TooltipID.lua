0f7b700cb89606ac5e3a9b6b407b591df8c88b6a
siweia <siwei1990@outlook.com>
2020-10-24 01:52:11 +0800

    Show questID on world map

--- Interface/AddOns/NDui/Modules/Tooltip/TooltipID.lua
+++ Interface/AddOns/NDui/Modules/Tooltip/TooltipID.lua
@@ -139,4 +139,11 @@ function TT:SetupTooltipID()
 	hooksecurefunc(GameTooltip, "SetAzeritePower", function(self, _, _, id)
 		if id then TT.AddLineForID(self, id, types.azerite, true) end
 	end)
+
+	-- Quests
+	hooksecurefunc("QuestMapLogTitleButton_OnEnter", function(self)
+		if self.questID then
+			TT.AddLineForID(GameTooltip, self.questID, types.quest)
+		end
+	end)
 end
