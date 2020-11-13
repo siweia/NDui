395b1646878ee94d077b0e909720100ed071f7b9
siweia <siwei1990@outlook.com>
2020-11-14 02:32:42 +0800

    Movable quest frame

--- Interface/AddOns/NDui/Plugins/DragEmAll.lua
+++ Interface/AddOns/NDui/Plugins/DragEmAll.lua
@@ -33,6 +33,7 @@ local frames = {
 	["PetitionFrame"] = false,
 	["PetStableFrame"] = false,
 	["PVEFrame"] = false,
+	["QuestFrame"] = false,
 	["RaidParentFrame"] = false,
 	["ReputationFrame"] = true,
 	["SendMailFrame"] = true,
