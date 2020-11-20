93631db9018e2b2c83d56c1440651706fd3e70b5
siweia <siwei1990@outlook.com>
2020-11-20 21:15:38 +0800

    Cleanup, no need to review

--- Interface/AddOns/NDui/Modules/UFs/Nameplates.lua
+++ Interface/AddOns/NDui/Modules/UFs/Nameplates.lua
@@ -629,7 +629,7 @@ end
 function UF:AddWidgetContainer(self)
 	local widgetContainer = CreateFrame("Frame", nil, self, "UIWidgetContainerTemplate")
 	widgetContainer:SetPoint("TOP", self.Castbar, "BOTTOM", 0, -5)
-	widgetContainer:SetScale(1/NDuiADB["UIScale"]) -- need reviewed
+	widgetContainer:SetScale(1/NDuiADB["UIScale"])
 	widgetContainer:Hide()
 
 	self.WidgetContainer = widgetContainer
