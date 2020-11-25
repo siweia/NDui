c83c2144b47c4ee47cda22bb344e938dad08a161
siweia <siwei1990@outlook.com>
2020-11-25 20:23:53 +0800

    Fix for some quests that interact with WorldFrame

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -555,6 +555,7 @@ function UF:CreateCastBar(self)
 			lag:ClearAllPoints()
 			lag:SetPoint("BOTTOM", cb, "TOP", 0, 2)
 			cb.Lag = lag
+			self:RegisterEvent("GLOBAL_MOUSE_UP", B.OnCastSent, true) -- Fix quests with WorldFrame interaction
 			self:RegisterEvent("CURRENT_SPELL_CAST_CHANGED", B.OnCastSent, true)
 		end
 	elseif mystyle == "nameplate" then
