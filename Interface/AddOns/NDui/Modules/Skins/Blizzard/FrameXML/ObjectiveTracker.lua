b26ecaa95379b08551bd565dc12d27ba9da2ad87
siweia <siwei1990@outlook.com>
2020-12-04 21:40:18 +0800

    Skins: add check for currency container, #118

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
@@ -143,11 +143,13 @@ tinsert(C.defaultThemes, function()
 		if widgetFrame and widgetFrame.Frame then
 			widgetFrame.Frame:SetAlpha(0)
 
-			for i = 1, widgetFrame.CurrencyContainer:GetNumChildren() do
-				local bu = select(i, widgetFrame.CurrencyContainer:GetChildren())
-				if bu and bu.Icon and not bu.styled then
-					B.ReskinIcon(bu.Icon)
-					bu.styled = true
+			if widgetFrame.CurrencyContainer then -- this may be removed, needs review
+				for i = 1, widgetFrame.CurrencyContainer:GetNumChildren() do
+					local bu = select(i, widgetFrame.CurrencyContainer:GetChildren())
+					if bu and bu.Icon and not bu.styled then
+						B.ReskinIcon(bu.Icon)
+						bu.styled = true
+					end
 				end
 			end
 		end
