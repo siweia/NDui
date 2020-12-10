ba10be5c0dcf658749b9b3649caaaad8394bca08
siweia <siwei1990@outlook.com>
2020-12-11 00:42:44 +0800

    Skins: fix MawBuffsBlock anchor #126

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
@@ -109,7 +109,7 @@ end
 
 local function GetMawBuffsAnchor(frame)
 	local center = frame:GetCenter()
-	if center and center < DB.ScreenWidth/2 then
+	if center and center < GetScreenWidth()/2 then
 		return "LEFT"
 	else
 		return "RIGHT"
@@ -210,7 +210,7 @@ tinsert(C.defaultThemes, function()
 		end
 	end
 
-	-- Maw buffs block
+	-- MawBuffsBlock
 	ScenarioBlocksFrame.MawBuffsBlock.Container:HookScript("OnClick", function(container)
 		local direc = GetMawBuffsAnchor(container)
 		if not container.lastDirec or container.lastDirec ~= direc then
