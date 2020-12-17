461b2b6a570955b944431d7b3163ada344a12324
siweia <siwei1990@outlook.com>
2020-12-17 23:01:37 +0800

    Skins: merge LFG stuff

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/LFGFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/LFGFrame.lua
@@ -239,4 +239,20 @@ tinsert(C.defaultThemes, function()
 	hooksecurefunc("LFGDungeonReadyStatusGrouped_UpdateIcon", function(button, role)
 		button.texture:SetTexCoord(B.GetRoleTexCoord(role))
 	end)
+
+	-- RaidFinder
+	RaidFinderFrameBottomInset:Hide()
+	RaidFinderFrameRoleBackground:Hide()
+	RaidFinderFrameRoleInset:Hide()
+	RaidFinderQueueFrameBackground:Hide()
+	-- this fixes right border of second reward being cut off
+	RaidFinderQueueFrameScrollFrame:SetWidth(RaidFinderQueueFrameScrollFrame:GetWidth()+1)
+
+	B.ReskinScroll(RaidFinderQueueFrameScrollFrameScrollBar)
+	B.ReskinDropDown(RaidFinderQueueFrameSelectionDropDown)
+	B.Reskin(RaidFinderFrameFindRaidButton)
+	B.Reskin(RaidFinderQueueFrameIneligibleFrameLeaveQueueButton)
+	B.Reskin(RaidFinderQueueFramePartyBackfillBackfillButton)
+	B.Reskin(RaidFinderQueueFramePartyBackfillNoBackfillButton)
+	styleRewardButton(RaidFinderQueueFrameScrollFrameChildFrameMoneyReward)
 end)
