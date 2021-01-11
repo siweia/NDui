43cd88d5e3f63748fff488f5ef84ed493da928ae
siweia <siwei1990@outlook.com>
2021-01-12 02:02:05 +0800

    Update auras

--- Interface/AddOns/NDui/Config/AuraWatchList/All.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/All.lua
@@ -186,8 +186,10 @@ local list = {
 		{AuraID = 324092, UnitID = "player", Flash = true},	-- 赤红，闪耀光辉
 		{AuraID = 328737, UnitID = "player", Flash = true},	-- 赤红，光辉残片
 		{AuraID = 326891, UnitID = "player", Flash = true},	-- 赎罪大厅，痛楚
+		{AuraID = 319603, UnitID = "player", Flash = true},	-- 赎罪大厅，羁石诅咒
 		{AuraID = 333299, UnitID = "player"},	-- 伤逝剧场，荒芜诅咒
 		{AuraID = 319637, UnitID = "player"},	-- 伤逝剧场，魂魄归体
+		{AuraID = 330725, UnitID = "player", Flash = true},	-- 伤逝剧场，暗影易伤
 		{AuraID = 336258, UnitID = "player", Flash = true},	-- 凋魂之殇，落单狩猎
 		{AuraID = 327401, UnitID = "player", Flash = true},	-- 通灵战潮，共受苦难
 		{AuraID = 323471, UnitID = "player", Flash = true},	-- 通灵战潮，切肉飞刀
@@ -236,7 +238,6 @@ local list = {
 		{AuraID = 320132, UnitID = "target"},	-- 彼界，暗影之怒
 		{AuraID = 320293, UnitID = "target", Value = true},	-- 伤逝剧场，融入死亡
 		{AuraID = 331275, UnitID = "target", Flash = true},	-- 伤逝剧场，不灭护卫
-		{AuraID = 330725, UnitID = "target", Flash = true},	-- 伤逝剧场，暗影易伤
 		{AuraID = 336449, UnitID = "target"},	-- 凋魂，玛卓克萨斯之墓
 		{AuraID = 336451, UnitID = "target"},	-- 凋魂，玛卓克萨斯之壁
 		{AuraID = 333737, UnitID = "target"},	-- 凋魂，凝结之疾
