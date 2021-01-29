fcf4534278d8579173c4ba15031f703369bd2735
siweia <siwei1990@outlook.com>
2021-01-30 01:36:47 +0800

    Update buff format

--- Interface/AddOns/NDui/Modules/Auras/Auras.lua
+++ Interface/AddOns/NDui/Modules/Auras/Auras.lua
@@ -51,8 +51,8 @@ local day, hour, minute = 86400, 3600, 60
 function A:FormatAuraTime(s)
 	if s >= day then
 		return format("%d"..DB.MyColor.."d", s/day), s%day
-	elseif s >= hour then
-		return format("%s"..DB.MyColor.."h", B:Round(s/hour, 1)), s%hour
+	elseif s >= 2*hour then
+		return format("%d"..DB.MyColor.."h", s/hour), s%hour
 	elseif s >= 10*minute then
 		return format("%d"..DB.MyColor.."m", s/minute), s%minute
 	elseif s >= minute then
