5ddb7e74699b3d99d5a5f6f341527e10fa019cc6
siweia <siwei1990@outlook.com>
2021-02-25 02:17:47 +0800

    Cleanup

--- Interface/AddOns/NDui/Modules/ActionBar/Bars/CustomBar.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/CustomBar.lua
@@ -2,6 +2,8 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 local Bar = B:GetModule("Actionbar")
 
+local tinsert = tinsert
+local mod, min, ceil = mod, min, ceil
 local cfg = C.Bars.bar4
 local margin, padding = C.Bars.margin, C.Bars.padding
 
