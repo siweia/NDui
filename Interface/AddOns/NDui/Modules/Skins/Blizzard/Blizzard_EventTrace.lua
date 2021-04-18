dd6c54ae2ae61f682e712a69133ae85ddffee1f9
siweia <siwei1990@outlook.com>
2021-04-18 20:46:41 +0800

    Skins: finalized EventTrace for 9.1

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_EventTrace.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_EventTrace.lua
@@ -9,20 +9,56 @@ end
 
 local function reskinScrollArrow(self, direction)
 	self.Texture:SetAlpha(0)
+	self.Overlay:SetAlpha(0)
 	local tex = self:CreateTexture(nil, "ARTWORK")
 	tex:SetAllPoints()
 	B.CreateBDFrame(tex, .25)
 	B.SetupArrow(tex, direction)
+	self.__texture = tex
+
+	self:HookScript("OnEnter", B.Texture_OnEnter)
+	self:HookScript("OnLeave", B.Texture_OnLeave)
 end
 
-local function ReskinEventTraceScroll(scrollBar)
+local function reskinEventTraceScrollBar(scrollBar)
 	B.StripTextures(scrollBar)
 	reskinScrollArrow(scrollBar.Back, "up")
 	reskinScrollArrow(scrollBar.Forward, "down")
 
-	local thumb = scrollBar.Track.Thumb
+	local thumb = scrollBar:GetThumb()
 	B.StripTextures(thumb, 0)
 	B.CreateBDFrame(thumb, 0, true)
+end
+
+local function reskinScrollChild(self)
+	for i = 1, self.ScrollTarget:GetNumChildren() do
+		local child = select(i, self.ScrollTarget:GetChildren())
+		local hideButton = child and child.HideButton
+		if hideButton and not hideButton.styled then
+			B.ReskinClose(hideButton)
+			hideButton:ClearAllPoints()
+			hideButton:SetPoint("LEFT", 3, 0)
+
+			local checkButton = child.CheckButton
+			if checkButton then
+				B.ReskinCheck(checkButton)
+				checkButton:SetSize(22, 22)
+			end
+
+			hideButton.styled = true
+		end
+	end
+end
+
+local function reskinEventTraceScrollBox(frame)
+	frame:DisableDrawLayer("BACKGROUND")
+	B.CreateBDFrame(frame, .25)
+	hooksecurefunc(frame, "Update", reskinScrollChild)
+end
+
+local function ReskinEventTraceFrame(frame)
+	reskinEventTraceScrollBox(frame.ScrollBox)
+	reskinEventTraceScrollBar(frame.ScrollBar)
 end
 
 C.themes["Blizzard_EventTrace"] = function()
@@ -35,8 +71,9 @@ C.themes["Blizzard_EventTrace"] = function()
 	local filterBar = EventTrace.Filter.Bar
 	B.ReskinEditBox(logBar.SearchBox)
 
-	ReskinEventTraceScroll(EventTrace.Log.Events.ScrollBar)
-	ReskinEventTraceScroll(EventTrace.Filter.ScrollBar)
+	ReskinEventTraceFrame(EventTrace.Log.Events)
+	ReskinEventTraceFrame(EventTrace.Log.Search)
+	ReskinEventTraceFrame(EventTrace.Filter)
 
 	local buttons = {
 		subtitleBar.ViewLog,
