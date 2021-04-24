600f40158b9873a4c0e038cbb64ef63fee17f4db
siweia <siwei1990@outlook.com>
2021-04-24 16:50:54 +0800

    Skins: currency icons in runeforge

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_RuneforgeUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_RuneforgeUI.lua
@@ -10,15 +10,38 @@ local function updateSelectedTexture(texture, shown)
 	end
 end
 
+local function replaceCurrencyDisplay(self)
+	if not self.currencyID then return end
+	local text = GetCurrencyString(self.currencyID, self.amount, self.colorCode, self.abbreviate)
+	local newText, count = gsub(text, "|T([^:]-):[%d+:]+|t", "|T%1:14:14:0:0:64:64:5:59:5:59|t")
+	if count > 0 then self:SetText(newText) end
+end
+
+local function SetCurrenciesHook(self)
+	if self.currencyFramePool then
+		for frame in self.currencyFramePool:EnumerateActive() do
+			if not frame.hooked then
+				replaceCurrencyDisplay(frame)
+				hooksecurefunc(frame, "SetCurrencyFromID", replaceCurrencyDisplay)
+
+				frame.hooked = true
+			end
+		end
+	end
+end
+
 C.themes["Blizzard_RuneforgeUI"] = function()
 	local frame = RuneforgeFrame
 	B.ReskinClose(frame.CloseButton, nil, -70, -70)
 
 	local createFrame = frame.CreateFrame
 	B.Reskin(createFrame.CraftItemButton)
-	if createFrame.Cost.Text then -- isNewPatch
+	if not DB.isNewPatch then
 		B.ReplaceIconString(createFrame.Cost.Text)
 		hooksecurefunc(createFrame.Cost.Text, "SetText", B.ReplaceIconString)
+	else
+		hooksecurefunc(frame.CurrencyDisplay, "SetCurrencies", SetCurrenciesHook)
+		hooksecurefunc(createFrame.Cost.Currencies, "SetCurrencies", SetCurrenciesHook)
 	end
 
 	local powerFrame = frame.CraftingFrame.PowerFrame
