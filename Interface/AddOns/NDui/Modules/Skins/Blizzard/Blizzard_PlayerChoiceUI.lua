563ec96836ec7befe5ec60d77a7648ac5fe8b5d6
siweia <siwei1990@outlook.com>
2021-05-02 00:40:48 +0800

    Skins: update PlayerChoiceUI

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_PlayerChoiceUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_PlayerChoiceUI.lua
@@ -55,11 +55,11 @@ C.themes["Blizzard_PlayerChoiceUI"] = function()
 
 				if child.Spell then
 					if not child.Spell.bg then
-						child.Spell.Border:SetTexture("")
-						child.Spell.IconMask:Hide()
+						child.Spell.Border:SetAlpha(0)
 						child.Spell.bg = B.ReskinIcon(child.Spell.Icon)
 					end
 
+					child.Spell.IconMask:Hide()
 					child.Spell.Text:SetTextColor(1, 1, 1)
 				end
 
