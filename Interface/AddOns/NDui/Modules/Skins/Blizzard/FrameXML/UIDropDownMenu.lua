d897a38f0c53bdf114c714c388e61874645d1dfe
siweia <siwei1990@outlook.com>
2021-05-20 02:52:18 +0800

    Skins: update dropdown skin

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/UIDropDownMenu.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/UIDropDownMenu.lua
@@ -2,21 +2,12 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 local r, g, b = DB.r, DB.g, DB.b
 
-local function toggleBackdrop(bu, show)
-	bu.bg:SetShown(show)
-end
-
-local function isCheckTexture(check)
-	if check:GetTexture() == "Interface\\Common\\UI-DropDownRadioChecks" then
-		return true
-	end
-end
-
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
+	local dropdowns = {"DropDownList", "L_DropDownList", "Lib_DropDownList"}
 	hooksecurefunc("UIDropDownMenu_CreateFrames", function()
-		for _, name in next, {"DropDownList", "L_DropDownList", "Lib_DropDownList"} do
+		for _, name in next, dropdowns do
 			for i = 1, UIDROPDOWNMENU_MAXLEVELS do
 				local backdrop = _G[name..i.."Backdrop"]
 				if backdrop and not backdrop.styled then
@@ -37,10 +28,10 @@ tinsert(C.defaultThemes, function()
 			local bu = _G["DropDownList"..level.."Button"..i]
 			local _, _, _, x = bu:GetPoint()
 			if bu:IsShown() and x then
-				local hl = _G["DropDownList"..level.."Button"..i.."Highlight"]
 				local check = _G["DropDownList"..level.."Button"..i.."Check"]
-				hl:SetPoint("TOPLEFT", -x + 1, 0)
-				hl:SetPoint("BOTTOMRIGHT", listFrame:GetWidth() - bu:GetWidth() - x - 1, 0)
+				local uncheck = _G["DropDownList"..level.."Button"..i.."UnCheck"]
+				local hl = _G["DropDownList"..level.."Button"..i.."Highlight"]
+				local arrow = _G["DropDownList"..level.."Button"..i.."ExpandArrow"]
 
 				if not bu.bg then
 					bu.bg = B.CreateBDFrame(bu)
@@ -49,37 +40,33 @@ tinsert(C.defaultThemes, function()
 					bu.bg:SetSize(12, 12)
 					hl:SetColorTexture(r, g, b, .25)
 
-					local arrow = _G["DropDownList"..level.."Button"..i.."ExpandArrow"]
-					B.SetupArrow(arrow:GetNormalTexture(), "right")
-					arrow:SetSize(14, 14)
-				end
-				toggleBackdrop(bu, false)
-
-				local uncheck = _G["DropDownList"..level.."Button"..i.."UnCheck"]
-				if isCheckTexture(uncheck) then uncheck:SetTexture("") end
-
-				if isCheckTexture(check) then
-					if not bu.notCheckable then
-						toggleBackdrop(bu, true)
-
-						-- only reliable way to see if button is radio or or check...
-						local _, co = check:GetTexCoord()
-						if co == 0 then
-							check:SetTexture("Interface\\Buttons\\UI-CheckBox-Check")
-							check:SetVertexColor(r, g, b, 1)
-							check:SetSize(20, 20)
-							check:SetDesaturated(true)
-						else
-							check:SetTexture(DB.bdTex)
-							check:SetVertexColor(r, g, b, .6)
-							check:SetSize(10, 10)
-							check:SetDesaturated(false)
-						end
-
-						check:SetTexCoord(0, 1, 0, 1)
+					if arrow then
+						B.SetupArrow(arrow:GetNormalTexture(), "right")
+						arrow:SetSize(14, 14)
 					end
-				else
-					check:SetSize(16, 16)
+				end
+
+				bu.bg:Hide()
+				hl:SetPoint("TOPLEFT", -x + C.mult, 0)
+				hl:SetPoint("BOTTOMRIGHT", listFrame:GetWidth() - bu:GetWidth() - x - C.mult, 0)
+				if uncheck then uncheck:SetTexture("") end
+
+				if not bu.notCheckable then
+					-- only reliable way to see if button is radio or or check...
+					local _, co = check:GetTexCoord()
+					if co == 0 then
+						check:SetTexture("Interface\\Buttons\\UI-CheckBox-Check")
+						check:SetVertexColor(r, g, b, 1)
+						check:SetSize(20, 20)
+						check:SetDesaturated(true)
+					else
+						check:SetColorTexture(r, g, b, .6)
+						check:SetSize(10, 10)
+						check:SetDesaturated(false)
+					end
+
+					check:SetTexCoord(0, 1, 0, 1)
+					bu.bg:Show()
 				end
 			end
 		end
@@ -88,7 +75,7 @@ tinsert(C.defaultThemes, function()
 	hooksecurefunc("UIDropDownMenu_SetIconImage", function(icon, texture)
 		if texture:find("Divider") then
 			icon:SetColorTexture(1, 1, 1, .2)
-			icon:SetHeight(1)
+			icon:SetHeight(C.mult)
 		end
 	end)
 end)
