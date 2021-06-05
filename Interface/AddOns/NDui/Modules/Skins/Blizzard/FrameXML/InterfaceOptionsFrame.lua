a2c799218a9e5373c9a9953b3202a8fbb91cc1a7
siweia <siwei1990@outlook.com>
2021-06-05 16:27:20 +0800

    Skins: update option panels

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/InterfaceOptionsFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/InterfaceOptionsFrame.lua
@@ -4,240 +4,76 @@ local B, C, L, DB = unpack(ns)
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
-	local restyled = false
+	local styled
 
 	InterfaceOptionsFrame:HookScript("OnShow", function()
-		if restyled then return end
+		if styled then return end
 
+		B.StripTextures(InterfaceOptionsFrameTab1)
+		B.StripTextures(InterfaceOptionsFrameTab2)
 		B.StripTextures(InterfaceOptionsFrameCategories)
 		B.StripTextures(InterfaceOptionsFramePanelContainer)
 		B.StripTextures(InterfaceOptionsFrameAddOns)
-		for i = 1, 2 do
-			B.StripTextures(_G["InterfaceOptionsFrameTab"..i])
-		end
+
 		B.SetBD(InterfaceOptionsFrame)
 		InterfaceOptionsFrame.Border:Hide()
 		B.StripTextures(InterfaceOptionsFrame.Header)
 		InterfaceOptionsFrame.Header:ClearAllPoints()
 		InterfaceOptionsFrame.Header:SetPoint("TOP", InterfaceOptionsFrame, 0, 0)
 
+		B.Reskin(InterfaceOptionsFrameDefaults)
+		B.Reskin(InterfaceOptionsFrameOkay)
+		B.Reskin(InterfaceOptionsFrameCancel)
+
 		local line = InterfaceOptionsFrame:CreateTexture(nil, "ARTWORK")
 		line:SetSize(C.mult, 546)
 		line:SetPoint("LEFT", 205, 10)
 		line:SetColorTexture(1, 1, 1, .25)
 
-		local buttons = {
-			"InterfaceOptionsFrameDefaults",
-			"InterfaceOptionsFrameOkay",
-			"InterfaceOptionsFrameCancel",
-			"InterfaceOptionsSocialPanelRedockChat",
-			"InterfaceOptionsSocialPanelTwitterLoginButton",
-			"InterfaceOptionsDisplayPanelResetTutorials",
+		local interfacePanels = {
+			"InterfaceOptionsControlsPanel",
+			"InterfaceOptionsCombatPanel",
+			"InterfaceOptionsDisplayPanel",
+			"InterfaceOptionsSocialPanel",
+			"InterfaceOptionsActionBarsPanel",
+			"InterfaceOptionsNamesPanel",
+			"InterfaceOptionsNamesPanelFriendly",
+			"InterfaceOptionsNamesPanelEnemy",
+			"InterfaceOptionsNamesPanelUnitNameplates",
+			"InterfaceOptionsCameraPanel",
+			"InterfaceOptionsMousePanel",
+			"InterfaceOptionsAccessibilityPanel",
+			"CompactUnitFrameProfiles",
+			"CompactUnitFrameProfilesGeneralOptionsFrame",
 		}
 		if DB.isNewPatch then
-			tinsert(buttons, "InterfaceOptionsAccessibilityPanelConfigureTextToSpeech")
-			tinsert(buttons, "InterfaceOptionsAccessibilityPanelRemoteTextToSpeechVoicePlaySample")
+			tinsert(interfacePanels, "InterfaceOptionsColorblindPanel")
 		end
-		for _, buttonName in pairs(buttons) do
-			local button = _G[buttonName]
-			if not button then
-				if DB.isDeveloper then print(buttonName, "not found.") end
-			else
-				B.Reskin(button)
-			end
-		end
-
-		local checkboxes = {
-			"InterfaceOptionsControlsPanelStickyTargeting",
-			"InterfaceOptionsControlsPanelAutoDismount",
-			"InterfaceOptionsControlsPanelAutoClearAFK",
-			"InterfaceOptionsControlsPanelAutoLootCorpse",
-			"InterfaceOptionsControlsPanelInteractOnLeftClick",
-			"InterfaceOptionsControlsPanelLootAtMouse",
-			"InterfaceOptionsCombatPanelTargetOfTarget",
-			"InterfaceOptionsCombatPanelFlashLowHealthWarning",
-			"InterfaceOptionsCombatPanelLossOfControl",
-			"InterfaceOptionsCombatPanelEnableFloatingCombatText",
-			"InterfaceOptionsCombatPanelAutoSelfCast",
-			"InterfaceOptionsDisplayPanelRotateMinimap",
-			"InterfaceOptionsDisplayPanelAJAlerts",
-			"InterfaceOptionsDisplayPanelShowInGameNavigation",
-			"InterfaceOptionsDisplayPanelShowTutorials",
-			"InterfaceOptionsSocialPanelProfanityFilter",
-			"InterfaceOptionsSocialPanelSpamFilter",	-- isNewPatch, removed in 38627
-			"InterfaceOptionsSocialPanelGuildMemberAlert",
-			"InterfaceOptionsSocialPanelBlockTrades",
-			"InterfaceOptionsSocialPanelBlockGuildInvites",
-			"InterfaceOptionsSocialPanelBlockChatChannelInvites",
-			"InterfaceOptionsSocialPanelShowAccountAchievments",
-			"InterfaceOptionsSocialPanelOnlineFriends",
-			"InterfaceOptionsSocialPanelOfflineFriends",
-			"InterfaceOptionsSocialPanelBroadcasts",
-			"InterfaceOptionsSocialPanelFriendRequests",
-			"InterfaceOptionsSocialPanelShowToastWindow",
-			"InterfaceOptionsSocialPanelEnableTwitter",
-			"InterfaceOptionsSocialPanelAutoAcceptQuickJoinRequests",
-			"InterfaceOptionsActionBarsPanelBottomLeft",
-			"InterfaceOptionsActionBarsPanelBottomRight",
-			"InterfaceOptionsActionBarsPanelRight",
-			"InterfaceOptionsActionBarsPanelRightTwo",
-			"InterfaceOptionsActionBarsPanelStackRightBars",
-			"InterfaceOptionsActionBarsPanelLockActionBars",
-			"InterfaceOptionsActionBarsPanelAlwaysShowActionBars",
-			"InterfaceOptionsActionBarsPanelCountdownCooldowns",
-			"InterfaceOptionsNamesPanelMyName",
-			"InterfaceOptionsNamesPanelNonCombatCreature",
-			"InterfaceOptionsNamesPanelFriendlyPlayerNames",
-			"InterfaceOptionsNamesPanelFriendlyMinions",
-			"InterfaceOptionsNamesPanelEnemyPlayerNames",
-			"InterfaceOptionsNamesPanelEnemyMinions",
-			"InterfaceOptionsNamesPanelUnitNameplatesPersonalResource",
-			"InterfaceOptionsNamesPanelUnitNameplatesPersonalResourceOnEnemy",
-			"InterfaceOptionsNamesPanelUnitNameplatesMakeLarger",
-			"InterfaceOptionsNamesPanelUnitNameplatesShowAll",
-			"InterfaceOptionsNamesPanelUnitNameplatesAggroFlash",
-			"InterfaceOptionsNamesPanelUnitNameplatesFriendlyMinions",
-			"InterfaceOptionsNamesPanelUnitNameplatesEnemyMinions",
-			"InterfaceOptionsNamesPanelUnitNameplatesEnemyMinus",
-			"InterfaceOptionsNamesPanelUnitNameplatesEnemies",
-			"InterfaceOptionsNamesPanelUnitNameplatesFriends",
-			"InterfaceOptionsCameraPanelWaterCollision",
-			"InterfaceOptionsMousePanelInvertMouse",
-			"InterfaceOptionsMousePanelEnableMouseSpeed",
-			"InterfaceOptionsMousePanelClickToMove",
-			"InterfaceOptionsMousePanelLockCursorToScreen",
-			"InterfaceOptionsAccessibilityPanelMovePad",
-			"InterfaceOptionsAccessibilityPanelCinematicSubtitles",
-			"InterfaceOptionsAccessibilityPanelOverrideFadeOut",
-			"InterfaceOptionsAccessibilityPanelColorblindMode",	-- isNewPatch, removed in 38709
-		}
-		if DB.isNewPatch then
-			tinsert(checkboxes, "InterfaceOptionsAccessibilityPanelSpeechToText")
-			tinsert(checkboxes, "InterfaceOptionsAccessibilityPanelTextToSpeech")
-			tinsert(checkboxes, "InterfaceOptionsAccessibilityPanelRemoteTextToSpeech")
-			tremove(checkboxes, 17)
-			tremove(checkboxes, 61)
-			tinsert(checkboxes, "InterfaceOptionsColorblindPanelColorblindMode")
-			tinsert(checkboxes, "InterfaceOptionsAccessibilityPanelQuestTextContrast")
-		end
-		for _, boxName in pairs(checkboxes) do
-			local checkbox = _G[boxName]
-			if not checkbox then
-				if DB.isDeveloper then print(boxName, "not found.") end
-			else
-				B.ReskinCheck(checkbox)
-			end
-		end
-
-		local dropdowns = {
-			"InterfaceOptionsControlsPanelAutoLootKeyDropDown",
-			"InterfaceOptionsCombatPanelFocusCastKeyDropDown",
-			"InterfaceOptionsCombatPanelSelfCastKeyDropDown",
-			"InterfaceOptionsDisplayPanelOutlineDropDown",
-			"InterfaceOptionsDisplayPanelSelfHighlightDropDown",
-			"InterfaceOptionsDisplayPanelDisplayDropDown",
-			"InterfaceOptionsDisplayPanelChatBubblesDropDown",
-			"InterfaceOptionsSocialPanelChatStyle",
-			"InterfaceOptionsSocialPanelTimestamps",
-			"InterfaceOptionsSocialPanelWhisperMode",
-			"InterfaceOptionsActionBarsPanelPickupActionKeyDropDown",
-			"InterfaceOptionsNamesPanelNPCNamesDropDown",
-			"InterfaceOptionsNamesPanelUnitNameplatesMotionDropDown",
-			"InterfaceOptionsCameraPanelStyleDropDown",
-			"InterfaceOptionsMousePanelClickMoveStyleDropDown",
-			"InterfaceOptionsAccessibilityPanelColorFilterDropDown", -- isNewPatch, removed in 38709
-			"InterfaceOptionsAccessibilityPanelMotionSicknessDropdown",
-			"InterfaceOptionsAccessibilityPanelShakeIntensityDropdown",
-		}
-		if DB.isNewPatch then
-			tremove(dropdowns, 16)
-			tinsert(dropdowns, "InterfaceOptionsColorblindPanelColorFilterDropDown")
-			tinsert(dropdowns, "InterfaceOptionsAccessibilityPanelRemoteTextToSpeechVoiceDropdown")
-		end
-		for _, ddName in pairs(dropdowns) do
-			local dropdown = _G[ddName]
-			if not dropdown then
-				if DB.isDeveloper then print(ddName, "not found.") end
-			else
-				B.ReskinDropDown(dropdown)
-			end
-		end
-
-		local sliders = {
-			"InterfaceOptionsCombatPanelSpellAlertOpacitySlider",
-			"InterfaceOptionsCameraPanelFollowSpeedSlider",
-			"InterfaceOptionsMousePanelMouseSensitivitySlider",
-			"InterfaceOptionsMousePanelMouseLookSpeedSlider",
-			"InterfaceOptionsAccessibilityPanelColorblindStrengthSlider", -- isNewPatch, removed in 38709
-		}
-		if DB.isNewPatch then
-			tremove(sliders, 5)
-			tinsert(sliders, "InterfaceOptionsColorblindPanelColorblindStrengthSlider")
-		end
-		for _, sliderName in pairs(sliders) do
-			local slider = _G[sliderName]
-			if not slider then
-				if DB.isDeveloper then print(sliderName, "not found.") end
-			else
-				B.ReskinSlider(slider)
-			end
-		end
-
-		if IsAddOnLoaded("Blizzard_CUFProfiles") then
-			CompactUnitFrameProfilesGeneralOptionsFrameAutoActivateBG:Hide()
-
-			local boxes = {
-				"CompactUnitFrameProfilesRaidStylePartyFrames",
-				"CompactUnitFrameProfilesGeneralOptionsFrameKeepGroupsTogether",
-				"CompactUnitFrameProfilesGeneralOptionsFrameHorizontalGroups",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayIncomingHeals",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayPowerBar",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayAggroHighlight",
-				"CompactUnitFrameProfilesGeneralOptionsFrameUseClassColors",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayPets",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayMainTankAndAssist",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayBorder",
-				"CompactUnitFrameProfilesGeneralOptionsFrameShowDebuffs",
-				"CompactUnitFrameProfilesGeneralOptionsFrameDisplayOnlyDispellableDebuffs",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate2Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate3Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate5Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate10Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate15Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate25Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivate40Players",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivateSpec1",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivateSpec2",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivateSpec3",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivateSpec4",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivatePvP",
-				"CompactUnitFrameProfilesGeneralOptionsFrameAutoActivatePvE",
-			}
-			for _, boxName in pairs(boxes) do
-				local checkbox = _G[boxName]
-				if not checkbox then
-					if DB.isDeveloper then print(boxName, "not found.") end
-				else
-					B.ReskinCheck(checkbox)
+		for _, name in pairs(interfacePanels) do
+			local frame = _G[name]
+			if frame then
+				for i = 1, frame:GetNumChildren() do
+					local child = select(i, frame:GetChildren())
+					if child:IsObjectType("CheckButton") then
+						B.ReskinCheck(child)
+					elseif child:IsObjectType("Button") then
+						B.Reskin(child)
+					elseif child:IsObjectType("Slider") then
+						B.ReskinSlider(child)
+					elseif child:IsObjectType("Frame") and child.Left and child.Middle and child.Right then
+						B.ReskinDropDown(child)
+					end
 				end
+			else
+				if DB.isDeveloper then print(name, "not found.") end
 			end
-
-			B.Reskin(CompactUnitFrameProfilesSaveButton)
-			B.Reskin(CompactUnitFrameProfilesDeleteButton)
-			B.Reskin(CompactUnitFrameProfilesGeneralOptionsFrameResetPositionButton)
-			B.ReskinDropDown(CompactUnitFrameProfilesProfileSelector)
-			B.ReskinDropDown(CompactUnitFrameProfilesGeneralOptionsFrameSortByDropdown)
-			B.ReskinDropDown(CompactUnitFrameProfilesGeneralOptionsFrameHealthTextDropdown)
-			B.ReskinSlider(CompactUnitFrameProfilesGeneralOptionsFrameHeightSlider)
-			B.ReskinSlider(CompactUnitFrameProfilesGeneralOptionsFrameWidthSlider)
 		end
 
-		restyled = true
+		styled = true
 	end)
 
 	hooksecurefunc("InterfaceOptions_AddCategory", function()
-		local num = #INTERFACEOPTIONS_ADDONCATEGORIES
-		for i = 1, num do
+		for i = 1, #INTERFACEOPTIONS_ADDONCATEGORIES do
 			local bu = _G["InterfaceOptionsFrameAddOnsButton"..i.."Toggle"]
 			if bu and not bu.styled then
 				B.ReskinCollapse(bu)
--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/VideoOptionsFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/VideoOptionsFrame.lua
@@ -1,27 +1,32 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local function reskinPanelSection(frame)
+	B.StripTextures(frame)
+	B.CreateBDFrame(frame, .25)
+	_G[frame:GetName().."Title"]:SetPoint("BOTTOMLEFT", frame, "TOPLEFT", 5, 2)
+end
+
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
-	B.StripTextures(VideoOptionsFrameCategoryFrame)
-	B.StripTextures(VideoOptionsFramePanelContainer)
-	B.StripTextures(VideoOptionsFrame.Header)
-	VideoOptionsFrame.Header:ClearAllPoints()
-	VideoOptionsFrame.Header:SetPoint("TOP", VideoOptionsFrame, 0, 0)
-
-	B.SetBD(VideoOptionsFrame)
-	VideoOptionsFrame.Border:Hide()
-	B.Reskin(VideoOptionsFrameOkay)
-	B.Reskin(VideoOptionsFrameCancel)
-	B.Reskin(VideoOptionsFrameDefaults)
-	B.Reskin(VideoOptionsFrameApply)
-
-	VideoOptionsFrameOkay:SetPoint("BOTTOMRIGHT", VideoOptionsFrameCancel, "BOTTOMLEFT", -1, 0)
-
 	local styledOptions = false
+
 	VideoOptionsFrame:HookScript("OnShow", function()
 		if styledOptions then return end
+
+		B.StripTextures(VideoOptionsFrameCategoryFrame)
+		B.StripTextures(VideoOptionsFramePanelContainer)
+		B.StripTextures(VideoOptionsFrame.Header)
+		VideoOptionsFrame.Header:ClearAllPoints()
+		VideoOptionsFrame.Header:SetPoint("TOP", VideoOptionsFrame, 0, 0)
+	
+		B.SetBD(VideoOptionsFrame)
+		VideoOptionsFrame.Border:Hide()
+		B.Reskin(VideoOptionsFrameOkay)
+		B.Reskin(VideoOptionsFrameCancel)
+		B.Reskin(VideoOptionsFrameDefaults)
+		B.Reskin(VideoOptionsFrameApply)
 
 		local line = VideoOptionsFrame:CreateTexture(nil, "ARTWORK")
 		line:SetSize(C.mult, 512)
@@ -31,152 +36,45 @@ tinsert(C.defaultThemes, function()
 		Display_:SetBackdrop(nil)
 		Graphics_:SetBackdrop(nil)
 		RaidGraphics_:SetBackdrop(nil)
-
 		GraphicsButton:DisableDrawLayer("BACKGROUND")
 		RaidButton:DisableDrawLayer("BACKGROUND")
+
+		reskinPanelSection(AudioOptionsSoundPanelPlayback)
+		reskinPanelSection(AudioOptionsSoundPanelHardware)
+		reskinPanelSection(AudioOptionsSoundPanelVolume)
 
 		local hline = Display_:CreateTexture(nil, "ARTWORK")
 		hline:SetSize(580, C.mult)
 		hline:SetPoint("TOPLEFT", GraphicsButton, "BOTTOMLEFT", 14, -4)
 		hline:SetColorTexture(1, 1, 1, .2)
 
-		B.StripTextures(AudioOptionsSoundPanelPlayback)
-		B.CreateBDFrame(AudioOptionsSoundPanelPlayback, .25)
-		B.StripTextures(AudioOptionsSoundPanelHardware)
-		B.CreateBDFrame(AudioOptionsSoundPanelHardware, .25)
-		B.StripTextures(AudioOptionsSoundPanelVolume)
-		B.CreateBDFrame(AudioOptionsSoundPanelVolume, .25)
-
-		AudioOptionsSoundPanelPlaybackTitle:SetPoint("BOTTOMLEFT", AudioOptionsSoundPanelPlayback, "TOPLEFT", 5, 2)
-		AudioOptionsSoundPanelHardwareTitle:SetPoint("BOTTOMLEFT", AudioOptionsSoundPanelHardware, "TOPLEFT", 5, 2)
-		AudioOptionsSoundPanelVolumeTitle:SetPoint("BOTTOMLEFT", AudioOptionsSoundPanelVolume, "TOPLEFT", 5, 2)
-
-		local dropdowns = {
-			"Display_DisplayModeDropDown",
-			"Display_ResolutionDropDown",
-			"Display_PrimaryMonitorDropDown",
-			"Display_AntiAliasingDropDown",
-			"Display_VerticalSyncDropDown",
-			"Graphics_TextureResolutionDropDown",
-			"Graphics_SpellDensityDropDown",
-			"Graphics_ProjectedTexturesDropDown",
-			"Graphics_ShadowsDropDown",
-			"Graphics_LiquidDetailDropDown",
-			"Graphics_SunshaftsDropDown",	-- isNewPatch, removed in 38627
-			"Graphics_ParticleDensityDropDown",
-			"Graphics_SSAODropDown",
-			"Graphics_DepthEffectsDropDown",
-			"Graphics_OutlineModeDropDown",
-			"RaidGraphics_TextureResolutionDropDown",
-			"RaidGraphics_SpellDensityDropDown",
-			"RaidGraphics_ProjectedTexturesDropDown",
-			"RaidGraphics_ShadowsDropDown",
-			"RaidGraphics_LiquidDetailDropDown",
-			"RaidGraphics_SunshaftsDropDown",	-- isNewPatch, removed in 38627
-			"RaidGraphics_ParticleDensityDropDown",
-			"RaidGraphics_SSAODropDown",
-			"RaidGraphics_DepthEffectsDropDown",
-			"RaidGraphics_OutlineModeDropDown",
-			"Advanced_BufferingDropDown",
-			"Advanced_FilteringDropDown",
-			"Advanced_RTShadowQualityDropDown",
-			"Advanced_SSAOTypeDropDown",
-			"Advanced_MultisampleAntiAliasingDropDown",
-			"Advanced_MultisampleAlphaTest",
-			"Advanced_PostProcessAntiAliasingDropDown",
-			"Advanced_ResampleQualityDropDown",
-			"Advanced_PhysicsInteractionDropDown",
-			"Advanced_AdapterDropDown",
-			"Advanced_GraphicsAPIDropDown",
-			"AudioOptionsSoundPanelHardwareDropDown",
-			"AudioOptionsSoundPanelSoundChannelsDropDown",
-			"AudioOptionsSoundPanelSoundCacheSizeDropDown",
-			"AudioOptionsVoicePanelOutputDeviceDropdown",
-			"AudioOptionsVoicePanelMicDeviceDropdown",
-			"AudioOptionsVoicePanelChatModeDropdown",
-			"InterfaceOptionsLanguagesPanelLocaleDropDown",
-			"InterfaceOptionsLanguagesPanelAudioLocaleDropDown"
+		local videoPanels = {
+			"Display_",
+			"Graphics_",
+			"RaidGraphics_",
+			"Advanced_",
+			"NetworkOptionsPanel",
+			"InterfaceOptionsLanguagesPanel",
+			"AudioOptionsSoundPanel",
+			"AudioOptionsVoicePanel",
 		}
-		if DB.isNewPatch then
-			tinsert(dropdowns, "Graphics_ComputeEffectsDropDown")
-			tinsert(dropdowns, "RaidGraphics_ComputeEffectsDropDown")
-			tremove(dropdowns, 11)
-			tremove(dropdowns, 20)
-		end
-		for i = 1, #dropdowns do
-			local dropdown = _G[dropdowns[i]]
-			if not dropdown then
-				if DB.isDeveloper then print(dropdowns[i], "not found.") end
+		for _, name in pairs(videoPanels) do
+			local frame = _G[name]
+			if frame then
+				for i = 1, frame:GetNumChildren() do
+					local child = select(i, frame:GetChildren())
+					if child:IsObjectType("CheckButton") then
+						B.ReskinCheck(child)
+					elseif child:IsObjectType("Button") then
+						B.Reskin(child)
+					elseif child:IsObjectType("Slider") then
+						B.ReskinSlider(child)
+					elseif child:IsObjectType("Frame") and child.Left and child.Middle and child.Right then
+						B.ReskinDropDown(child)
+					end
+				end
 			else
-				B.ReskinDropDown(dropdown)
-			end
-		end
-
-		local sliders = {
-			"Display_RenderScaleSlider",
-			"Display_UIScaleSlider",
-			"Graphics_Quality",
-			"Graphics_ViewDistanceSlider",
-			"Graphics_EnvironmentalDetailSlider",
-			"Graphics_GroundClutterSlider",
-			"RaidGraphics_Quality",
-			"RaidGraphics_ViewDistanceSlider",
-			"RaidGraphics_EnvironmentalDetailSlider",
-			"RaidGraphics_GroundClutterSlider",
-			"Advanced_MaxFPSSlider",
-			"Advanced_MaxFPSBKSlider",
-			"Advanced_TargetFPSSlider",
-			"Advanced_GammaSlider",
-			"Advanced_ContrastSlider",
-			"Advanced_BrightnessSlider",
-			"AudioOptionsSoundPanelMasterVolume",
-			"AudioOptionsSoundPanelSoundVolume",
-			"AudioOptionsSoundPanelMusicVolume",
-			"AudioOptionsSoundPanelAmbienceVolume",
-			"AudioOptionsSoundPanelDialogVolume",
-			"AudioOptionsVoicePanelVoiceChatVolume",
-			"AudioOptionsVoicePanelVoiceChatDucking",
-			"AudioOptionsVoicePanelVoiceChatMicVolume",
-			"AudioOptionsVoicePanelVoiceChatMicSensitivity",
-		}
-		for i = 1, #sliders do
-			local slider = _G[sliders[i]]
-			if not slider then
-				if DB.isDeveloper then print(sliders[i], "not found.") end
-			else
-				B.ReskinSlider(slider)
-			end
-		end
-
-		local checkboxes = {
-			"Display_UseUIScale",
-			"Display_RaidSettingsEnabledCheckBox",
-			"Advanced_MaxFPSCheckBox",
-			"Advanced_MaxFPSBKCheckBox",
-			"Advanced_TargetFPSCheckBox",
-			"NetworkOptionsPanelOptimizeSpeed",
-			"NetworkOptionsPanelUseIPv6",
-			"NetworkOptionsPanelAdvancedCombatLogging",
-			"AudioOptionsSoundPanelEnableSound",
-			"AudioOptionsSoundPanelSoundEffects",
-			"AudioOptionsSoundPanelErrorSpeech",
-			"AudioOptionsSoundPanelEmoteSounds",
-			"AudioOptionsSoundPanelPetSounds",
-			"AudioOptionsSoundPanelMusic",
-			"AudioOptionsSoundPanelLoopMusic",
-			"AudioOptionsSoundPanelPetBattleMusic",
-			"AudioOptionsSoundPanelAmbientSounds",
-			"AudioOptionsSoundPanelDialogSounds",
-			"AudioOptionsSoundPanelSoundInBG",
-			"AudioOptionsSoundPanelReverb",
-			"AudioOptionsSoundPanelHRTF",
-		}
-		for i = 1, #checkboxes do
-			local checkbox = _G[checkboxes[i]]
-			if not checkbox then
-				if DB.isDeveloper then print(checkbox[i], "not found.") end
-			else
-				B.ReskinCheck(checkbox)
+				if DB.isDeveloper then print(name, "not found.") end
 			end
 		end
 
@@ -185,8 +83,7 @@ tinsert(C.defaultThemes, function()
 		B.StripTextures(testInputDevie.VUMeter)
 		testInputDevie.VUMeter.Status:SetStatusBarTexture(DB.bdTex)
 		local bg = B.CreateBDFrame(testInputDevie.VUMeter, .3)
-		bg:SetPoint("TOPLEFT", 4, -4)
-		bg:SetPoint("BOTTOMRIGHT", -4, 4)
+		bg:SetInside(nil, 4, 4)
 
 		styledOptions = true
 	end)
@@ -197,13 +94,4 @@ tinsert(C.defaultThemes, function()
 			self.styled = true
 		end
 	end)
-
-	-- Deprecated
-	B.StripTextures(AudioOptionsFrame.Header)
-	AudioOptionsFrame.Header:ClearAllPoints()
-	AudioOptionsFrame.Header:SetPoint("TOP", AudioOptionsFrame, 0, 0)
-	B.SetBD(AudioOptionsFrame)
-	B.Reskin(AudioOptionsFrameOkay)
-	B.Reskin(AudioOptionsFrameCancel)
-	B.Reskin(AudioOptionsFrameDefaults)
 end)
