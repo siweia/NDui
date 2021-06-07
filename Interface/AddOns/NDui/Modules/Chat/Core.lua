e6d4ee6c06c66b1fce500eda1c84da840b8041c0
siweia <siwei1990@outlook.com>
2021-06-07 23:46:29 +0800

    Hide chatframe textures

--- Interface/AddOns/NDui/Modules/Chat/Core.lua
+++ Interface/AddOns/NDui/Modules/Chat/Core.lua
@@ -56,7 +56,7 @@ local function GradientBackground(self)
 	frame:SetFrameLevel(0)
 	frame:SetShown(C.db["Chat"]["ChatBGType"] == 3)
 
-	local tex = B.SetGradient(frame, "H", 0, 0, 0, .5, 0)
+	local tex = B.SetGradient(frame, "H", 0, 0, 0, .7, 0)
 	tex:SetOutside()
 	local line = B.SetGradient(frame, "H", cr, cg, cb, .5, 0, nil, C.mult)
 	line:SetPoint("BOTTOMLEFT", frame, "TOPLEFT")
@@ -107,10 +107,21 @@ function module:SkinChat()
 	B.HideObject(self.buttonFrame)
 	B.HideObject(self.ScrollBar)
 	B.HideObject(self.ScrollToBottomButton)
+	module:ToggleChatFrameTextures(self)
 
 	self.oldAlpha = self.oldAlpha or 0 -- fix blizz error
 
 	self.styled = true
+end
+
+function module:ToggleChatFrameTextures(frame)
+	if C.db["Chat"]["ChatBGType"] == 1 then
+		frame:EnableDrawLayer("BORDER")
+		frame:EnableDrawLayer("BACKGROUND")
+	else
+		frame:DisableDrawLayer("BORDER")
+		frame:DisableDrawLayer("BACKGROUND")
+	end
 end
 
 function module:ToggleChatBackground()
@@ -122,6 +133,7 @@ function module:ToggleChatBackground()
 		if frame.__gradient then
 			frame.__gradient:SetShown(C.db["Chat"]["ChatBGType"] == 3)
 		end
+		module:ToggleChatFrameTextures(frame)
 	end
 end
 
