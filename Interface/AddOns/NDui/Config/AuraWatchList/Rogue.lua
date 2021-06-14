91e2dc3c8416652e633cb2487f27272882e1e0f0
siweia <siwei1990@outlook.com>
2021-06-14 22:16:20 +0800

    Update Rogue aura #147

--- Interface/AddOns/NDui/Config/AuraWatchList/Rogue.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/Rogue.lua
@@ -6,6 +6,7 @@ local module = B:GetModule("AurasTable")
 local list = {
 	["Player Aura"] = {		-- 玩家光环组
 		{AuraID = 1784, UnitID = "player"},		-- 潜行
+		{AuraID = 115191, UnitID = "player"},	-- 潜行
 		{AuraID = 2983, UnitID = "player"},		-- 疾跑
 		{AuraID = 36554, UnitID = "player"},	-- 暗影步
 		{AuraID = 197603, UnitID = "player"},	-- 黑暗之拥
