91ae672fc9a956a26eb76b544eaddcf758629f7e
siweia <siwei1990@outlook.com>
2021-06-25 18:32:01 +0800

    Enum in pet battle

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/PetBattleUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/PetBattleUI.lua
@@ -1,6 +1,8 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local LE_BATTLE_PET_ALLY = LE_BATTLE_PET_ALLY or Enum.BattlePetOwner.Ally -- isNewPatch, deprecated in 9.1
+
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["PetBattle"] then return end
 
