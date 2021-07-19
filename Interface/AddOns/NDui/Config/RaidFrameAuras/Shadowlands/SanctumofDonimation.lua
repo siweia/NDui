838299808b9a6cd4f46dd5f4482ba3ecaca5ced4
siweia <siwei1990@outlook.com>
2021-07-20 00:25:41 +0800

    Update auras

--- Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/SanctumofDonimation.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/Shadowlands/SanctumofDonimation.lua
@@ -10,8 +10,7 @@ BOSS = 2435 -- 塔拉格鲁
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 347283) -- 捕食者之嚎
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 347286) -- 不散之惧
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 346986) -- 粉碎护甲
-module:RegisterDebuff(TIER, INSTANCE, BOSS, 347991) -- 高塔之十
-module:RegisterDebuff(TIER, INSTANCE, BOSS, 347269) -- 永恒锁链
+module:RegisterDebuff(TIER, INSTANCE, BOSS, 347269, 6) -- 永恒锁链
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 346985) -- 压制
 module:RegisterDebuff(TIER, INSTANCE, BOSS, 347274) -- 毁灭猛击
 
