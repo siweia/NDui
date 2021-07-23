cc96f767f2e07c35f9abb0a3ba066ea8f64cc853
siweia <siwei1990@outlook.com>
2021-07-24 02:53:07 +0800

    Single file

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -30,7 +30,6 @@ local C_UIWidgetManager_GetDiscreteProgressStepsVisualizationInfo = C_UIWidgetMa
 local C_UIWidgetManager_GetTextureWithAnimationVisualizationInfo = C_UIWidgetManager.GetTextureWithAnimationVisualizationInfo
 local C_QuestLog_GetLogIndexForQuestID = C_QuestLog.GetLogIndexForQuestID
 local GetOverrideBarSkin, GetActionInfo, GetSpellInfo = GetOverrideBarSkin, GetActionInfo, GetSpellInfo
-local GetItemCount, GetSocketTypes, GetExistingSocketInfo = GetItemCount, GetSocketTypes, GetExistingSocketInfo
 
 --[[
 	Miscellaneous 各种有用没用的小玩意儿
@@ -70,7 +69,6 @@ function M:OnLogin()
 	M:FasterMovieSkip()
 	M:EnhanceDressup()
 	M:FuckTrainSound()
-	M:DomiExtractor()
 
 	-- Unregister talent event
 	if PlayerTalentFrame then
@@ -776,121 +774,4 @@ function M:FuckTrainSound()
 	for _, soundID in pairs(trainSounds) do
 		MuteSoundFile(soundID)
 	end
-end
-
--- Domination shards select and drop
-local TT = B:GetModule("Tooltip")
-local EXTRACTOR_ID = 187532
-local foundShards = {}
-
-function M:DomiShard_Equip()
-	if not self.itemLink then return end
-
-	PickupContainerItem(self.bagID, self.slotID)
-	ClickSocketButton(1)
-	ClearCursor()
-end
-
-function M:DomiShard_ShowTooltip()
-	if not self.itemLink then return end
-
-	GameTooltip:ClearLines()
-	GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT")
-	GameTooltip:SetHyperlink(self.itemLink)
-	GameTooltip:Show()
-end
-
-function M:DomiShards_Refresh()
-	wipe(foundShards)
-
-	for bagID = 0, 4 do
-		for slotID = 1, GetContainerNumSlots(bagID) do
-			local _, _, _, _, _, _, itemLink, _, _, itemID = GetContainerItemInfo(bagID, slotID)
-			local rank = itemID and TT.DomiRankData[itemID]
-			if rank then
-				local index = TT.DomiIndexData[itemID]
-				if not index then break end
-
-				local button = M.DomiShardsFrame.icons[index]
-				button.bagID = bagID
-				button.slotID = slotID
-				button.itemLink = itemLink
-				button.count:SetText(rank)
-				button.Icon:SetDesaturated(false)
-
-				foundShards[index] = true
-			end
-		end
-	end
-
-	for index, button in pairs(M.DomiShardsFrame.icons) do
-		if not foundShards[index] then
-			button.itemLink = nil
-			button.count:SetText("")
-			button.Icon:SetDesaturated(true)
-		end
-	end
-end
-
-function M:DomiShards_ListFrame()
-	if M.DomiShardsFrame then return end
-
-	local frame = CreateFrame("Frame", "NDuiDomiShards", ItemSocketingFrame)
-	frame:SetSize(102, 102)
-	frame:SetPoint("RIGHT", -35, 30)
-	frame.icons = {}
-	M.DomiShardsFrame = frame
-
-	for index, value in pairs(TT.DomiDataByGroup) do
-		for itemID in pairs(value) do
-			local button = CreateFrame("Button", nil, frame)
-			button:SetSize(30, 30)
-			button:SetPoint("TOPLEFT", 3 + mod(index-1, 3)*33, -3 - floor((index-1) / 3)*33)
-			B.PixelIcon(button, GetItemIcon(itemID), true)
-			button:SetScript("OnClick", M.DomiShard_Equip)
-			button:SetScript("OnEnter", M.DomiShard_ShowTooltip)
-			button:SetScript("OnLeave", B.HideTooltip)
-			button.count = B.CreateFS(button, 12, "", "system", "BOTTOMRIGHT", -3, 3)
-
-			frame.icons[index] = button
-			break
-		end
-	end
-
-	M:DomiShards_Refresh()
-	B:RegisterEvent("BAG_UPDATE", M.DomiShards_Refresh)
-end
-
-function M:DomiShards_ExtractButton()
-	if M.DomiExtButton then return end
-	if GetItemCount(EXTRACTOR_ID) == 0 then return end
-	ItemSocketingSocketButton:SetWidth(80)
-	if InCombatLockdown() then return end
-
-	local button = CreateFrame("Button", "NDuiExtractorButton", ItemSocketingFrame, "UIPanelButtonTemplate, SecureActionButtonTemplate")
-	button:SetSize(80, 22)
-	button:SetText(L["Drop"])
-	button:SetPoint("RIGHT", ItemSocketingSocketButton, "LEFT", -3, 0)
-	button:SetAttribute("type", "macro")
-	button:SetAttribute("macrotext", "/use item:"..EXTRACTOR_ID.."\n/click ItemSocketingSocket1")
-	if C.db["Skins"]["BlizzardSkins"] then B.Reskin(button) end
-
-	M.DomiExtButton = button
-end
-
-function M:DomiExtractor()
-	hooksecurefunc("ItemSocketingFrame_LoadUI", function()
-		if not ItemSocketingFrame then return end
-
-		M:DomiShards_ListFrame()
-		M:DomiShards_ExtractButton()
-
-		if M.DomiExtButton then
-			M.DomiExtButton:SetAlpha(GetSocketTypes(1) == "Domination" and GetExistingSocketInfo(1) and 1 or 0)
-		end
-
-		if M.DomiShardsFrame then
-			M.DomiShardsFrame:SetShown(GetSocketTypes(1) == "Domination" and not GetExistingSocketInfo(1))
-		end
-	end)
 end
