2ad5a9a7d1ba0076f1a2a68e8e5a2e8b0c4b2393
siweia <siwei1990@outlook.com>
2021-11-27 11:12:28 +0800

    LibShowUIPanel v6

--- Interface/AddOns/NDui/Libs/LibShowUIPanel-1.0.lua
+++ Interface/AddOns/NDui/Libs/LibShowUIPanel-1.0.lua
@@ -3,21 +3,11 @@
 -- @Link   : https://dengsir.github.io
 -- @Date   : 6/15/2021, 11:20:01 PM
 --
-local MAJOR, MINOR = 'LibShowUIPanel-1.0', 5
+local MAJOR, MINOR = 'LibShowUIPanel-1.0', 6
 
 ---@class LibShowUIPanel-1.0
 local Lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 if not Lib then
-    return
-end
-
-if WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then
-    Lib.ShowUIPanel = ShowUIPanel
-    Lib.HideUIPanel = HideUIPanel
-    Lib.ToggleFrame = ToggleFrame
-    Lib.Show = ShowUIPanel
-    Lib.Hide = HideUIPanel
-    Lib.Toggle = ToggleFrame
     return
 end
 
@@ -106,7 +96,7 @@ function Lib.Toggle(frame)
     end
 end
 
-if not oldminor or oldminor < 3 then
+if not oldminor or oldminor < 6 then
     -- 可长期持有的API
     function Lib.ShowUIPanel(frame, force)
         return Lib.Show(frame, force)
