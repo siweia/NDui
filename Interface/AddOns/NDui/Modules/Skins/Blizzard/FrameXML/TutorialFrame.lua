79aafedcc2c9f017cf8e558480ba3e8c4adfb909
siweia <siwei1990@outlook.com>
2021-12-03 01:43:00 +0800

    Skins: cleanup TutorialFrame

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/TutorialFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/TutorialFrame.lua
@@ -18,14 +18,6 @@ tinsert(C.defaultThemes, function()
 	TutorialFrameOkayButton:ClearAllPoints()
 	TutorialFrameOkayButton:SetPoint("BOTTOMLEFT", TutorialFrameNextButton, "BOTTOMRIGHT", 10, 0)
 
-	-- because gradient alpha and OnUpdate doesn't work for some reason...
-
-	if select(14, TutorialFrameOkayButton:GetRegions()) then
-		select(14, TutorialFrameOkayButton:GetRegions()):Hide()
-		select(15, TutorialFramePrevButton:GetRegions()):Hide()
-		select(15, TutorialFrameNextButton:GetRegions()):Hide()
-		select(14, TutorialFrameCloseButton:GetRegions()):Hide()
-	end
 	TutorialFramePrevButton:SetScript("OnEnter", nil)
 	TutorialFrameNextButton:SetScript("OnEnter", nil)
 	TutorialFrameOkayButton.__bg:SetBackdropColor(0, 0, 0, .25)
