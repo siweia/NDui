3fa759cbe77b40b35ba3133ff035d46e252288da
siweia <siwei1990@outlook.com>
2021-12-05 14:56:03 +0800

    Hide option stack right bars

--- Interface/AddOns/NDui/Modules/ActionBar/Bars/Hide_blizzart.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/Hide_blizzart.lua
@@ -78,6 +78,10 @@ function Bar:HideBlizz()
 		DisableAllScripts(frame)
 	end
 
+	-- Hide blizz options
+	SetCVar("multiBarRightVerticalLayout", 0)
+	InterfaceOptionsActionBarsPanelStackRightBars:EnableMouse(false)
+	InterfaceOptionsActionBarsPanelStackRightBars:SetAlpha(0)
 	-- Fix maw block anchor
 	MainMenuBarVehicleLeaveButton:RegisterEvent("PLAYER_ENTERING_WORLD")
 	-- Update button grid
