e917f1a8b82aa446d74cdde8f0f79727fee85769
siweia <siwei1990@outlook.com>
2021-12-22 01:23:27 +0800

    Remove WardrobeFrame from moving list

--- Interface/AddOns/NDui/Plugins/DragEmAll.lua
+++ Interface/AddOns/NDui/Plugins/DragEmAll.lua
@@ -72,7 +72,7 @@ local lodFrames = {
 	Blizzard_BlackMarketUI		= { ["BlackMarketFrame"] = false },
 	Blizzard_Calendar			= { ["CalendarFrame"] = false, ["CalendarCreateEventFrame"] = true, ["CalendarEventPickerFrame"] = false },
 	Blizzard_ChallengesUI		= { ["ChallengesKeystoneFrame"] = false },
-	Blizzard_Collections		= { ["WardrobeFrame"] = false, ["WardrobeOutfitEditFrame"] = false },
+	--Blizzard_Collections		= { ["WardrobeFrame"] = false, ["WardrobeOutfitEditFrame"] = false }, -- FIXME: blizz bug in collection mover
 	Blizzard_CovenantRenown		= { ["CovenantRenownFrame"] = false, },
 	Blizzard_CovenantSanctum	= { ["CovenantSanctumFrame"] = false, },
 	Blizzard_Communities		= { ["CommunitiesFrame"] = false, ["CommunitiesSettingsDialog"] = false, ["CommunitiesGuildLogFrame"] = false, ["CommunitiesTicketManagerDialog"] = false, ["CommunitiesAvatarPickerDialog"] = false, ["CommunitiesFrame.NotificationSettingsDialog"] = false, ["ClubFinderCommunityAndGuildFinderFrame.RequestToJoinFrame"] = false},
