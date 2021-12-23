666b0b9079e3abbc2e0bef3b4ae4cc3e508972d7
siweia <siwei1990@outlook.com>
2021-12-23 23:03:39 +0800

    Add skin for MRT timers

--- Interface/AddOns/NDui/Modules/Skins/AddOns/Others.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/Others.lua
@@ -74,10 +74,53 @@ function S:SoulbindsTalents()
 	end)
 end
 
+local function restyleMRTWidget(self)
+	local iconTexture = self.iconTexture
+	local bar = self.statusbar
+
+	if not self.styled then
+		B.SetBD(iconTexture)
+		self.__bg = B.SetBD(bar)
+		self.background:SetAllPoints(bar)
+
+		self.styled = true
+	end
+	iconTexture:SetTexCoord(unpack(DB.TexCoord))
+	self.__bg:SetShown(bar:GetWidth() ~= 0)
+
+	local parent = self.parent
+	if parent.optionIconPosition == 3 or parent.optionIconTitles then
+		-- do nothing
+	elseif parent.optionIconPosition == 2 then
+		self.icon:SetPoint("RIGHT", self, 3, 0)
+	else
+		self.icon:SetPoint("LEFT", self, -3, 0)
+	end
+end
+
+function S:MRT_Skin()
+	if not IsAddOnLoaded("MRT") then return end
+
+	local name = "MRTRaidCooldownCol"
+	for i = 1, 10 do
+		local column = _G[name..i]
+		local lines = column and column.lines
+		if lines then
+			for j = 1, #lines do
+				local line = lines[j]
+				if line.UpdateStyle then
+					hooksecurefunc(line, "UpdateStyle", restyleMRTWidget)
+				end
+			end
+		end
+	end
+end
+
 function S:OtherSkins()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
 	S:FriendGroups()
 	S:PostalSkin()
 	S:SoulbindsTalents()
+	S:MRT_Skin()
 end
