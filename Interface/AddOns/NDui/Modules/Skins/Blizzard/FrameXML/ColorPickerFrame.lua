09a45366a85786ad7866ee508a3edef2831b410a
siweia <siwei1990@outlook.com>
2022-01-05 10:35:15 +0800

    Skins: centralize buttons in color picker

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ColorPickerFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ColorPickerFrame.lua
@@ -6,11 +6,16 @@ tinsert(C.defaultThemes, function()
 
 	B.StripTextures(ColorPickerFrame.Header)
 	ColorPickerFrame.Header:ClearAllPoints()
-	ColorPickerFrame.Header:SetPoint("TOP", ColorPickerFrame, 0, 0)
+	ColorPickerFrame.Header:SetPoint("TOP", ColorPickerFrame, 0, 10)
 	ColorPickerFrame.Border:Hide()
 
 	B.SetBD(ColorPickerFrame)
 	B.Reskin(ColorPickerOkayButton)
 	B.Reskin(ColorPickerCancelButton)
 	B.ReskinSlider(OpacitySliderFrame, true)
+
+	ColorPickerCancelButton:ClearAllPoints()
+	ColorPickerCancelButton:SetPoint("BOTTOMLEFT", ColorPickerFrame, "BOTTOM", 1, 6)
+	ColorPickerOkayButton:ClearAllPoints()
+	ColorPickerOkayButton:SetPoint("BOTTOMRIGHT", ColorPickerFrame, "BOTTOM", -1, 6)
 end)
