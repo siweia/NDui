4747adfd262437e9877a68b7a5e17a3614326a47
siweia <siwei1990@outlook.com>
2022-02-25 23:14:08 +0800

    Replace encrypted affix spell textures

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -758,6 +758,12 @@ local filteredStyle = {
 	["arena"] = true,
 }
 
+local replaceEncryptedIcons = {
+	[368078] = 348567, -- 移速
+	[368079] = 348567, -- 移速
+	[368103] = 648208, -- 急速
+	[368243] = 237538, -- CD
+}
 function UF.PostUpdateIcon(element, _, button, _, _, duration, expiration, debuffType)
 	if duration then button.iconbg:Show() end
 
@@ -793,6 +799,11 @@ function UF.PostUpdateIcon(element, _, button, _, _, duration, expiration, debuf
 			button:SetScript("OnUpdate", nil)
 			button.timer:Hide()
 		end
+	end
+
+	local newTexture = replaceEncryptedIcons[button.spellID]
+	if newTexture then
+		button.icon:SetTexture(newTexture)
 	end
 end
 
