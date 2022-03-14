9a7bf40c346be4e7dcc43f9a9073821ae7a8816c
siweia <siwei1990@outlook.com>
2022-03-15 01:11:57 +0800

    Update data transfer

--- Interface/AddOns/NDui/Core/ProfileGUI.lua
+++ Interface/AddOns/NDui/Core/ProfileGUI.lua
@@ -474,6 +474,8 @@ function G:ExportGUIData()
 			for k, v in pairs(VALUE) do
 				text = text..";ACCOUNT:"..KEY..":"..k..":"..v
 			end
+		elseif VALUE == true or VALUE == false then
+			text = text..";ACCOUNT:"..KEY..":"..tostring(VALUE)
 		end
 	end
 
@@ -520,7 +522,11 @@ function G:ImportGUIData()
 		local option = options[i]
 		local key, value, arg1 = strsplit(":", option)
 		if arg1 == "true" or arg1 == "false" then
-			C.db[key][value] = toBoolean(arg1)
+			if key == "ACCOUNT" then
+				NDuiADB[value] = toBoolean(arg1)
+			else
+				C.db[key][value] = toBoolean(arg1)
+			end
 		elseif arg1 == "EMPTYTABLE" then
 			C.db[key][value] = {}
 		elseif strfind(value, "Color") and (arg1 == "r" or arg1 == "g" or arg1 == "b") then
