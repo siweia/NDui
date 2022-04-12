85473c2b5eb9b1d3f415c698ad253e9534200f88
siweia <siwei1990@outlook.com>
2022-04-12 17:06:59 +0800

    Only pass thru auras of current class

--- Interface/AddOns/NDui/Config/AuraWatchList/Priest.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/Priest.lua
@@ -2,6 +2,8 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 local module = B:GetModule("AurasTable")
 
+if DB.MyClass ~= "PRIEST" then return end
+
 -- 牧师的法术监控
 local list = {
 	["Player Aura"] = {		-- 玩家光环组
