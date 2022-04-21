d9ca9dd294d7c942db664dfcdeee9a32c97497be
siweia <siwei1990@outlook.com>
2022-04-22 00:28:30 +0800

    Exclude boss casting time

--- Interface/AddOns/NDui/Config/AuraWatchList/All.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/All.lua
@@ -403,7 +403,7 @@ local list = {
 		{IntID = 352875, Duration = 30},	-- 格里恩，虔敬者之路
 		{IntID = 114018, Duration = 15, OnSuccess = true, UnitID = "all"},	-- 帷幕
 		{IntID = 316958, Duration = 30, OnSuccess = true, UnitID = "all"},	-- 红土
-		{IntID = 353635, Duration = 30, OnSuccess = true, UnitID = "all"},	-- 坍缩之星自爆时间
+		{IntID = 353635, Duration = 27.5, OnSuccess = true, UnitID = "all"},	-- 坍缩之星自爆时间
 	},
 }
 
