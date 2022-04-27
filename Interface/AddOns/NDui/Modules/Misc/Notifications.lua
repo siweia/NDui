7a1a008b3c1f5163d14512aeb817d17d849bd406
siweia <siwei1990@outlook.com>
2022-04-27 14:50:06 +0800

    Remove timestamp check, needs review

--- Interface/AddOns/NDui/Modules/Misc/Notifications.lua
+++ Interface/AddOns/NDui/Modules/Misc/Notifications.lua
@@ -427,7 +427,6 @@ for i = 1, 40 do
 	groupUnits["raidpet"..i] = true
 end
 
-local lastTime = 0
 local itemList = {
 	[54710] = true,		-- 随身邮箱
 	[67826] = true,		-- 基维斯
@@ -459,11 +458,8 @@ local itemList = {
 }
 
 function M:ItemAlert_Update(unit, _, spellID)
-	local now = GetTime()
-	if groupUnits[unit] and itemList[spellID] and lastTime ~= now then
+	if groupUnits[unit] and itemList[spellID] then
 		SendChatMessage(format(L["SpellItemAlertStr"], UnitName(unit), GetSpellLink(spellID) or GetSpellInfo(spellID)), msgChannel())
-
-		lastTime = now
 	end
 end
 
