0abecc4ad6646c40e82c9d0024085da813e18a67
siweia <siwei1990@outlook.com>
2022-05-14 21:27:49 +0800

    Scroll end size tweaks

--- Interface/AddOns/NDui/Modules/Skins/AddOns/Rematch.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/Rematch.lua
@@ -55,7 +55,7 @@ end
 
 function S:ReskinScrollEnd(direction)
 	B.ReskinArrow(self, direction)
-	self:SetSize(17, 12)
+	self:SetSize(16, 12)
 	self.__texture:SetVertexColor(1, .8, 0)
 	self:HookScript("OnLeave", scrollEndOnLeave)
 end
