ae7da96b0ef9cfab1e512a46e6cb149bf6379bcb
siweia <siwei1990@outlook.com>
2022-06-04 18:37:49 +0800

    Update oUF version tag

--- Interface/AddOns/NDui/Libs/oUF/ouf.lua
+++ Interface/AddOns/NDui/Libs/oUF/ouf.lua
@@ -1,6 +1,6 @@
 local parent, ns = ...
 local global = GetAddOnMetadata(parent, 'X-oUF')
-local _VERSION = '@project-version@'
+local _VERSION = '10.1.1'
 if(_VERSION:find('project%-version')) then
 	_VERSION = 'devel'
 end
