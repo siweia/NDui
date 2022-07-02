5828594423f4308fe5bfb8a74286f5e9a1185952
siweia <siwei1990@outlook.com>
2022-07-02 15:28:01 +0800

    Reparent chargeStars

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -1336,11 +1336,16 @@ function UF:CreateClassPower(self)
 		if isDK then
 			bars[i].timer = B.CreateFS(bars[i], 13, "")
 		elseif DB.MyClass == "ROGUE" then
-			local chargeStar = bars[i]:CreateTexture()
+			if not bar.chargeParent then
+				bar.chargeParent = CreateFrame("Frame", nil, bar)
+				bar.chargeParent:SetAllPoints()
+				bar.chargeParent:SetFrameLevel(8)
+			end
+			local chargeStar = bar.chargeParent:CreateTexture()
 			chargeStar:SetAtlas("VignetteKill")
 			chargeStar:SetDesaturated(true)
 			chargeStar:SetSize(22, 22)
-			chargeStar:SetPoint("CENTER")
+			chargeStar:SetPoint("CENTER", bars[i])
 			chargeStar:Hide()
 			bars[i].chargeStar = chargeStar
 		end
