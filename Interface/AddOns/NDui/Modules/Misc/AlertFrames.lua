9253ab8dea3c9ee5feba44d7fe4893f403becd78
siweia <siwei1990@outlook.com>
2022-09-01 19:23:45 +0800

    Update mover for alert frames

--- Interface/AddOns/NDui/Modules/Misc/AlertFrames.lua
+++ Interface/AddOns/NDui/Modules/Misc/AlertFrames.lua
@@ -3,23 +3,29 @@ local B, C, L, DB = unpack(ns)
 local M = B:GetModule("Misc")
 
 local _G = getfenv(0)
+local ipairs = ipairs
 local UIParent = _G.UIParent
+local AlertFrame = _G.AlertFrame
 local GroupLootContainer = _G.GroupLootContainer
 
-local POSITION, YOFFSET = "TOP", -10
+local POSITION, ANCHOR_POINT, YOFFSET = "TOP", "BOTTOM", -10
 local parentFrame
 
-function M:GroupLootContainer_UpdateAnchor()
+function M:AlertFrame_UpdateAnchor()
 	local y = select(2, parentFrame:GetCenter())
 	local screenHeight = UIParent:GetTop()
 	if y > screenHeight/2 then
 		POSITION = "TOP"
+		ANCHOR_POINT = "BOTTOM"
 		YOFFSET = -10
 	else
 		POSITION = "BOTTOM"
+		ANCHOR_POINT = "TOP"
 		YOFFSET = 10
 	end
 
+	self:ClearAllPoints()
+	self:SetPoint(POSITION, parentFrame)
 	GroupLootContainer:ClearAllPoints()
 	GroupLootContainer:SetPoint(POSITION, parentFrame)
 end
@@ -44,6 +50,48 @@ function M:UpdatGroupLootContainer()
 	end
 end
 
+function M:AlertFrame_SetPoint(relativeAlert)
+	self:ClearAllPoints()
+	self:SetPoint(POSITION, relativeAlert, ANCHOR_POINT, 0, YOFFSET)
+end
+
+function M:AlertFrame_AdjustQueuedAnchors(relativeAlert)
+	for alertFrame in self.alertFramePool:EnumerateActive() do
+		M.AlertFrame_SetPoint(alertFrame, relativeAlert)
+		relativeAlert = alertFrame
+	end
+
+	return relativeAlert
+end
+
+function M:AlertFrame_AdjustAnchors(relativeAlert)
+	if self.alertFrame:IsShown() then
+		M.AlertFrame_SetPoint(self.alertFrame, relativeAlert)
+		return self.alertFrame
+	end
+
+	return relativeAlert
+end
+
+function M:AlertFrame_AdjustAnchorsNonAlert(relativeAlert)
+	if self.anchorFrame:IsShown() then
+		M.AlertFrame_SetPoint(self.anchorFrame, relativeAlert)
+		return self.anchorFrame
+	end
+
+	return relativeAlert
+end
+
+function M:AlertFrame_AdjustPosition()
+	if self.alertFramePool then
+		self.AdjustAnchors = M.AlertFrame_AdjustQueuedAnchors
+	elseif not self.anchorFrame then
+		self.AdjustAnchors = M.AlertFrame_AdjustAnchors
+	elseif self.anchorFrame then
+		self.AdjustAnchors = M.AlertFrame_AdjustAnchorsNonAlert
+	end
+end
+
 function M:AlertFrame_Setup()
 	parentFrame = CreateFrame("Frame", nil, UIParent)
 	parentFrame:SetSize(200, 30)
@@ -52,8 +100,15 @@ function M:AlertFrame_Setup()
 	GroupLootContainer:EnableMouse(false)
 	GroupLootContainer.ignoreFramePositionManager = true
 
-	M:GroupLootContainer_UpdateAnchor()
-	hooksecurefunc("GroupLootFrame_OpenNewFrame", M.GroupLootContainer_UpdateAnchor)
+	for _, alertFrameSubSystem in ipairs(AlertFrame.alertFrameSubSystems) do
+		M.AlertFrame_AdjustPosition(alertFrameSubSystem)
+	end
+
+	hooksecurefunc(AlertFrame, "AddAlertFrameSubSystem", function(_, alertFrameSubSystem)
+		M.AlertFrame_AdjustPosition(alertFrameSubSystem)
+	end)
+
+	hooksecurefunc(AlertFrame, "UpdateAnchors", M.AlertFrame_UpdateAnchor)
 	hooksecurefunc("GroupLootContainer_Update", M.UpdatGroupLootContainer)
 end
 M:RegisterMisc("AlertFrame", M.AlertFrame_Setup)
