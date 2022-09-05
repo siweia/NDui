42e3d783282cf5892b9dd10c790e91152b9c925a
siweia <siwei1990@outlook.com>
2022-09-05 12:58:13 +0800

    Fix mover anchor when 'SpecRaidPos' on, by using mover control

--- Interface/AddOns/NDui/Modules/UFs/Spawns.lua
+++ Interface/AddOns/NDui/Modules/UFs/Spawns.lua
@@ -786,14 +786,14 @@ function UF:OnLogin()
 			B:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED", UpdateSpecPos)
 
 			if raidMover then
-				raidMover:HookScript("OnDragStop", function()
+				hooksecurefunc(raidMover, "SetPoint", function()
 					local specIndex = GetSpecialization()
 					if not specIndex then return end
 					C.db["Mover"]["RaidPos"..specIndex] = C.db["Mover"]["RaidFrame"]
 				end)
 			end
 			if partyMover then
-				partyMover:HookScript("OnDragStop", function()
+				hooksecurefunc(partyMover, "SetPoint", function()
 					local specIndex = GetSpecialization()
 					if not specIndex then return end
 					C.db["Mover"]["PartyPos"..specIndex] = C.db["Mover"]["PartyFrame"]
