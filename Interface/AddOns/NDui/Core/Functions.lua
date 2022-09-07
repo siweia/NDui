b2c5f3e0f6bd371bba014a10b881120face8daf0
siweia <siwei1990@outlook.com>
2022-09-07 10:21:00 +0800

    Skin code tweak

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -1021,8 +1021,8 @@ do
 
 		local thumb = self:GetThumb()
 		if thumb then
-			B.StripTextures(thumb, 0)
-			B.CreateBDFrame(thumb, 0):SetBackdropColor(cr, cg, cb, .25)
+			thumb:DisableDrawLayer("BACKGROUND")
+			B.CreateBDFrame(thumb, 0, true):SetBackdropColor(cr, cg, cb, .75)
 		end
 	end
 
