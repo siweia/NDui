4db255da400a6b65604bfc46dee527e02d535968
siweia <siwei1990@outlook.com>
2022-09-12 14:56:14 +0800

    Skins: CraftUI has been merged into TradeSkillUI

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_TradeSkillUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_TradeSkillUI.lua
@@ -1,51 +1,6 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
-C.themes["Blizzard_CraftUI"] = function()
-	B.ReskinPortraitFrame(CraftFrame, 10, -10, -30, 70)
-	B.ReskinScroll(CraftListScrollFrameScrollBar)
-	B.ReskinScroll(CraftDetailScrollFrameScrollBar)
-	B.Reskin(CraftCreateButton)
-	B.Reskin(CraftCancelButton)
-
-	B.StripTextures(CraftRankFrameBorder)
-	B.StripTextures(CraftRankFrame)
-	CraftRankFrame:SetStatusBarTexture(DB.bdTex)
-	CraftRankFrame.SetStatusBarColor = B.Dummy
-	CraftRankFrame:GetStatusBarTexture():SetGradient("VERTICAL", .1, .3, .9, .2, .4, 1)
-	B.CreateBDFrame(CraftRankFrame, .25)
-	CraftRankFrame:SetWidth(220)
-
-	B.StripTextures(CraftDetailScrollChildFrame)
-	B.StripTextures(CraftIcon)
-	B.CreateBDFrame(CraftIcon)
-	B.ReskinCollapse(CraftCollapseAllButton)
-	CraftExpandButtonFrame:DisableDrawLayer("BACKGROUND")
-
-	hooksecurefunc("CraftFrame_SetSelection", function(id)
-		if not id then return end
-		local tex = CraftIcon:GetNormalTexture()
-		if tex then
-			tex:SetTexCoord(.08, .92, .08, .92)
-		end
-	end)
-
-	for i = 1, MAX_CRAFT_REAGENTS do
-		local icon = _G["CraftReagent"..i.."IconTexture"]
-		icon:SetTexCoord(.08, .92, .08, .92)
-		B.CreateBDFrame(icon)
-
-		local nameFrame = _G["CraftReagent"..i.."NameFrame"]
-		nameFrame:Hide()
-		local bg = B.CreateBDFrame(nameFrame, .25)
-		bg:SetPoint("TOPLEFT", icon, "TOPRIGHT", 3, C.mult)
-		bg:SetPoint("BOTTOMRIGHT", icon, "BOTTOMRIGHT", 100, -C.mult)
-	end
-
-	B.ReskinDropDown(CraftFrameFilterDropDown)
-	B.ReskinCheck(CraftFrameAvailableFilterCheckButton)
-end
-
 C.themes["Blizzard_TradeSkillUI"] = function()
 	B.ReskinPortraitFrame(TradeSkillFrame, 10, -10, -30, 70)
 	B.ReskinScroll(TradeSkillListScrollFrameScrollBar)
