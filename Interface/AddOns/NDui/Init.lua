d71b4a9c830a5ff606bc191a08e4c1cbfeaebbf3
siweia <siwei1990@outlook.com>
2022-09-16 02:40:49 +0800

    [10.0] Re-enable bags

--- Interface/AddOns/NDui/Init.lua
+++ Interface/AddOns/NDui/Init.lua
@@ -118,10 +118,6 @@ local function IncorrectExpansion() -- left it for the future
 end
 
 B:RegisterEvent("PLAYER_LOGIN", function()
-	if DB.isNewPatch and not DB.isDeveloper then
-		C.db["Bags"]["Enable"] = false -- todo: why the fuck it taints?
-	end
-
 	-- Initial
 	B:SetupUIScale()
 	B:RegisterEvent("UI_SCALE_CHANGED", UpdatePixelScale)
