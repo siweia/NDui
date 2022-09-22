19f6390c420cbc6275899c07d0a4ca18fa33696a
siweia <siwei1990@outlook.com>
2022-09-22 12:55:23 +0800

    Skins: fix error in 45704

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/BattlefieldFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/BattlefieldFrame.lua
@@ -10,24 +10,26 @@ tinsert(C.defaultThemes, function()
 	BattlefieldFrameBGTex:Hide()
 	B.CreateBDFrame(BattlefieldFrameInfoScrollFrame, .25)
 
-	local wintergraspIcon = WintergraspTimer.texture
-	wintergraspIcon:SetTexture(135865) -- Spell_Frost_WizardMark
-	B.CreateBDFrame(wintergraspIcon, .25)
+	if WintergraspTimer then -- removed in 45704, needs review
+		local wintergraspIcon = WintergraspTimer.texture
+		wintergraspIcon:SetTexture(135865) -- Spell_Frost_WizardMark
+		B.CreateBDFrame(wintergraspIcon, .25)
 
-	hooksecurefunc(wintergraspIcon, "SetTexCoord", function(self, _, _, y1, y2)
-		if self.isCutting then return end
-		self.isCutting = true
+		hooksecurefunc(wintergraspIcon, "SetTexCoord", function(self, _, _, y1, y2)
+			if self.isCutting then return end
+			self.isCutting = true
 
-		if y1 == .5 and y2 == 1 then
-			self:SetDesaturated(false) -- can queue
-		elseif y1 == 0 and y2 == .5 then
-			self:SetDesaturated(true) -- can't queue
-		end
-		self:SetTexCoord(unpack(DB.TexCoord))
+			if y1 == .5 and y2 == 1 then
+				self:SetDesaturated(false) -- can queue
+			elseif y1 == 0 and y2 == .5 then
+				self:SetDesaturated(true) -- can't queue
+			end
+			self:SetTexCoord(unpack(DB.TexCoord))
 
-		self.isCutting = nil
-	end)
-	wintergraspIcon:SetTexCoord(0, 1, 0, .5)
+			self.isCutting = nil
+		end)
+		wintergraspIcon:SetTexCoord(0, 1, 0, .5)
+	end
 
 	-- WorldStateScoreFrame
 	B.ReskinPortraitFrame(WorldStateScoreFrame, 13, -15, -108, 70)
