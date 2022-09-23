fb56ee7f50d7119abc2161a68660abd74a41778f
siweia <siwei1990@outlook.com>
2022-09-23 12:24:58 +0800

    [10.0] Skins: fix HelpFrame error

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/HelpFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/HelpFrame.lua
@@ -6,7 +6,11 @@ tinsert(C.defaultThemes, function()
 
 	B.StripTextures(HelpFrame)
 	B.SetBD(HelpFrame)
-	B.ReskinClose(HelpFrameCloseButton)
+	if DB.isNewPatch then
+		B.ReskinClose(HelpFrame.CloseButton)
+	else
+		B.ReskinClose(HelpFrameCloseButton)
+	end
 	B.StripTextures(HelpBrowser.BrowserInset)
 
 	B.StripTextures(BrowserSettingsTooltip)
