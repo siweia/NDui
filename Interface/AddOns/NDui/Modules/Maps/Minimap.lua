807dcc65817798f159e32c4feabeec57b13d3242
siweia <siwei1990@outlook.com>
2022-10-13 22:38:08 +0800

    [10.0] Correct anchor on minimap elements if flipped

--- Interface/AddOns/NDui/Modules/Maps/Minimap.lua
+++ Interface/AddOns/NDui/Modules/Maps/Minimap.lua
@@ -159,8 +159,15 @@ function module:ReskinRegions()
 
 	-- Difficulty Flags
 	if MinimapCluster.InstanceDifficulty then -- isNewPatch
-		MinimapCluster.InstanceDifficulty:ClearAllPoints()
-		MinimapCluster.InstanceDifficulty:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 2, 2)
+		local function updateFlagAnchor(frame, _, _, _, _, _, force)
+			if force then return end
+			frame:ClearAllPoints()
+			frame:SetPoint("TOPRIGHT", Minimap, "TOPRIGHT", 2, 2, true)
+		end
+		MinimapCluster.InstanceDifficulty:SetParent(Minimap)
+		MinimapCluster.InstanceDifficulty:SetScale(.7)
+		updateFlagAnchor(MinimapCluster.InstanceDifficulty)
+		hooksecurefunc(MinimapCluster.InstanceDifficulty, "SetPoint", updateFlagAnchor)
 	else
 		local flags = {"MiniMapInstanceDifficulty", "GuildInstanceDifficulty", "MiniMapChallengeMode"}
 		for _, v in pairs(flags) do
@@ -651,10 +658,10 @@ function module:SetupMinimap()
 	Minimap:ClearAllPoints()
 	Minimap:SetPoint("TOPRIGHT", mover)
 	if DB.isNewPatch then
-		hooksecurefunc(Minimap, "SetPoint", function(frame, _, parent)
-			if parent ~= mover then
-				frame:SetPoint("TOPRIGHT", mover)
-			end
+		hooksecurefunc(Minimap, "SetPoint", function(frame, _, _, _, _, _, force)
+			if force then return end
+			frame:ClearAllPoints()
+			frame:SetPoint("TOPRIGHT", mover, "TOPRIGHT", 0, 0, true)
 		end)
 	end
 	Minimap.mover = mover
