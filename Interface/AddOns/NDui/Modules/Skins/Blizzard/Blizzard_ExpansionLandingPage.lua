d49a04ee6197c90edcaf35b3b021291a91e99947
siweia <siwei1990@outlook.com>
2022-10-20 18:39:17 +0800

    [10.0] Skins: init ExpansionLandingPage

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_ExpansionLandingPage.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_ExpansionLandingPage.lua
@@ -2,5 +2,41 @@ local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
 C.themes["Blizzard_ExpansionLandingPage"] = function()
-	-- todo
+	local frame = _G.ExpansionLandingPage
+
+	local panel
+
+	if frame.Overlay then
+		for i = 1, frame.Overlay:GetNumChildren() do
+			local child = select(i, frame.Overlay:GetChildren())
+			if child.DragonridingPanel then
+				panel = child
+				break
+			end
+		end
+	end
+
+	if not panel then return end
+
+	if panel.DragonridingPanel then
+		B.Reskin(panel.DragonridingPanel.SkillsButton)
+	end
+	if panel.CloseButton then
+		B.ReskinClose(panel.CloseButton)
+	end
+	if panel.MajorFactionList then
+		hooksecurefunc(panel.MajorFactionList.ScrollBox, "Update", function(self)
+			for i = 1, self.ScrollTarget:GetNumChildren() do
+				local child = select(i, self.ScrollTarget:GetChildren())
+				if child.UnlockedState then
+					if not child.styled then
+						B.ReskinCheck(child.UnlockedState.WatchFactionButton)
+						child.UnlockedState.WatchFactionButton:SetSize(32, 32)
+						child.UnlockedState.WatchFactionButton.Label:SetFontObject(Game20Font)
+						child.styled = true
+					end
+				end
+			end
+		end)
+	end
 end
