c046c9e2cec942d3619c7b07aa1c8880048d7094
siweia <siwei1990@outlook.com>
2022-10-26 12:29:52 +0800

    Cleanup skin for WA

--- Interface/AddOns/NDui/Modules/Skins/AddOns/WeakAuras.lua
+++ Interface/AddOns/NDui/Modules/Skins/AddOns/WeakAuras.lua
@@ -64,32 +64,6 @@ end
 local function ReskinWA()
 	if not C.db["Skins"]["WeakAuras"] then return end
 
-	local regionTypes = WeakAuras.regionTypes
-	local Create_Icon, Modify_Icon = regionTypes.icon.create, regionTypes.icon.modify
-	local Create_AuraBar, Modify_AuraBar = regionTypes.aurabar.create, regionTypes.aurabar.modify
-
-	regionTypes.icon.create = function(parent, data)
-		local region = Create_Icon(parent, data)
-		Skin_WeakAuras(region, "icon")
-		return region
-	end
-
-	regionTypes.aurabar.create = function(parent)
-		local region = Create_AuraBar(parent)
-		Skin_WeakAuras(region, "aurabar")
-		return region
-	end
-
-	regionTypes.icon.modify = function(parent, region, data)
-		Modify_Icon(parent, region, data)
-		Skin_WeakAuras(region, "icon")
-	end
-
-	regionTypes.aurabar.modify = function(parent, region, data)
-		Modify_AuraBar(parent, region, data)
-		Skin_WeakAuras(region, "aurabar")
-	end
-
 	local function OnPrototypeCreate(region)
 		Skin_WeakAuras(region, region.regionType)
 	end
