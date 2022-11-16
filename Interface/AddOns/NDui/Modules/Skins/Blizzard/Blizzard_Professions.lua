b8f316d99b8957f756bef20749968b60569e4664
siweia <siwei1990@outlook.com>
2022-11-16 23:12:48 +0800

    Skins: update professions and orders

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Professions.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_Professions.lua
@@ -1,6 +1,10 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+local flyoutFrame
+
+-- [[ Professions ]]
+
 local function reskinSlotButton(button)
 	if button and not button.styled then
 		button:SetNormalTexture(0)
@@ -33,6 +37,33 @@ local function reskinQualityContainer(container)
 	button.bg = B.ReskinIcon(button.Icon)
 	B.ReskinIconBorder(button.IconBorder, true)
 	reskinArrowInput(container.EditBox)
+end
+
+local function refreshFlyoutButtons(self)
+	for i = 1, self.ScrollTarget:GetNumChildren() do
+		local button = select(i, self.ScrollTarget:GetChildren())
+		if button.IconBorder and not button.styled then
+			button.bg = B.ReskinIcon(button.icon)
+			button:SetNormalTexture(0)
+			button:SetPushedTexture(0)
+			button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+			B.ReskinIconBorder(button.IconBorder, true)
+
+			button.styled = true
+		end
+	end
+end
+
+local function reskinFlyouts(flyout)
+	if not flyout.styled then
+		B.StripTextures(flyout)
+		B.SetBD(flyout):SetFrameLevel(2)
+		B.ReskinCheck(flyout.HideUnownedCheckBox)
+		flyout.HideUnownedCheckBox.bg:SetInside(nil, 6, 6)
+		hooksecurefunc(flyout.ScrollBox, "Update", refreshFlyoutButtons)
+
+		flyout.styled = true
+	end
 end
 
 C.themes["Blizzard_Professions"] = function()
@@ -226,37 +257,15 @@ C.themes["Blizzard_Professions"] = function()
 
 	-- Item flyout
 	if OpenProfessionsItemFlyout then
-		local function refreshFlyoutButtons(self)
-			for i = 1, self.ScrollTarget:GetNumChildren() do
-				local button = select(i, self.ScrollTarget:GetChildren())
-				if button.IconBorder and not button.styled then
-					button.bg = B.ReskinIcon(button.icon)
-					button:SetNormalTexture(0)
-					button:SetPushedTexture(0)
-					button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
-					B.ReskinIconBorder(button.IconBorder, true)
-
-					button.styled = true
-				end
-			end
-		end
-
-		local flyout
 		hooksecurefunc("OpenProfessionsItemFlyout", function()
-			if not flyout then
-				for i = 1, frame:GetNumChildren() do
-					local child = select(i, frame:GetChildren())
-					if child.HideUnownedCheckBox then
-						flyout = child
+			if flyoutFrame then return end
 
-						B.StripTextures(flyout)
-						B.SetBD(flyout):SetFrameLevel(2)
-						B.ReskinCheck(flyout.HideUnownedCheckBox)
-						flyout.HideUnownedCheckBox.bg:SetInside(nil, 6, 6)
-						hooksecurefunc(flyout.ScrollBox, "Update", refreshFlyoutButtons)
-
-						break
-					end
+			for i = 1, frame:GetNumChildren() do
+				local child = select(i, frame:GetChildren())
+				if child.HideUnownedCheckBox then
+					flyoutFrame = child
+					reskinFlyouts(flyoutFrame)
+					break
 				end
 			end
 		end)
@@ -305,4 +314,204 @@ C.themes["Blizzard_Professions"] = function()
 			end
 		end
 	end)
+end
+
+-- [[ Profession Orders ]]
+
+local function hideCategoryButton(button)
+	button.NormalTexture:Hide()
+	button.SelectedTexture:SetColorTexture(0, .6, 1, .3)
+	button.HighlightTexture:SetColorTexture(1, 1, 1, .1)
+end
+
+local function reskinListIcon(frame)
+	if not frame.tableBuilder then return end
+
+	for i = 1, 22 do
+		local row = frame.tableBuilder.rows[i]
+		if row then
+			local cell = row.cells and row.cells[1]
+			if cell and cell.Icon then
+				if not cell.styled then
+					cell.Icon.bg = B.ReskinIcon(cell.Icon)
+					if cell.IconBorder then cell.IconBorder:Hide() end
+					cell.styled = true
+				end
+				cell.Icon.bg:SetShown(cell.Icon:IsShown())
+			end
+		end
+	end
+end
+
+local function reskinListHeader(headerContainer)
+	local maxHeaders = headerContainer:GetNumChildren()
+	for i = 1, maxHeaders do
+		local header = select(i, headerContainer:GetChildren())
+		if header and not header.styled then
+			header:DisableDrawLayer("BACKGROUND")
+			header.bg = B.CreateBDFrame(header)
+			local hl = header:GetHighlightTexture()
+			hl:SetColorTexture(1, 1, 1, .1)
+			hl:SetAllPoints(header.bg)
+
+			header.styled = true
+		end
+
+		if header.bg then
+			header.bg:SetPoint("BOTTOMRIGHT", i < maxHeaders and -5 or 0, -2)
+		end
+	end
+end
+
+local function reskinBrowseOrders(frame)
+	local headerContainer = frame.RecipeList and frame.RecipeList.HeaderContainer
+	if headerContainer then
+		reskinListHeader(headerContainer)
+	end
+end
+
+local function reskinMoneyInput(box)
+	B.ReskinEditBox(box)
+	box.__bg:SetPoint("TOPLEFT", 0, -3)
+	box.__bg:SetPoint("BOTTOMRIGHT", 0, 3)
+end
+
+C.themes["Blizzard_ProfessionsCustomerOrders"] = function()
+	local frame = _G.ProfessionsCustomerOrdersFrame
+
+	B.ReskinPortraitFrame(frame)
+	for i = 1, 2 do
+		B.ReskinTab(frame.Tabs[i])
+	end
+	B.StripTextures(frame.MoneyFrameBorder)
+	B.CreateBDFrame(frame.MoneyFrameBorder, .25)
+	B.StripTextures(frame.MoneyFrameInset)
+
+	local searchBar = frame.BrowseOrders.SearchBar
+	B.Reskin(searchBar.FavoritesSearchButton)
+	searchBar.FavoritesSearchButton:SetSize(22, 22)
+	B.ReskinEditBox(searchBar.SearchBox)
+	B.Reskin(searchBar.SearchButton)
+
+	local filterButton = searchBar.FilterButton
+	B.ReskinFilterButton(filterButton)
+	B.ReskinFilterReset(filterButton.ClearFiltersButton)
+
+	B.StripTextures(frame.BrowseOrders.CategoryList)
+	B.ReskinTrimScroll(frame.BrowseOrders.CategoryList.ScrollBar, true)
+
+	hooksecurefunc(frame.BrowseOrders.CategoryList.ScrollBox, "Update", function(self)
+		for i = 1, self.ScrollTarget:GetNumChildren() do
+			local child = select(i, self.ScrollTarget:GetChildren())
+			if child.Text and not child.styled then
+				hideCategoryButton(child)
+				hooksecurefunc(child, "Init", hideCategoryButton)
+
+				child.styled = true
+			end
+		end
+	end)
+
+	local recipeList = frame.BrowseOrders.RecipeList
+	B.StripTextures(recipeList)
+	B.CreateBDFrame(recipeList.ScrollBox, .25):SetInside()
+	B.ReskinTrimScroll(recipeList.ScrollBar, true)
+
+	hooksecurefunc(frame.BrowseOrders, "SetupTable", reskinBrowseOrders)
+	hooksecurefunc(frame.BrowseOrders, "StartSearch", reskinListIcon)
+
+	-- Form
+	B.Reskin(frame.Form.BackButton)
+	B.ReskinCheck(frame.Form.TrackRecipeCheckBox.Checkbox)
+	frame.Form.RecipeHeader:Hide() -- needs review
+	B.CreateBDFrame(frame.Form.RecipeHeader, .25)
+	B.StripTextures(frame.Form.LeftPanelBackground)
+	B.StripTextures(frame.Form.RightPanelBackground)
+
+	local itemButton = frame.Form.OutputIcon
+	itemButton.CircleMask:Hide()
+	itemButton.bg = B.ReskinIcon(itemButton.Icon)
+	B.ReskinIconBorder(itemButton.IconBorder, true, true)
+
+	local hl = itemButton:GetHighlightTexture()
+	hl:SetColorTexture(1, 1, 1, .25)
+	hl:SetInside(itemButton.bg)
+
+	B.ReskinEditBox(frame.Form.OrderRecipientTarget)
+	frame.Form.OrderRecipientTarget.__bg:SetPoint("TOPLEFT", -8, -2)
+	frame.Form.OrderRecipientTarget.__bg:SetPoint("BOTTOMRIGHT", 0, 2)
+
+	B.StripTextures(frame.Form.PaymentContainer.NoteEditBox)
+	local bg = B.CreateBDFrame(frame.Form.PaymentContainer.NoteEditBox, .25)
+	bg:SetPoint("TOPLEFT", 15, 5)
+	bg:SetPoint("BOTTOMRIGHT", -18, 0)
+
+	B.ReskinDropDown(frame.Form.OrderRecipientDropDown)
+	reskinMoneyInput(frame.Form.PaymentContainer.TipMoneyInputFrame.GoldBox)
+	reskinMoneyInput(frame.Form.PaymentContainer.TipMoneyInputFrame.SilverBox)
+	B.ReskinDropDown(frame.Form.PaymentContainer.DurationDropDown)
+	B.Reskin(frame.Form.PaymentContainer.ListOrderButton)
+
+	local viewButton = frame.Form.PaymentContainer.ViewListingsButton
+	viewButton:SetAlpha(0)
+	local buttonFrame = CreateFrame("Frame", nil, frame.Form.PaymentContainer)
+	buttonFrame:SetInside(viewButton)
+	local tex = buttonFrame:CreateTexture(nil, "ARTWORK")
+	tex:SetAllPoints()
+	tex:SetTexture("Interface\\CURSOR\\Crosshair\\Repair")
+
+	local current = frame.Form.CurrentListings
+	B.StripTextures(current)
+	B.SetBD(current)
+	B.Reskin(current.CloseButton)
+	B.ReskinTrimScroll(current.OrderList.ScrollBar, true)
+	reskinListHeader(current.OrderList.HeaderContainer)
+	B.StripTextures(current.OrderList)
+	current:ClearAllPoints()
+	current:SetPoint("LEFT", frame, "RIGHT", 10, 0)
+
+	hooksecurefunc(frame.Form, "Init", function(self)
+		for slot in self.reagentSlotPool:EnumerateActive() do
+			local button = slot.Button		
+			if button and not button.styled then
+				button:SetNormalTexture(0)
+				button:SetPushedTexture(0)
+				button.bg = B.ReskinIcon(button.Icon)
+				B.ReskinIconBorder(button.IconBorder, true)
+				local hl = button:GetHighlightTexture()
+				hl:SetColorTexture(1, 1, 1, .25)
+				hl:SetInside(button.bg)
+				if button.SlotBackground then
+					button.SlotBackground:Hide()
+				end
+		
+				button.styled = true
+			end
+		end
+	end)
+
+	-- Item flyout
+	if OpenProfessionsItemFlyout then
+		hooksecurefunc("OpenProfessionsItemFlyout", function()
+			if flyoutFrame then return end
+
+			for i = 1, frame:GetNumChildren() do
+				local child = select(i, frame:GetChildren())
+				if child.HideUnownedCheckBox then
+					flyoutFrame = child
+					reskinFlyouts(flyoutFrame)
+					break
+				end
+			end
+		end)
+	end
+
+	-- Orders
+	B.Reskin(frame.MyOrdersPage.RefreshButton)
+	frame.MyOrdersPage.RefreshButton.__bg:SetInside(nil, 3, 3)
+	reskinListHeader(frame.MyOrdersPage.OrderList.HeaderContainer)
+	B.ReskinTrimScroll(frame.MyOrdersPage.OrderList.ScrollBar, true)
+
+	B.StripTextures(frame.MyOrdersPage.OrderList)
+	B.CreateBDFrame(frame.MyOrdersPage.OrderList, .25)
 end
