38d46e879b419b0d65a96eeae5ede77b20eec89e
siweia <siwei1990@outlook.com>
2022-12-05 16:53:02 +0800

    Evoker healer use incorrect dispel spellID

--- Interface/AddOns/NDui/Core/ExtraGUI.lua
+++ Interface/AddOns/NDui/Core/ExtraGUI.lua
@@ -436,6 +436,9 @@ function G:SetupClickCast(parent)
 		StaticPopup_Show("RESET_NDUI_CLICKSETS")
 	end)
 
+	local fixedSpells = {
+		["360823"] = "365585", -- incorrect spellID for Evoker
+	}
 	local function addClick(scroll, options)
 		local value, key, modKey = options[1]:GetText(), options[2].Text:GetText(), options[3].Text:GetText()
 		if not value or not key then UIErrorsFrame:AddMessage(DB.InfoColor..L["Incomplete Input"]) return end
@@ -445,6 +448,7 @@ function G:SetupClickCast(parent)
 		local fullkey = (modKey == "" and key or modKey.."-"..key)
 		if NDuiADB["ClickSets"][DB.MyClass][fullkey] then UIErrorsFrame:AddMessage(DB.InfoColor..L["Existing ClickSet"]) return end
 
+		value = fixedSpells[value] or value
 		NDuiADB["ClickSets"][DB.MyClass][fullkey] = tonumber(value) or value
 		createBar(scroll.child, fullkey, value)
 		clearEdit(options)
