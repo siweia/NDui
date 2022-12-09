467fc6fb1b1658115060b4aeda06b44863d77730
siweia <siwei1990@outlook.com>
2022-12-09 20:10:30 +0800

    Update raiddebuffs for ULD

--- Interface/AddOns/NDui/Config/RaidFrameAuras/WLK/ULD.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/WLK/ULD.lua
@@ -5,4 +5,6 @@ local module = B:GetModule("AurasTable")
 local TIER = 3
 local INSTANCE = 603 -- 奥杜尔
 
-module:RegisterDebuff(TIER, INSTANCE, 0, 18431) -- 低沉咆哮
+module:RegisterDebuff(TIER, INSTANCE, 0, 65121) -- 灼热之光
+module:RegisterDebuff(TIER, INSTANCE, 0, 64234) -- 重力炸弹
+module:RegisterDebuff(TIER, INSTANCE, 0, 64292, 6) -- 捉人
