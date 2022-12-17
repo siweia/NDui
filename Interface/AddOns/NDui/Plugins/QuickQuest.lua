018393efd354ac4acb3653547ae53bc8d46d1e0d
siweia <siwei1990@outlook.com>
2022-12-18 07:26:10 +0800

    Check gossip id

--- Interface/AddOns/NDui/Plugins/QuickQuest.lua
+++ Interface/AddOns/NDui/Plugins/QuickQuest.lua
@@ -240,14 +240,16 @@ QuickQuest:Register("GOSSIP_SHOW", function()
 	local numOptions = #gossipInfoTable
 	local firstOptionID = gossipInfoTable[1] and gossipInfoTable[1].gossipOptionID
 
-	if autoSelectFirstOptionList[npcID] then
-		return C_GossipInfo_SelectOption(firstOptionID)
-	end
-
-	if available == 0 and active == 0 and numOptions == 1 then
-		local _, instance, _, _, _, _, _, mapID = GetInstanceInfo()
-		if instance ~= "raid" and not ignoreGossipNPC[npcID] and not ignoreInstances[mapID] then
+	if firstOptionID then
+		if autoSelectFirstOptionList[npcID] then
 			return C_GossipInfo_SelectOption(firstOptionID)
+		end
+
+		if available == 0 and active == 0 and numOptions == 1 then
+			local _, instance, _, _, _, _, _, mapID = GetInstanceInfo()
+			if instance ~= "raid" and not ignoreGossipNPC[npcID] and not ignoreInstances[mapID] then
+				return C_GossipInfo_SelectOption(firstOptionID)
+			end
 		end
 	end
 
