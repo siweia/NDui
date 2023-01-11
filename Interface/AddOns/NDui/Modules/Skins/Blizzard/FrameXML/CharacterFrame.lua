3e5c547d3948c3775df69b3ab7c1085f9963c1df
siweia <siwei1990@outlook.com>
2023-01-11 23:26:14 +0800

    [10.0.5] Update skins

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/CharacterFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/CharacterFrame.lua
@@ -241,8 +241,10 @@ tinsert(C.defaultThemes, function()
 	end)
 
 	-- Reputation Frame
-	ReputationDetailCorner:Hide()
-	ReputationDetailDivider:Hide()
+	if not DB.isNewPatch then
+		ReputationDetailCorner:Hide()
+		ReputationDetailDivider:Hide()
+	end
 	ReputationDetailFrame:SetPoint("TOPLEFT", ReputationFrame, "TOPRIGHT", 3, -28)
 
 	local function UpdateFactionSkins()
