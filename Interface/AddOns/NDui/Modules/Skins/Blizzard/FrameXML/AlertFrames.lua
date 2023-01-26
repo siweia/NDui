59fba2cb8284bfd504b946116f920a8d8837a4ff
siweia <siwei1990@outlook.com>
2023-01-26 12:18:58 +0800

    Skins: update event toast

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/AlertFrames.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/AlertFrames.lua
@@ -395,12 +395,10 @@ tinsert(C.defaultThemes, function()
 	hooksecurefunc(EventToastManagerFrame, "DisplayToast", function(self)
 		local toast = self.currentDisplayingToast
 		local border = toast and toast.IconBorder
-		if border then
-			if not border.bg then
-				border:SetTexture("")
-				border.bg = B.ReskinIcon(toast.Icon)
-			end
-			border.bg:SetBackdropBorderColor(border:GetVertexColor())
+		if border and not toast.bg then
+			toast.bg = B.ReskinIcon(toast.Icon)
+			border:SetTexture("")
+			B.ReskinIconBorder(border, true)
 		end
 	end)
 end)
