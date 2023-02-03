9e86d9362c3f5c258cf09a225486104ff6c263aa
siweia <siwei1990@outlook.com>
2023-02-04 02:04:29 +0800

    Skins: merge friends frame and recruit frame

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/FriendsFrame.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/FriendsFrame.lua
@@ -104,12 +104,6 @@ tinsert(C.defaultThemes, function()
 	FriendsFrameStatusDropDown:ClearAllPoints()
 	FriendsFrameStatusDropDown:SetPoint("TOPLEFT", FriendsFrame, "TOPLEFT", 10, -28)
 
-	for _, button in pairs({FriendsTabHeaderSoRButton, FriendsTabHeaderRecruitAFriendButton}) do
-		button:SetPushedTexture(0)
-		button:GetRegions():SetTexCoord(unpack(DB.TexCoord))
-		B.CreateBDFrame(button, .25)
-	end
-
 	-- FriendsFrameBattlenetFrame
 
 	FriendsFrameBattlenetFrame:GetRegions():Hide()
@@ -188,4 +182,47 @@ tinsert(C.defaultThemes, function()
 	WhoFrameWhoButton:SetPoint("RIGHT", WhoFrameAddFriendButton, "LEFT", -1, 0)
 	WhoFrameAddFriendButton:SetPoint("RIGHT", WhoFrameGroupInviteButton, "LEFT", -1, 0)
 	FriendsFrameTitleText:SetPoint("TOP", FriendsFrame, "TOP", 0, -8)
+
+	-- Recruite frame
+
+	RecruitAFriendFrame.SplashFrame.Description:SetTextColor(1, 1, 1)
+	B.Reskin(RecruitAFriendFrame.SplashFrame.OKButton)
+	B.StripTextures(RecruitAFriendFrame.RewardClaiming)
+	B.Reskin(RecruitAFriendFrame.RewardClaiming.ClaimOrViewRewardButton)
+	B.Reskin(RecruitAFriendFrame.RecruitmentButton)
+
+	local recruitList = RecruitAFriendFrame.RecruitList
+	B.StripTextures(recruitList.Header)
+	B.CreateBDFrame(recruitList.Header, .25)
+	recruitList.ScrollFrameInset:Hide()
+	B.ReskinTrimScroll(recruitList.ScrollBar)
+
+	local recruitmentFrame = RecruitAFriendRecruitmentFrame
+	B.StripTextures(recruitmentFrame)
+	B.ReskinClose(recruitmentFrame.CloseButton)
+	B.SetBD(recruitmentFrame)
+	B.StripTextures(recruitmentFrame.EditBox)
+	local bg = B.CreateBDFrame(recruitmentFrame.EditBox, .25)
+	bg:SetPoint("TOPLEFT", -3, -3)
+	bg:SetPoint("BOTTOMRIGHT", 0, 3)
+	B.Reskin(recruitmentFrame.GenerateOrCopyLinkButton)
+
+	local rewardsFrame = RecruitAFriendRewardsFrame
+	B.StripTextures(rewardsFrame)
+	B.ReskinClose(rewardsFrame.CloseButton)
+	B.SetBD(rewardsFrame)
+
+	rewardsFrame:HookScript("OnShow", function(self)
+		for i = 1, self:GetNumChildren() do
+			local child = select(i, self:GetChildren())
+			local button = child and child.Button
+			if button and not button.styled then
+				B.ReskinIcon(button.Icon)
+				button.IconBorder:Hide()
+				button:GetHighlightTexture():SetColorTexture(1, 1, 1, .25)
+
+				button.styled = true
+			end
+		end
+	end)
 end)
