56b9fcc4198bec802060e708c6350146c93885a1
siweia <siwei1990@outlook.com>
2023-02-11 08:59:40 +0800

    Separate file for blizz fix

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -71,15 +71,6 @@ function M:OnLogin()
 	M:UpdateMaxZoomLevel()
 	M:MoveBlizzFrames()
 
-	-- Unregister talent event
-	if PlayerTalentFrame then
-		PlayerTalentFrame:UnregisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-	else
-		hooksecurefunc("TalentFrame_LoadUI", function()
-			PlayerTalentFrame:UnregisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-		end)
-	end
-
 	-- Auto chatBubbles
 	if NDuiADB["AutoBubbles"] then
 		local function updateBubble()
@@ -106,20 +97,6 @@ function M:OnLogin()
 				self.editBox:SetText(DELETE_ITEM_CONFIRM_STRING)
 			end
 		end)
-	end
-
-	-- Fix blizz bug in addon list
-	local _AddonTooltip_Update = AddonTooltip_Update
-	function AddonTooltip_Update(owner)
-		if not owner then return end
-		if owner:GetID() < 1 then return end
-		_AddonTooltip_Update(owner)
-	end
-
-	-- Fix empty string in party guide promote
-	if not PROMOTE_GUIDE then
-		if DB.isDeveloper then print(DB.NDuiString..": Blizzard promote string fixed.") end
-		PROMOTE_GUIDE = PARTY_PROMOTE_GUIDE
 	end
 end
 
@@ -490,86 +467,6 @@ do
 
 		_MerchantItemButton_OnModifiedClick(self, ...)
 	end
-end
-
--- Fix Drag Collections taint
-do
-	local done
-	local function setupMisc(event, addon)
-		if event == "ADDON_LOADED" and addon == "Blizzard_Collections" then
-			-- Fix undragable issue
-			local checkBox = WardrobeTransmogFrame.ToggleSecondaryAppearanceCheckbox
-			checkBox.Label:ClearAllPoints()
-			checkBox.Label:SetPoint("LEFT", checkBox, "RIGHT", 2, 1)
-			checkBox.Label:SetWidth(152)
-
-			CollectionsJournal:HookScript("OnShow", function()
-				if not done then
-					if InCombatLockdown() then
-						B:RegisterEvent("PLAYER_REGEN_ENABLED", setupMisc)
-					else
-						B.CreateMF(CollectionsJournal)
-					end
-					done = true
-				end
-			end)
-			B:UnregisterEvent(event, setupMisc)
-		elseif event == "PLAYER_REGEN_ENABLED" then
-			B.CreateMF(CollectionsJournal)
-			B:UnregisterEvent(event, setupMisc)
-		end
-	end
-
-	B:RegisterEvent("ADDON_LOADED", setupMisc)
-end
-
--- Select target when click on raid units
-do
-	local function fixRaidGroupButton()
-		for i = 1, 40 do
-			local bu = _G["RaidGroupButton"..i]
-			if bu and bu.unit and not bu.clickFixed then
-				bu:SetAttribute("type", "target")
-				bu:SetAttribute("unit", bu.unit)
-
-				bu.clickFixed = true
-			end
-		end
-	end
-
-	local function setupMisc(event, addon)
-		if event == "ADDON_LOADED" and addon == "Blizzard_RaidUI" then
-			if not InCombatLockdown() then
-				fixRaidGroupButton()
-			else
-				B:RegisterEvent("PLAYER_REGEN_ENABLED", setupMisc)
-			end
-			B:UnregisterEvent(event, setupMisc)
-		elseif event == "PLAYER_REGEN_ENABLED" then
-			if RaidGroupButton1 and RaidGroupButton1:GetAttribute("type") ~= "target" then
-				fixRaidGroupButton()
-				B:UnregisterEvent(event, setupMisc)
-			end
-		end
-	end
-
-	B:RegisterEvent("ADDON_LOADED", setupMisc)
-end
-
--- Fix blizz guild news hyperlink error
-do
-	local function fixGuildNews(event, addon)
-		if addon ~= "Blizzard_GuildUI" then return end
-
-		local _GuildNewsButton_OnEnter = GuildNewsButton_OnEnter
-		function GuildNewsButton_OnEnter(self)
-			if not (self.newsInfo and self.newsInfo.whatText) then return end
-			_GuildNewsButton_OnEnter(self)
-		end
-
-		B:UnregisterEvent(event, fixGuildNews)
-	end
-	B:RegisterEvent("ADDON_LOADED", fixGuildNews)
 end
 
 local function skipOnKeyDown(self, key)
