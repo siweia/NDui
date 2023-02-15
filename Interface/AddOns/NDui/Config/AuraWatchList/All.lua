2911a2ebbe0179ca6ea9229d41f28ba0121abe9a
siweia <siwei1990@outlook.com>
2023-02-15 23:37:01 +0800

    Update auras and nameplate filter

--- Interface/AddOns/NDui/Config/AuraWatchList/All.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/All.lua
@@ -52,8 +52,9 @@ local list = {
 		{AuraID = 20572, UnitID = "player"},	-- 血性狂怒 兽人
 		{AuraID = 33697, UnitID = "player"},	-- 血性狂怒 兽人
 		{AuraID = 292463, UnitID = "player"},	-- 帕库之拥 赞达拉
-		-- 隐形药水
+		-- 药水
 		{AuraID = 371124, UnitID = "player"},	-- 沉静西风药水
+		{AuraID = 371024, UnitID = "player"},	-- 元素强能药水
 		-- 10.0 饰品
 		{AuraID = 381476, UnitID = "player"},	-- 爆发烈焰
 		{AuraID = 383941, UnitID = "player"},	-- 崩坏之力
@@ -188,7 +189,8 @@ local list = {
 		{AuraID = 153692, UnitID = "player", Flash = true},	-- 死疽沥青，影月墓地
 		{AuraID = 400474, UnitID = "player"},	-- 能量湍流，阻击战
 		{AuraID = 397911, UnitID = "player"},	-- 毁灭之触，青龙寺
-		{AuraID = 397798, UnitID = "player"},	-- 腐蚀漩涡，青龙寺
+		{AuraID = 397797, UnitID = "player"},	-- 腐蚀漩涡，青龙寺
+		{AuraID = 397799, UnitID = "player", Flash = true},	-- 腐蚀漩涡，青龙寺
 		{AuraID = 381862, UnitID = "player", Flash = true},	-- 地狱火之核，红玉
 		{AuraID = 376760, UnitID = "player"},	-- 狂风之力，学院
 		{AuraID = 391977, UnitID = "player"},	-- 涌动超载，学院
