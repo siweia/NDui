983b131ee71ca33a386223dc7a0b79c87ff80d0f
siweia <siwei1990@outlook.com>
2023-02-22 20:59:23 +0800

    Create minimap bg even pulse disabled

--- Interface/AddOns/NDui/Modules/Maps/Minimap.lua
+++ Interface/AddOns/NDui/Modules/Maps/Minimap.lua
@@ -11,12 +11,12 @@ local C_Timer_After = C_Timer.After
 local cr, cg, cb = DB.r, DB.g, DB.b
 
 function module:CreatePulse()
+	local bg = B.SetBD(Minimap)
+	bg:SetFrameStrata("BACKGROUND")
+
 	if not C.db["Map"]["CombatPulse"] then return end
 
 	local MinimapMailFrame = MinimapCluster.IndicatorFrame.MailFrame
-
-	local bg = B.SetBD(Minimap)
-	bg:SetFrameStrata("BACKGROUND")
 	local anim = bg:CreateAnimationGroup()
 	anim:SetLooping("BOUNCE")
 	anim.fader = anim:CreateAnimation("Alpha")
