3b568b269ad9aeadf1b2a7c0320c13835326e514
siweia <siwei1990@outlook.com>
2023-03-24 18:47:45 +0800

    [10.1] Add entries for S2 dungeons and raid

--- Interface/AddOns/NDui/Core/ExtraGUI.lua
+++ Interface/AddOns/NDui/Core/ExtraGUI.lua
@@ -114,35 +114,26 @@ function G:SetupRaidDebuffs(parent)
 	local maxLevel = UnitLevel("player") > 60
 	local dungeons = {}
 
-	if maxLevel then
-		for dungeonID = 1196, 1204 do
-			if dungeonID ~= 1200 then
-				AddNewDungeon(dungeons, dungeonID)
-			end
-		end
-		AddNewDungeon(dungeons, 313)  -- 青龙寺
-		AddNewDungeon(dungeons, 537)  -- 影月墓地
-		AddNewDungeon(dungeons, 721)  -- 英灵殿
-		AddNewDungeon(dungeons, 800)  -- 群星庭院
-	else
-		for dungeonID = 1182, 1189 do
+	for dungeonID = 1196, 1204 do
+		if dungeonID ~= 1200 then
 			AddNewDungeon(dungeons, dungeonID)
 		end
-		AddNewDungeon(dungeons, 1194) -- 集市
-		AddNewDungeon(dungeons, 536)  -- 恐轨车站
-		AddNewDungeon(dungeons, 558)  -- 钢铁码头
-		AddNewDungeon(dungeons, 860)  -- 重返卡拉赞
-		AddNewDungeon(dungeons, 1178) -- 麦卡贡
+	end
+	AddNewDungeon(dungeons, 313)  -- 青龙寺
+	AddNewDungeon(dungeons, 537)  -- 影月墓地
+	AddNewDungeon(dungeons, 721)  -- 英灵殿
+	AddNewDungeon(dungeons, 800)  -- 群星庭院
+	if DB.isPatch10_1 then
+		AddNewDungeon(dungeons, 68)  -- 旋云之巅
+		AddNewDungeon(dungeons, 767)  -- 耐萨里奥的巢穴
+		AddNewDungeon(dungeons, 1001)  -- 自由镇
+		AddNewDungeon(dungeons, 1022)  -- 地渊孢林
 	end
 
 	local raids = {
-		[1] = EJ_GetInstanceInfo(1190),
-		[2] = EJ_GetInstanceInfo(1193),
-		[3] = EJ_GetInstanceInfo(1195),
+		[1] = EJ_GetInstanceInfo(1200),
+		[2] = EJ_GetInstanceInfo(1208),
 	}
-	if maxLevel then
-		raids[4] = EJ_GetInstanceInfo(1200)
-	end
 
 	options[1] = G:CreateDropdown(frame, DUNGEONS.."*", 120, -30, dungeons, L["Dungeons Intro"], 130, 30)
 	options[1]:Hide()
