013f43fad4c632200e73f0cea519d7a180af1f61
siweia <siwei1990@outlook.com>
2023-05-05 19:08:11 +0800

    [3.4.2] Start to add 3.4.2 compats

--- Interface/AddOns/NDui/Core/Database.lua
+++ Interface/AddOns/NDui/Core/Database.lua
@@ -9,7 +9,7 @@ DB.Support = GetAddOnMetadata("NDui", "X-Support")
 DB.Client = GetLocale()
 DB.ScreenWidth, DB.ScreenHeight = GetPhysicalScreenSize()
 DB.isClassic = select(4, GetBuildInfo()) < 90000
-DB.isNewPatch = select(4, GetBuildInfo()) >= 30401 -- 3.4.1 WLK
+DB.isNewPatch = select(4, GetBuildInfo()) >= 30402 -- 3.4.2 WLK
 
 -- Colors
 DB.MyName = UnitName("player")
