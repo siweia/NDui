57335816d0a9f4ccb367fab7a8f57a3f12b4a068
siweia <siwei1990@outlook.com>
2023-05-19 20:31:39 +0800

    [10.1.5] Header only exists in new patch

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ObjectiveTracker.lua
@@ -281,11 +281,13 @@ tinsert(C.defaultThemes, function()
 		BONUS_OBJECTIVE_TRACKER_MODULE.Header,
 		WORLD_QUEST_TRACKER_MODULE.Header,
 		MONTHLY_ACTIVITIES_TRACKER_MODULE.Header,
-		ADVENTURE_TRACKER_MODULE.Header, -- isNewPatch
 		ObjectiveTrackerFrame.BlocksFrame.UIWidgetsHeader,
 	}
 	for _, header in pairs(headers) do
 		reskinHeader(header)
+	end
+	if DB.isNewPatch then
+		reskinHeader(ADVENTURE_TRACKER_MODULE.Header)
 	end
 
 	-- Minimize Button
