f482b612b17cf2e9c7a2426ed88962aa923ed9df
siweia <siwei1990@outlook.com>
2023-06-10 00:04:18 +0800

    Temp fix for gameIcons in Friends infobar, or not temp

--- Interface/AddOns/NDui/Modules/Infobar/Friends.lua
+++ Interface/AddOns/NDui/Modules/Infobar/Friends.lua
@@ -425,8 +425,8 @@ function info:FriendsPanel_CreateButton(parent, index)
 	button.gameIcon = button:CreateTexture(nil, "ARTWORK")
 	button.gameIcon:SetPoint("RIGHT", button, -8, 0)
 	button.gameIcon:SetSize(16, 16)
-	button.gameIcon:SetTexCoord(.17, .83, .17, .83)
-	B.CreateBDFrame(button.gameIcon)
+	--button.gameIcon:SetTexCoord(.17, .83, .17, .83)
+	--B.CreateBDFrame(button.gameIcon)
 
 	button:RegisterForClicks("AnyUp")
 	button:SetScript("OnClick", buttonOnClick)
@@ -448,7 +448,8 @@ function info:FriendsPanel_UpdateButton(button)
 		local classColor = DB.ClassColors[class] or levelColor
 		button.name:SetText(format("%s%s|r %s%s", levelColor, level, B.HexRGB(classColor), name))
 		button.zone:SetText(format("%s%s", zoneColor, area))
-		button.gameIcon:SetAtlas(BNet_GetBattlenetClientAtlas(BNET_CLIENT_WOW))
+		C_Texture.SetTitleIconTexture(button.gameIcon, BNET_CLIENT_WOW, Enum.TitleIconVersion.Medium)
+		--button.gameIcon:SetAtlas(BNet_GetBattlenetClientAtlas(BNET_CLIENT_WOW))
 
 		button.isBNet = nil
 		button.data = friendTable[index]
@@ -468,11 +469,13 @@ function info:FriendsPanel_UpdateButton(button)
 		button.zone:SetText(format("%s%s", zoneColor, infoText))
 
 		if client == CLIENT_WOW_DIFF then
-			button.gameIcon:SetAtlas(BNet_GetBattlenetClientAtlas(BNET_CLIENT_WOW))
+			C_Texture.SetTitleIconTexture(button.gameIcon, BNET_CLIENT_WOW, Enum.TitleIconVersion.Medium)
+			--button.gameIcon:SetAtlas(BNet_GetBattlenetClientAtlas(BNET_CLIENT_WOW))
 		elseif client == BNET_CLIENT_WOW then
 			button.gameIcon:SetTexture("Interface\\FriendsFrame\\PlusManz-"..factionName)
 		else
-			button.gameIcon:SetAtlas(BNet_GetBattlenetClientAtlas(client))
+			C_Texture.SetTitleIconTexture(button.gameIcon, client, Enum.TitleIconVersion.Medium)
+			--button.gameIcon:SetAtlas(BNet_GetBattlenetClientAtlas(client))
 		end
 
 		button.isBNet = true
