eec2179a9fe68a76c1dd37e7d8248ae12a556415
siweia <siwei1990@outlook.com>
2023-09-11 20:19:28 +0800

    Skins: update ready check frame

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ReadyCheck.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/ReadyCheck.lua
@@ -5,15 +5,9 @@ tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
 	-- Ready check
+	B.StripTextures(ReadyCheckListenerFrame)
 	B.SetBD(ReadyCheckFrame)
 	ReadyCheckPortrait:SetAlpha(0)
-	select(2, ReadyCheckListenerFrame:GetRegions()):Hide()
-
-	ReadyCheckFrame:HookScript("OnShow", function(self)
-		if self.initiator and UnitIsUnit("player", self.initiator) then
-			self:Hide()
-		end
-	end)
 
 	B.Reskin(ReadyCheckFrameYesButton)
 	B.Reskin(ReadyCheckFrameNoButton)
