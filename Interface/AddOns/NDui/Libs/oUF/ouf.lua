89cd2a72e6f4eddb86e546f07d1989025a58fe26
siweia <siwei1990@outlook.com>
2023-11-08 22:42:16 +0800

    Update oUF

--- Interface/AddOns/NDui/Libs/oUF/ouf.lua
+++ Interface/AddOns/NDui/Libs/oUF/ouf.lua
@@ -302,6 +302,16 @@ local function initObject(unit, style, styleFunc, header, ...)
 			object:SetAttribute('*type2', 'togglemenu')
 			object:SetAttribute('toggleForVehicle', true)
 
+			--[[ frame.IsPingable
+			This boolean can be set to false to disable the frame from being pingable. Enabled by default.
+			--]]
+			--[[ Override: frame:GetContextualPingType()
+			Used to define which contextual ping is used for the frame.
+
+			By default this wraps `C_Ping.GetContextualPingTypeForUnit(UnitGUID(frame.unit))`.
+			--]]
+			object:SetAttribute('ping-receiver', true)
+
 			if(isEventlessUnit(objectUnit)) then
 				oUF:HandleEventlessUnit(object)
 			else
@@ -346,15 +356,6 @@ local function initObject(unit, style, styleFunc, header, ...)
 			func(object)
 		end
 
-		--[[ frame.IsPingable
-		This boolean can be set to false to disable the frame from being pingable. Enabled by default.
-		--]]
-		--[[ Override: frame:GetContextualPingType()
-		Used to define which contextual ping is used for the frame.
-
-		By default this wraps `C_Ping.GetContextualPingTypeForUnit(UnitGUID(frame.unit))`.
-		--]]
-		object:SetAttribute('ping-receiver', true)
 		Mixin(object, PingableType_UnitFrameMixin)
 
 		-- Make Clique kinda happy
@@ -605,6 +606,8 @@ do
 				frame:SetAttribute('*type1', 'target')
 				frame:SetAttribute('*type2', 'togglemenu')
 				frame:SetAttribute('oUF-guessUnit', unit)
+
+				frame:SetAttribute('ping-receiver', true)
 			end
 
 			local body = header:GetAttribute('oUF-initialConfigFunction')
