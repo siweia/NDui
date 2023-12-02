c2ec21f938a08fb0fbd07a3dfce49575b638c9bf
siweia <siwei1990@outlook.com>
2023-12-02 14:32:07 +0800

    IsItemInRange protected in combat

--- Interface/AddOns/NDui/Plugins/ExtraQuestButton.lua
+++ Interface/AddOns/NDui/Plugins/ExtraQuestButton.lua
@@ -306,7 +306,7 @@ end)
 
 ExtraQuestButton:SetScript("OnUpdate", function(self, elapsed)
 	if self.updateRange then
-		if (self.rangeTimer or 0) > TOOLTIP_UPDATE_TIME then
+		if not InCombatLockdown() and ((self.rangeTimer or 0) > TOOLTIP_UPDATE_TIME) then
 			local HotKey = self.HotKey
 			local Icon = self.Icon
 
