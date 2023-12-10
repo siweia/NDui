defc95f80a907e30ce4846816dc4e4a5d467908f
siweia <siwei1990@outlook.com>
2023-12-11 01:15:16 +0800

    Skins: more elements in engraving

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_EngravingUI.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_EngravingUI.lua
@@ -10,6 +10,31 @@ C.themes["Blizzard_EngravingUI"] = function()
 		B.ReskinEditBox(EngravingFrameSearchBox)
 		B.ReskinDropDown(EngravingFrameFilterDropDown)
 		B.ReskinScroll(EngravingFrameScrollFrameScrollBar)
+
+		for i = 1, 15 do
+			local header = _G["EngravingFrameHeader"..i]
+			if header then
+				B.Reskin(header)
+			end
+		end
+
+		if EngravingFrame_UpdateRuneList then
+			hooksecurefunc("EngravingFrame_UpdateRuneList", function(self)
+				for _, button in ipairs(self.scrollFrame.buttons) do
+					if not button.styled then
+						button:SetNormalTexture(0)
+						local hl = button:GetHighlightTexture()
+						hl:SetColorTexture(1, 1, 1, .25)
+						hl:SetInside()
+						button.selectedTex:SetColorTexture(1, .8, 0, .5)
+						button.selectedTex:SetInside()
+						B.ReskinIcon(button.icon)
+		
+						button.styled = true
+					end
+				end
+			end)
+		end
 	end
 
 	local button = RuneFrameControlButton
