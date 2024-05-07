46717ef96e87f0e07b0d18b6cf8855e1274ba5a9
siweia <siwei1990@outlook.com>
2024-05-08 01:27:25 +0800

    Add altpower

--- Interface/AddOns/NDui/Modules/UFs/Functions.lua
+++ Interface/AddOns/NDui/Modules/UFs/Functions.lua
@@ -1428,6 +1428,35 @@ function UF:UpdateUFClassPower()
 	end
 end
 
+function UF.PostUpdateAltPower(element, _, cur, _, max)
+	if cur and max then
+		local perc = floor((cur/max)*100)
+		if perc < 35 then
+			element:SetStatusBarColor(0, 1, 0)
+		elseif perc < 70 then
+			element:SetStatusBarColor(1, 1, 0)
+		else
+			element:SetStatusBarColor(1, 0, 0)
+		end
+	end
+end
+
+function UF:CreateAltPower(self)
+	local bar = CreateFrame("StatusBar", nil, self)
+	bar:SetStatusBarTexture(DB.normTex)
+	bar:SetPoint("TOPLEFT", self, "BOTTOMLEFT", 0, -3)
+	bar:SetPoint("TOPRIGHT", self, "BOTTOMRIGHT", 0, -3)
+	bar:SetHeight(2)
+	B.SetBD(bar, 0)
+
+	local text = B.CreateFS(bar, 14, "")
+	text:SetJustifyH("CENTER")
+	self:Tag(text, "[altpower]")
+
+	self.AlternativePower = bar
+	self.AlternativePower.PostUpdate = UF.PostUpdateAltPower
+end
+
 function UF:CreateExpRepBar(self)
 	local bar = CreateFrame("StatusBar", nil, self)
 	bar:SetPoint("TOPLEFT", self, "TOPRIGHT", 5, 0)
