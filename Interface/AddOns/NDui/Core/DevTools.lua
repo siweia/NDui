648712aae79d32ef9892b0bf684a406453969bad
siweia <siwei1990@outlook.com>
2024-05-26 13:01:01 +0800

    Add IsPartyLFG

--- Interface/AddOns/NDui/Core/DevTools.lua
+++ Interface/AddOns/NDui/Core/DevTools.lua
@@ -130,8 +130,12 @@ do
 
 	SlashCmdList["NDUI_VER_CHECK"] = function(msg)
 		local channel
-		if IsInRaid() then
+		if IsPartyLFG() then
+			channel = "INSTANCE_CHAT"
+		elseif IsInRaid() then
 			channel = "RAID"
+		elseif IsInGroup() then
+			channel = "PARTY"
 		elseif IsInGuild() then
 			channel = "GUILD"
 		end
