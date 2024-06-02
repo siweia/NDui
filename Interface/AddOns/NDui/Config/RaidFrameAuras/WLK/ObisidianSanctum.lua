25e07c2aa6c1816e0fdc74380ab782a22d49e6f7
siweia <siwei1990@outlook.com>
2024-06-03 00:02:15 +0800

    Remove Raiddebuffs data from WLK and TBC

--- Interface/AddOns/NDui/Config/RaidFrameAuras/WLK/ObisidianSanctum.lua
+++
@@ -1,8 +0,0 @@
-local _, ns = ...
-local B, C, L, DB = unpack(ns)
-local module = B:GetModule("AurasTable")
-
-local TIER = 3
-local INSTANCE = 615 -- 黑曜石圣殿
-
-module:RegisterDebuff(TIER, INSTANCE, 0, 18431) -- 低沉咆哮
