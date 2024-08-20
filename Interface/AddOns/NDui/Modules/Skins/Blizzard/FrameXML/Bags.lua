a6f0bb4510b685ff60237ce011c076cd7f6b375c
siweia <siwei1990@outlook.com>
2024-08-20 21:27:50 +0800

    Skins: correctly hide shadows on bank

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/Bags.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/Bags.lua
@@ -187,6 +187,7 @@ tinsert(C.defaultThemes, function()
 	BankSlotsFrame:DisableDrawLayer("BORDER")
 	BankFrameMoneyFrameBorder:Hide()
 	BankSlotsFrame.NineSlice:SetAlpha(0)
+	BankSlotsFrame.EdgeShadows:Hide()
 
 	-- "item slots" and "bag slots" text
 	select(9, BankSlotsFrame:GetRegions()):SetDrawLayer("OVERLAY")
@@ -221,6 +222,7 @@ tinsert(C.defaultThemes, function()
 	ReagentBankFrame:DisableDrawLayer("BORDER")
 	ReagentBankFrame:DisableDrawLayer("ARTWORK")
 	ReagentBankFrame.NineSlice:SetAlpha(0)
+	ReagentBankFrame.EdgeShadows:Hide()
 
 	B.Reskin(ReagentBankFrame.DespositButton)
 	B.Reskin(ReagentBankFrameUnlockInfoPurchaseButton)
