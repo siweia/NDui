64bb0b220c8e695f7b20be10f681507b8d986b82
siweia <siwei1990@outlook.com>
2024-08-21 19:56:01 +0800

    Precaution for nil error

--- Interface/AddOns/NDui/Modules/UFs/Tags.lua
+++ Interface/AddOns/NDui/Modules/UFs/Tags.lua
@@ -255,7 +255,8 @@ oUF.Tags.Methods["npctitle"] = function(unit)
 		scanTip:SetOwner(UIParent, "ANCHOR_NONE")
 		scanTip:SetUnit(unit)
 
-		local title = _G[format("NDui_ScanTooltipTextLeft%d", GetCVarBool("colorblindmode") and 3 or 2)]:GetText()
+		local textLine = _G[format("NDui_ScanTooltipTextLeft%d", GetCVarBool("colorblindmode") and 3 or 2)]
+		local title = textLine and textLine:GetText()
 		if title and not strfind(title, "^"..LEVEL) then
 			return title
 		end
