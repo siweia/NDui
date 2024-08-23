a3d2e29650edaf280f3ca163b750759a0f20e8bc
siweia <siwei1990@outlook.com>
2024-08-24 03:50:35 +0800

    Handle minimap recycle bin for Tomtom and WIM

--- Interface/AddOns/NDui/Modules/Maps/Minimap.lua
+++ Interface/AddOns/NDui/Modules/Maps/Minimap.lua
@@ -218,6 +218,7 @@ function module:RecycleBin()
 		["HandyNotes.-Pin"] = true,
 		["Guidelime"] = true,
 		["QuestieFrame"] = true,
+		["TTMinimapButton"] = true,
 	}
 	B.SplitList(ignoredButtons, NDuiADB["IgnoredButtons"])
 
@@ -288,6 +289,10 @@ function module:RecycleBin()
 					child:SetScript("OnMouseUp", nil)
 				elseif name == "BagSync_MinimapButton" then
 					child:HookScript("OnMouseUp", clickFunc)
+				elseif name == "WIM3MinimapButton" then
+					child.SetParent = B.Dummy
+					child:SetFrameStrata("DIALOG")
+					child.SetFrameStrata = B.Dummy
 				end
 
 				child.styled = true
