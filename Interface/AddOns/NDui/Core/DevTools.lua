cde9cfa962ffd542dee626ae5aaf2a8f9555a2a2
siweia <siwei1990@outlook.com>
2024-09-02 02:20:00 +0800

    IsPartyLFG not secure in classic

--- Interface/AddOns/NDui/Core/DevTools.lua
+++ Interface/AddOns/NDui/Core/DevTools.lua
@@ -130,7 +130,9 @@ do
 
 	SlashCmdList["NDUI_VER_CHECK"] = function(msg)
 		local channel
-		if IsInRaid() then
+		if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
+			channel = "INSTANCE_CHAT"
+		elseif IsInRaid() then
 			channel = "RAID"
 		elseif IsInGuild() then
 			channel = "GUILD"
