7eb378581087ebec0bdbf96401313e10bc7fee5f
siweia <siwei1990@outlook.com>
2024-11-03 21:14:09 +0800

    Update aura

--- Interface/AddOns/NDui/Config/AuraWatchList/All.lua
+++ Interface/AddOns/NDui/Config/AuraWatchList/All.lua
@@ -334,6 +334,7 @@ local list = {
 		{AuraID = 343470, UnitID = "target", Value = true},	-- 通灵战潮，碎骨之盾
 		{AuraID = 328351, UnitID = "target", Flash = true},	-- 通灵战潮，染血长枪
 		{AuraID = 273721, UnitID = "target", Flash = true},	-- 围攻，1号易伤
+		{AuraID = 423588, UnitID = "target", Value = true},	-- 修道院，圣光屏障
 		-- S1
 		{AuraID = 445409, UnitID = "target", Value = true},	-- 加固壳壁，斯卡莫拉克
 		-- Raids
