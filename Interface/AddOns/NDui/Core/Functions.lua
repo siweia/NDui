4387ba155ec1ef9c00c48f8976a281a88fe680f6
siweia <siwei1990@outlook.com>
2024-12-07 10:19:54 +0800

    Fix isEnable error

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -1077,7 +1077,7 @@ do
 
 	-- Handle close button
 	function B:Texture_OnEnter()
-		if self:IsEnabled() then
+		if self.IsEnabled and self:IsEnabled() then
 			if self.bg then
 				self.bg:SetBackdropColor(cr, cg, cb, .25)
 			else
