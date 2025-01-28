202efc08c09d022a63f9637642586f9ccb178624
siweia <siwei1990@outlook.com>
2025-01-28 16:05:14 +0800

    Adjust popup buttons for the new menu system

--- Interface/AddOns/NDui/Modules/Misc/Misc.lua
+++ Interface/AddOns/NDui/Modules/Misc/Misc.lua
@@ -370,75 +370,90 @@ do
 end
 
 -- Buttons to enhance popup menu
-function M:MenuButton_AddFriend()
-	C_FriendList_AddFriend(M.MenuButtonName)
+function M:CustomMenu_AddFriend(rootDescription, data, name)
+	rootDescription:CreateButton(DB.InfoColor..ADD_CHARACTER_FRIEND, function()
+		local fullName = data.server and data.name.."-"..data.server or data.name
+		C_FriendList.AddFriend(name or fullName)
+	end)
 end
 
-function M:MenuButton_CopyName()
-	local editBox = ChatEdit_ChooseBoxForSend()
-	local hasText = (editBox:GetText() ~= "")
-	ChatEdit_ActivateChat(editBox)
-	editBox:Insert(M.MenuButtonName)
-	if not hasText then editBox:HighlightText() end
+local guildInviteString = gsub(CHAT_GUILD_INVITE_SEND, HEADER_COLON, "")
+function M:CustomMenu_GuildInvite(rootDescription, data, name)
+	rootDescription:CreateButton(DB.InfoColor..guildInviteString, function()
+		local fullName = data.server and data.name.."-"..data.server or data.name
+		C_GuildInfo.Invite(name or fullName)
+	end)
 end
 
-function M:MenuButton_GuildInvite()
-	GuildInvite(M.MenuButtonName)
+function M:CustomMenu_CopyName(rootDescription, data, name)
+	rootDescription:CreateButton(DB.InfoColor..COPY_NAME, function()
+		local editBox = ChatEdit_ChooseBoxForSend()
+		local hasText = (editBox:GetText() ~= "")
+		ChatEdit_ActivateChat(editBox)
+		editBox:Insert(name or data.name)
+		if not hasText then editBox:HighlightText() end
+	end)
+end
+
+function M:CustomMenu_Whisper(rootDescription, data)
+	rootDescription:CreateButton(DB.InfoColor..WHISPER, function()
+		ChatFrame_SendTell(data.name)
+	end)
 end
 
 function M:QuickMenuButton()
 	if not C.db["Misc"]["MenuButton"] then return end
 
-	local menuList = {
-		{text = ADD_FRIEND, func = M.MenuButton_AddFriend, color = {0, .6, 1}},
-		{text = gsub(CHAT_GUILD_INVITE_SEND, HEADER_COLON, ""), func = M.MenuButton_GuildInvite, color = {0, .8, 0}},
-		{text = COPY_NAME, func = M.MenuButton_CopyName, color = {1, 0, 0}},
-	}
+	--hooksecurefunc(UnitPopupManager, "OpenMenu", function(_, which)
+	--	print("MENU_UNIT_"..which)
+	--end)
 
-	local frame = CreateFrame("Frame", "NDuiMenuButtonFrame", DropDownList1)
-	frame:SetSize(10, 10)
-	frame:SetPoint("TOPLEFT")
-	frame:Hide()
-	for i = 1, 3 do
-		local button = CreateFrame("Button", nil, frame)
-		button:SetSize(25, 10)
-		button:SetPoint("TOPLEFT", frame, (i-1)*28 + 2, -2)
-		B.PixelIcon(button, nil, true)
-		button.Icon:SetColorTexture(unpack(menuList[i].color))
-		button:SetScript("OnClick", menuList[i].func)
-		B.AddTooltip(button, "ANCHOR_TOP", menuList[i].text)
-	end
+	Menu.ModifyMenu("MENU_UNIT_SELF", function(_, rootDescription, data)
+		M:CustomMenu_CopyName(rootDescription, data)
+		M:CustomMenu_Whisper(rootDescription, data)
+	end)
 
-	hooksecurefunc("ToggleDropDownMenu", function(level, _, dropdownMenu)
-		if level and level > 1 then return end
+	Menu.ModifyMenu("MENU_UNIT_TARGET", function(_, rootDescription, data)
+		M:CustomMenu_CopyName(rootDescription, data)
+	end)
 
-		local name = dropdownMenu.name
-		local unit = dropdownMenu.unit
-		local bnetID = dropdownMenu.bnetIDAccount
-		local isPlayer = unit and UnitIsPlayer(unit)
-		local isFriendMenu = dropdownMenu == FriendsDropDown -- menus on FriendsFrame
-		if not name or (not isPlayer and not dropdownMenu.chatType and not isFriendMenu) then
-			frame:Hide()
-			return
-		end
+	Menu.ModifyMenu("MENU_UNIT_PLAYER", function(_, rootDescription, data)
+		M:CustomMenu_GuildInvite(rootDescription, data)
+	end)
 
-		if bnetID then
-			local gameID = select(6, BNGetFriendInfoByID(bnetID))
-			if gameID then
-				local _, characterName, client, realmName = BNGetGameAccountInfo(gameID)
-				if client == "WoW" and realmName ~= "" then
-					M.MenuButtonName = characterName.."-"..realmName
-					frame:Show()
-				end
+	Menu.ModifyMenu("MENU_UNIT_FRIEND", function(_, rootDescription, data)
+		M:CustomMenu_AddFriend(rootDescription, data)
+		M:CustomMenu_GuildInvite(rootDescription, data)
+	end)
+
+	Menu.ModifyMenu("MENU_UNIT_BN_FRIEND", function(_, rootDescription, data)
+		local fullName
+		local gameAccountInfo = data.accountInfo and data.accountInfo.gameAccountInfo
+		if gameAccountInfo then
+			local characterName = gameAccountInfo.characterName
+			local realmName = gameAccountInfo.realmName
+			if characterName and realmName then
+				fullName = characterName.."-"..realmName
 			end
-		else
-			local server = dropdownMenu.server
-			if not server or server == "" then
-				server = DB.MyRealm
-			end
-			M.MenuButtonName = name.."-"..server
-			frame:Show()
 		end
+		M:CustomMenu_AddFriend(rootDescription, data, fullName)
+		M:CustomMenu_GuildInvite(rootDescription, data, fullName)
+		M:CustomMenu_CopyName(rootDescription, data, fullName)
+	end)
+
+	Menu.ModifyMenu("MENU_UNIT_PARTY", function(_, rootDescription, data)
+		M:CustomMenu_GuildInvite(rootDescription, data)
+	end)
+
+	Menu.ModifyMenu("MENU_UNIT_RAID", function(_, rootDescription, data)
+		M:CustomMenu_AddFriend(rootDescription, data)
+		M:CustomMenu_GuildInvite(rootDescription, data)
+		M:CustomMenu_CopyName(rootDescription, data)
+		M:CustomMenu_Whisper(rootDescription, data)
+	end)
+
+	Menu.ModifyMenu("MENU_UNIT_RAID_PLAYER", function(_, rootDescription, data)
+		M:CustomMenu_GuildInvite(rootDescription, data)
 	end)
 end
 
