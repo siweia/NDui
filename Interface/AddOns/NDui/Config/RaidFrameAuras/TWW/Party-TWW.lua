b6843bf12480ee29278505b9a580b9f622fbb894
siweia <siwei1990@outlook.com>
2025-03-25 01:12:08 +0800

    Update auras

--- Interface/AddOns/NDui/Config/RaidFrameAuras/TWW/Party-TWW.lua
+++ Interface/AddOns/NDui/Config/RaidFrameAuras/TWW/Party-TWW.lua
@@ -46,6 +46,7 @@ module:RegisterDebuff(TIER, INSTANCE, 0, 432031) -- 抓握之血(BOSS战)
 
 INSTANCE = 1267 -- 圣焰隐修院
 module:RegisterSeasonSpells(TIER, INSTANCE)
+module:RegisterDebuff(TIER, INSTANCE, 0, 446403) -- 牺牲烈焰
 
 INSTANCE = 1210 -- 暗焰裂口
 module:RegisterSeasonSpells(TIER, INSTANCE)
