426df05115be2b785de757d8b656f3d1c86df573
siweia <siwei1990@outlook.com>
2025-06-28 23:54:29 +0800

    Fix buff frame error

--- Interface/AddOns/NDui/Modules/Auras/Auras.lua
+++ Interface/AddOns/NDui/Modules/Auras/Auras.lua
@@ -320,7 +320,7 @@ function A:CreateAuraIcon(button)
 	B.CreateBD(button, .25)
 	B.CreateSD(button)
 
-	button:RegisterForClicks("RightButtonUp")
+	--button:RegisterForClicks("RightButtonUp")
 	button:SetScript("OnAttributeChanged", A.OnAttributeChanged)
 	--button:HookScript("OnMouseDown", A.RemoveSpellFromIgnoreList)
 	button:SetScript("OnEnter", A.Button_OnEnter)
