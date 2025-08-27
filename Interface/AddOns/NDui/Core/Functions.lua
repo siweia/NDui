533fc9ab63ff5f822c692de02800a802ce03b095
VIKING <viking.yfy@qq.com>
2025-08-27 19:32:18 +0800

    Adjust the shadow frame level

--- Interface/AddOns/NDui/Core/Functions.lua
+++ Interface/AddOns/NDui/Core/Functions.lua
@@ -551,7 +551,7 @@ do
 		self.__shadow:SetOutside(self, size or 4, size or 4)
 		self.__shadow:SetBackdrop(shadowBackdrop)
 		self.__shadow:SetBackdropBorderColor(0, 0, 0, size and 1 or .4)
-		self.__shadow:SetFrameLevel(1)
+		self.__shadow:SetFrameLevel(frame:GetFrameLevel())
 
 		return self.__shadow
 	end
