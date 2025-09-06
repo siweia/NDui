9eaef597450e6eb6148f0ea536d480d963f7e273
siweia <siwei1990@outlook.com>
2025-09-06 23:45:31 +0800

    Correct API

--- Interface/AddOns/NDui/Libs/oUF/Elements/classpower.lua
+++ Interface/AddOns/NDui/Libs/oUF/Elements/classpower.lua
@@ -234,7 +234,7 @@ local function Visibility(self, event, unit)
 			if(not RequireSpec or RequireSpec == C_SpecializationInfo.GetSpecialization()) then
 				-- use 'player' instead of unit because 'SPELLS_CHANGED' is a unitless event
 				if(not RequirePower or RequirePower == UnitPowerType('player')) then
-					if(not RequireSpell or C_SpellBook.IsSpellKnown(RequireSpell)) then
+					if(not RequireSpell or IsPlayerSpell(RequireSpell)) then
 						self:UnregisterEvent('SPELLS_CHANGED', Visibility)
 						shouldEnable = true
 						unit = 'player'
