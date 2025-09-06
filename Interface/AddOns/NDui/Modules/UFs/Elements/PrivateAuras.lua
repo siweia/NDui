f7734ef4f1faea8172624a9f462412056de7c883
siweia <siwei1990@outlook.com>
2025-09-07 00:49:28 +0800

    Fix private auras visibility

--- Interface/AddOns/NDui/Modules/UFs/Elements/PrivateAuras.lua
+++ Interface/AddOns/NDui/Modules/UFs/Elements/PrivateAuras.lua
@@ -6,12 +6,19 @@ local PA = B:GetModule("PrivateAuras")
 
 function UF:CreatePrivateAuras(frame)
 	frame.PrivateAuras = CreateFrame("Frame", frame:GetName().."PrivateAuras", frame)
-	PA:RemoveAuras(frame.PrivateAuras)
+	hooksecurefunc(frame, "UpdateAllElements", UF.UpdatePrivateAuras)
+end
 
-	local db = C.db["UFs"]
-	PA:SetupPrivateAuras(db, frame.PrivateAuras, frame.unit)
-	frame.PrivateAuras:ClearAllPoints()
-	frame.PrivateAuras:SetPoint("TOP", frame, "TOP", 0, 0)
-	frame.PrivateAuras:SetSize(db.PrivateSize, db.PrivateSize)
-	frame.PrivateAuras:SetFrameLevel(frame:GetFrameLevel() + 5)
+function UF.UpdatePrivateAuras(frame)
+	if frame.PrivateAuras then
+		PA:RemoveAuras(frame.PrivateAuras)
+
+		local db = C.db["UFs"]
+		if db then
+			PA:SetupPrivateAuras(db, frame.PrivateAuras, frame.unit)
+			frame.PrivateAuras:ClearAllPoints()
+			frame.PrivateAuras:SetPoint("TOP", frame, "TOP", 0, 0)
+			frame.PrivateAuras:SetSize(db.PrivateSize, db.PrivateSize)
+		end
+	end
 end
