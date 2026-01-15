a03c950d9a50f794bfc3ae6c25dca6a7b6e1f8dc
siweia <siwei1990@outlook.com>
2026-01-16 02:00:25 +0800

    Missing ReplaceIconString

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/AddonList.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/FrameXML/AddonList.lua
@@ -1,6 +1,14 @@
 local _, ns = ...
 local B, C, L, DB = unpack(ns)
 
+function B:ReplaceIconString(text)
+	if not text then text = self:GetText() end
+	if not text or text == "" then return end
+
+	local newText, count = gsub(text, "|T([^:]-):[%d+:]+|t", "|T%1:14:14:0:0:64:64:5:59:5:59|t")
+	if count > 0 then self:SetFormattedText("%s", newText) end
+end
+
 tinsert(C.defaultThemes, function()
 	if not C.db["Skins"]["BlizzardSkins"] then return end
 
