c78e6ede5a384d6734c8457e71d68e7c26c4e2b0
siweia <siwei1990@outlook.com>
2026-01-26 01:40:33 +0800

    Fix quick keybind

--- Interface/AddOns/NDui/Modules/ActionBar/Keybind.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/Keybind.lua
@@ -114,7 +114,7 @@ function Bar:Bind_Create()
 	end
 
 	if not IsAddOnLoaded("Blizzard_MacroUI") then
-		hooksecurefunc("LoadAddOn", Bar.Bind_RegisterMacro)
+		hooksecurefunc(C_AddOns, "LoadAddOn", Bar.Bind_RegisterMacro)
 	else
 		Bar.Bind_RegisterMacro("Blizzard_MacroUI")
 	end
