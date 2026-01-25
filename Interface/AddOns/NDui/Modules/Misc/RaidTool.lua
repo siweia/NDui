f91b05d5d08831575354781527a737299a17cdf5
siweia <siwei1990@outlook.com>
2026-01-25 15:27:17 +0800

    Force to leave party when holding key shift

--- Interface/AddOns/NDui/Modules/Misc/RaidTool.lua
+++ Interface/AddOns/NDui/Modules/Misc/RaidTool.lua
@@ -57,7 +57,7 @@ function M:RaidTool_Header()
 		end
 	end)
 	frame:SetScript("OnDoubleClick", function(_, btn)
-		if btn == "RightButton" and (IsPartyLFG() and IsLFGComplete() or not IsInInstance()) then
+		if btn == "RightButton" and (IsShiftKeyDown() or (IsPartyLFG() and IsLFGComplete() or not IsInInstance())) then
 			LeaveParty()
 		end
 	end)
