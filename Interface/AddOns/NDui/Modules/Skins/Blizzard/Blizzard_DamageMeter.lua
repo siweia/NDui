2c163baeb6eb615b813b530855dc9d7edff78233
siweia <siwei1990@outlook.com>
2026-02-04 01:16:04 +0800

    Add skin for blizzard damage meter

---
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_DamageMeter.lua
@@ -0,0 +1,45 @@
+local _, ns = ...
+local B, C, L, DB = unpack(ns)
+
+local function UpdateButtonState(button)
+	button.Icon:SetTexture("Interface\\WorldMap\\GEAR_64GREY")
+	button.Icon:SetInside(nil, 2, 2)
+	if button:IsOver() then
+		button.Icon:SetVertexColor(1, .8, 0)
+	else
+		button.Icon:SetVertexColor(1, 1, 1)
+	end
+end
+
+local function ReskinMeterWindow(frame)
+	if not frame or frame.styled then return end
+
+	frame.Header:SetTexture()
+	frame.Background:SetTexture()
+	B.SetBD(frame.Bckground)
+	B.ReskinDropDown(frame.DamageMeterTypeDropdown)
+	B.Reskin(frame.SessionDropdown)
+	frame.SessionDropdown:SetSize(42, 22)
+
+	UpdateButtonState(frame.SettingsDropdown)
+	hooksecurefunc(frame.SettingsDropdown, "OnButtonStateChanged", UpdateButtonState)
+
+	B.ReskinTrimScroll(frame.SourceWindow.ScrollBar)
+	frame.SourceWindow.Background:SetTexture()
+	B.SetBD(frame.SourceWindow.Background)
+
+	frame.styled = true
+end
+
+C.themes["Blizzard_DamageMeter"] = function()
+	hooksecurefunc(DamageMeter, "SetupSessionWindow", function(self, windowData, windowIndex)
+		ReskinMeterWindow(windowData.sessionWindow)
+	end)
+
+	for i = 1, 3 do
+		local frame = _G["DamageMeterSessionWindow"..i]
+		if frame then
+			ReskinMeterWindow(frame)
+		end
+	end
+end
