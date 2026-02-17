37e7a78469f82fe187c6073b104db8cab5aaa03b
siweia <siwei1990@outlook.com>
2026-02-17 18:49:55 +0800

    CVar to show classcolor name on onlyname mode

--- Interface/AddOns/NDui/Modules/UFs/Nameplates.lua
+++ Interface/AddOns/NDui/Modules/UFs/Nameplates.lua
@@ -24,6 +24,7 @@ function UF:UpdatePlateCVars()
 	SetCVar("nameplateMaxAlpha", C.db["Nameplate"]["MinAlpha"])
 	SetCVar("nameplateOverlapV", C.db["Nameplate"]["VerticalSpacing"])
 	SetCVar("nameplateShowOnlyNameForFriendlyPlayerUnits", C.db["Nameplate"]["CVarOnlyNames"] and 1 or 0)
+	SetCVar("nameplateUseClassColorForFriendlyPlayerUnitNames", C.db["Nameplate"]["CVarOnlyNames"] and 1 or 0)
 	SetCVar("nameplateShowFriendlyNPCs", C.db["Nameplate"]["CVarShowNPCs"] and 1 or 0)
 	SetCVar("nameplateMaxDistance", C.db["Nameplate"]["PlateRange"])
 end
