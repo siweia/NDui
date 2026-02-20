a66ad771a68addd4414adcaa56f035498d2a1ea0
siweia <siwei1990@outlook.com>
2026-02-20 18:39:32 +0800

    Use petbar and stancebar from retail

--- Interface/AddOns/NDui/Modules/ActionBar/Bars/Hide_blizzart.lua
+++ Interface/AddOns/NDui/Modules/ActionBar/Bars/Hide_blizzart.lua
@@ -11,17 +11,14 @@ local scripts = {
 }
 
 local framesToHide = {
-	MainMenuBar, OverrideActionBar, MultiBarLeft, MultiBarRight,
-	MainActionBar, MultiBarBottomLeft, MultiBarBottomRight, StatusTrackingBarManager, BagsBar, PossessActionBar
+	MainMenuBar, MainActionBar, MultiBarBottomLeft, MultiBarBottomRight, MultiBarLeft, MultiBarRight, MultiBar5, MultiBar6, MultiBar7, OverrideActionBar, PossessActionBar, PetActionBar, StanceBar, StatusTrackingBarManager, BagsBar
 }
 
 local framesToDisable = {
-	MainMenuBar,
-	MicroButtonAndBagsBar, MainMenuBarArtFrame, StatusTrackingBarManager,
-	ActionBarDownButton, ActionBarUpButton,
+	MainMenuBar, MainActionBar, MultiBarBottomLeft, MultiBarBottomRight, MultiBarLeft, MultiBarRight, MultiBar5, MultiBar6, MultiBar7, PossessActionBar, PetActionBar, StanceBar,
+	MicroButtonAndBagsBar, StatusTrackingBarManager, MainMenuBarVehicleLeaveButton,
 	OverrideActionBar,
 	OverrideActionBarExpBar, OverrideActionBarHealthBar, OverrideActionBarPowerBar, OverrideActionBarPitchFrame,
-	VerticalMultiBarsContainer, PossessActionBar
 }
 
 local function DisableAllScripts(frame)
@@ -36,6 +33,37 @@ local function updateTokenVisibility()
 	TokenFrame_LoadUI()
 	TokenFrame_Update()
 	BackpackTokenFrame_Update()
+end
+
+local function buttonEventsRegisterFrame(self, added)
+	local frames = self.frames
+	for index = #frames, 1, -1 do
+		local frame = frames[index]
+		local wasAdded = frame == added
+		if not added or wasAdded then
+			if not strmatch(frame:GetName(), "ExtraActionButton%d") then
+				self.frames[index] = nil
+			end
+
+			if wasAdded then
+				break
+			end
+		end
+	end
+end
+
+local function DisableDefaultBarEvents() -- credit: Simpy
+	-- shut down some events for things we dont use
+	_G.ActionBarController:UnregisterAllEvents()
+	_G.ActionBarController:RegisterEvent("SETTINGS_LOADED") -- this is needed for page controller to spawn properly
+	_G.ActionBarController:RegisterEvent("UPDATE_EXTRA_ACTIONBAR") -- this is needed to let the ExtraActionBar show
+	_G.ActionBarActionEventsFrame:UnregisterAllEvents()
+	-- used for ExtraActionButton and TotemBar (on wrath)
+	_G.ActionBarButtonEventsFrame:UnregisterAllEvents()
+	_G.ActionBarButtonEventsFrame:RegisterEvent("ACTIONBAR_SLOT_CHANGED") -- needed to let the ExtraActionButton show and Totems to swap
+	_G.ActionBarButtonEventsFrame:RegisterEvent("ACTIONBAR_UPDATE_COOLDOWN") -- needed for cooldowns of them both
+	hooksecurefunc(_G.ActionBarButtonEventsFrame, "RegisterFrame", buttonEventsRegisterFrame)
+	buttonEventsRegisterFrame(_G.ActionBarButtonEventsFrame)
 end
 
 function Bar:HideBlizz()
@@ -53,6 +81,7 @@ function Bar:HideBlizz()
 		DisableAllScripts(frame)
 	end
 
+	DisableDefaultBarEvents()
 	-- Hide blizz options
 	SetCVar("multiBarRightVerticalLayout", 0)
 	-- Update token panel
