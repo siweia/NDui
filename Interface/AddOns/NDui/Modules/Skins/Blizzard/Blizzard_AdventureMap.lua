1c18df73ddcba3dd4b93fec2d66101f78c61892a
siweia <siwei1990@outlook.com>
2026-03-22 11:25:45 +0800

    Update skins for AdventureMap

--- Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_AdventureMap.lua
+++ Interface/AddOns/NDui/Modules/Skins/Blizzard/Blizzard_AdventureMap.lua
@@ -15,17 +15,21 @@ C.themes["Blizzard_AdventureMap"] = function()
 		if self.styled then return end
 		dialog.Details.Child.TitleHeader:SetTextColor(1, .8, 0)
 		dialog.Details.Child.ObjectivesHeader:SetTextColor(1, .8, 0)
-
 		self.styled = true
+	end)
 
-		for _, bu in next, {dialog:GetChildren()} do
-			if bu.Icon and not bu.bg then
-				B.ReskinIcon(bu.Icon)
-				local bg = B.CreateBDFrame(bu.Icon, .25)
-				bg:SetPoint("BOTTOMRIGHT")
-				bu.ItemNameBG:Hide()
-				bu.bg = bg
-			end
+	local function handleReward(bu)
+		if bu.Icon and not bu.bg then
+			B.ReskinIcon(bu.Icon)
+			local bg = B.CreateBDFrame(bu.Icon, .25)
+			bg:SetPoint("BOTTOMRIGHT")
+			bu.ItemNameBG:Hide()
+			bu.bg = bg
+		end
+	end
+	hooksecurefunc(dialog, "RefreshRewards", function(self)
+		for button in self.rewardPool:EnumerateActive() do
+			handleReward(button)
 		end
 	end)
 end
