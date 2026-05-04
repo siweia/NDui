dee8dd15313effb70f2713c38c417278bab12f57
siweia <siwei1990@outlook.com>
2026-05-05 01:56:58 +0800

    Refactor channel rename

--- Interface/AddOns/NDui/Modules/Chat/ChannelRename.lua
+++ Interface/AddOns/NDui/Modules/Chat/ChannelRename.lua
@@ -2,8 +2,9 @@
 local B, C, L, DB = unpack(ns)
 local module = B:GetModule("Chat")
 
-local gsub, strfind, strmatch = string.gsub, string.find, string.match
+local gsub, strfind, strmatch, format, strsub, strlen = string.gsub, string.find, string.match, string.format, string.sub, string.len
 local BetterDate, time, date, GetCVarBool = BetterDate, time, date, GetCVarBool
+local RemoveExtraSpaces = RemoveExtraSpaces
 local INTERFACE_ACTION_BLOCKED = INTERFACE_ACTION_BLOCKED
 local C_DateAndTime_GetCurrentCalendarTime = C_DateAndTime.GetCurrentCalendarTime
 local colon = HEADER_COLON
@@ -35,6 +36,7 @@ local function AddAuthorLogo(link, unitName)
 	if unitName and DB.Devs[unitName] then
 		return "|T"..DB.chatLogo..":12:24|t"..link
 	end
+	return link
 end
 
 -- Channel name abbr
@@ -92,11 +94,6 @@ local CHANNEL_ABBR_LOCALES = {
 	GUILD = { abbr = L["GuildAbbr"] },
 	OFFICER = { abbr = L["OfficerAbbr"] },
 }
-
-local PARTY_LEADER = strmatch(CHAT_PARTY_LEADER_GET, "|h%[(.-)%]|h")
-local PARTY_GUIDE = strmatch(CHAT_PARTY_GUIDE_GET, "|h%[(.-)%]|h")
-local RAID_LEADER = strmatch(CHAT_RAID_LEADER_GET, "|h%[(.-)%]|h")
-local INSTANCE_LEADER = strmatch(CHAT_INSTANCE_CHAT_LEADER_GET, "|h%[(.-)%]|h")
 
 local matchPattern = "(|H(%w+):?([^:]+):?(%d*)|h)%[(.-)%]|h"
 
@@ -164,43 +161,94 @@ local function KillCNColon(link, tag)
 	end
 end
 
-function module:UpdateChannelNames(text, r, g, b, ...)
-	if not text or B:IsSecretValue(text) then
-		return self:oldAddMsg(text, r, g, b, ...)
+-- Filter: intercept chat events, build formatted msg, apply modifications, call AddMessage securely
+local function ChatMsgFilter(self, event,
+	msg, sender, language, channelString, target, flags, zoneChannelID, channelIndex, channelBaseName, languageID,
+	lineID, senderGUID, bnSenderID, isMobile, isSubtitle, hideSenderInLetterbox, suppressRaidIcons)
+
+	if not msg or B:IsSecretValue(msg) then
+		return
 	end
 
-	if strfind(text, INTERFACE_ACTION_BLOCKED) and not DB.isDeveloper then return end
+	if strfind(msg, INTERFACE_ACTION_BLOCKED) and not DB.isDeveloper then
+		return true
+	end
+	-- 1. Get format key (e.g. CHAT_SAY_GET = "%s:\32")
+	local chatType = strsub(event, 10)
+	local formatKey = _G["CHAT_"..chatType.."_GET"]
+	if not formatKey then return end
 
-	-- Timestamp
+	-- 2. Get colored name (with class colors etc.)
+	local coloredName = ChatFrameUtil.GetDecoratedSenderName(event, msg, sender, language, channelString, target, flags, zoneChannelID, channelIndex, channelBaseName, languageID, lineID, senderGUID, bnSenderID, isMobile)
+
+	-- 3. Get player flags (AFK/DND/GM icons)
+	local pflag = ChatFrameUtil.GetPFlag(flags, zoneChannelID, channelIndex)
+
+	-- 4. Build player link
+	local playerLink
+	if chatType == "BN_WHISPER" or chatType == "BN_WHISPER_INFORM" then
+		playerLink = GetBNPlayerLink(sender, "["..coloredName.."]", bnSenderID, lineID, ChatFrameUtil.GetChatCategory(chatType), 0)
+	else
+		playerLink = GetPlayerLink(sender, "["..coloredName.."]", lineID, ChatFrameUtil.GetChatCategory(chatType), 0)
+	end
+
+	-- 5. Build the base message (same as Blizzard's MessageFormatter)
+	local msgText = msg
+	-- Escape % signs
+	msgText = gsub(msgText, "%%", "%%%%")
+	-- Replace icon/group expressions
+	msgText = C_ChatInfo.ReplaceIconAndGroupExpressions(msgText, suppressRaidIcons)
+	-- Remove extra spaces
+	msgText = RemoveExtraSpaces(msgText)
+
+	local outMsg = format(formatKey..msgText, pflag..playerLink)
+
+	-- 6. Add channel prefix for custom channels (channelString non-empty)
+	local channelLength = strlen(channelString)
+	if channelLength > 0 then
+		local channelName = ChatFrameUtil.ResolvePrefixedChannelName(channelString)
+		if channelName then
+			outMsg = "|Hchannel:channel:"..(channelIndex or 0).."|h["..channelName.."]|h "..outMsg
+		end
+	end
+
+	-- 7. Apply modifications on the formatted text
+	-- Timestamp (prefix)
 	if NDuiADB["TimestampFormat"] > 1 then
 		local locTime, realmTime = GetCurrentTime()
-		local defaultTimestamp = GetCVar("showTimestamps")
-		if defaultTimestamp == "none" then defaultTimestamp = nil end
-		local oldTimeStamp = defaultTimestamp and gsub(BetterDate(defaultTimestamp, locTime), "%[([^]]*)%]", "%%[%1%%]")
-		if oldTimeStamp then
-			text = gsub(text, oldTimeStamp, "")
-		end
 		local timeStamp = BetterDate(DB.GreyColor..timestampFormat[NDuiADB["TimestampFormat"]].."|r", realmTime or locTime)
-		text = timeStamp..text
+		outMsg = timeStamp..outMsg
 	end
 
-	text = gsub(text, "(|Hplayer:([^|:]+))", AddAuthorLogo)
+	-- Author Logo
+	outMsg = gsub(outMsg, "(|Hplayer:([^|:]+))", AddAuthorLogo)
+	-- Kill colon
 	if isCN then
-		text = gsub(text, cnPattern, KillCNColon) -- 干掉全角冒号
+		outMsg = gsub(outMsg, cnPattern, KillCNColon)
 	end
-	text = gsub(text, enPattern, KillColon) -- 干掉半角冒号
-	--text = gsub(text, "(|Hplayer:.-)%[(.-)%]", "%1%2") -- 干掉名字方括号
-	text = gsub(text, matchPattern, AbbrChannelName)
+	outMsg = gsub(outMsg, enPattern, KillColon)
+	-- Channel abbreviation
+	outMsg = gsub(outMsg, matchPattern, AbbrChannelName)
 
-	return self:oldAddMsg(text, r, g, b, ...)
+	-- 8. Get color info and display
+	local info = ChatTypeInfo[chatType] or ChatTypeInfo["SYSTEM"]
+	self:AddMessage(outMsg, info.r, info.g, info.b, info.id)
+
+	return true
 end
 
 function module:ChannelRename()
-	for i = 1, NUM_CHAT_WINDOWS do
-		if i ~= 2 then
-			local chatFrame = _G["ChatFrame"..i]
-			chatFrame.oldAddMsg = chatFrame.AddMessage
-			chatFrame.AddMessage = module.UpdateChannelNames
-		end
+	local events = {
+		"CHAT_MSG_SAY", "CHAT_MSG_YELL",
+		"CHAT_MSG_GUILD", "CHAT_MSG_OFFICER",
+		"CHAT_MSG_PARTY", "CHAT_MSG_PARTY_LEADER", "CHAT_MSG_PARTY_GUIDE",
+		"CHAT_MSG_RAID", "CHAT_MSG_RAID_LEADER",
+		"CHAT_MSG_INSTANCE_CHAT", "CHAT_MSG_INSTANCE_CHAT_LEADER",
+		"CHAT_MSG_WHISPER", "CHAT_MSG_WHISPER_INFORM",
+		"CHAT_MSG_BN_WHISPER", "CHAT_MSG_BN_WHISPER_INFORM",
+		"CHAT_MSG_CHANNEL",
+	}
+	for _, event in ipairs(events) do
+		ChatFrame_AddMessageEventFilter(event, ChatMsgFilter)
 	end
 end
