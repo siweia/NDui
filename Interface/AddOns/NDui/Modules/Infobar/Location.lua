7cb9fd749c79a13403dd1e9b26648b43df3863bf
siweia <siwei1990@outlook.com>
2026-07-23 00:13:29 +0800

    Option to show coords on infobar

--- Interface/AddOns/NDui/Modules/Infobar/Location.lua
+++ Interface/AddOns/NDui/Modules/Infobar/Location.lua
@@ -29,43 +29,24 @@ local function formatCoords()
 	return format("%.1f, %.1f", coordX*100, coordY*100)
 end
 
-info.eventList = {
-	"ZONE_CHANGED",
-	"ZONE_CHANGED_INDOORS",
-	"ZONE_CHANGED_NEW_AREA",
-	"PLAYER_ENTERING_WORLD",
-}
-
-info.onEvent = function(self)
-	subzone = GetSubZoneText()
-	zone = GetZoneText()
-	pvpType, _, faction = GetZonePVPInfo()
-	pvpType = pvpType or "neutral"
-
-	local r, g, b = unpack(zoneInfo[pvpType][2])
-	self.text:SetText((subzone ~= "") and subzone or zone)
-	self.text:SetTextColor(r, g, b)
-end
-
-local function UpdateCoords(self, elapsed)
-	self.elapsed = (self.elapsed or 0) + elapsed
-	if self.elapsed > .1 then
-		local x, y = mapModule:GetPlayerMapPos(C_Map_GetBestMapForUnit("player"))
-		if x then
-			coordX, coordY = x, y
-		else
-			coordX, coordY = 0, 0
-			self:SetScript("OnUpdate", nil)
-		end
-		self:onEnter()
-
-		self.elapsed = 0
+local function UpdateLocationText(self)
+	if NDuiADB["ShowCoords"] then
+		self.text:SetFormattedText("%s |cffffffff(%s)|r", subzone, formatCoords())
+	else
+		self.text:SetText(subzone)
 	end
 end
 
-info.onEnter = function(self)
-	self:SetScript("OnUpdate", UpdateCoords)
+local function RefreshCoords(mapID)
+	local x, y = mapModule:GetPlayerMapPos(mapID or C_Map_GetBestMapForUnit("player"))
+	if x then
+		coordX, coordY = x, y
+	else
+		coordX, coordY = 0, 0
+	end
+end
 
+local function UpdateTooltip(self)
 	local _, anchor, offset = module:GetTooltipAnchor(info)
 	GameTooltip:SetOwner(self, "ANCHOR_"..anchor, 0, offset)
 	GameTooltip:ClearLines()
@@ -82,19 +63,87 @@ info.onEnter = function(self)
 
 	GameTooltip:AddDoubleLine(" ", DB.LineString)
 	GameTooltip:AddDoubleLine(" ", DB.LeftButton..L["WorldMap"].." ", 1,1,1, .6,.8,1)
+	GameTooltip:AddDoubleLine(" ", DB.ScrollButton..L["Switch Mode"].." ", 1,1,1, .6,.8,1)
 	GameTooltip:AddDoubleLine(" ", DB.RightButton..L["Send My Pos"].." ", 1,1,1, .6,.8,1)
 	GameTooltip:Show()
 end
 
+local function UpdateCoords(self, elapsed)
+	self.elapsed = (self.elapsed or 0) + elapsed
+	if self.elapsed > .1 then
+		RefreshCoords()
+		if NDuiADB["ShowCoords"] then
+			UpdateLocationText(self)
+		end
+		if GameTooltip:IsOwned(self) then
+			UpdateTooltip(self)
+		end
+
+		self.elapsed = 0
+	end
+end
+
+info.eventList = {
+	"ZONE_CHANGED",
+	"ZONE_CHANGED_INDOORS",
+	"ZONE_CHANGED_NEW_AREA",
+	"PLAYER_ENTERING_WORLD",
+}
+
+info.onEvent = function(self)
+	subzone = GetMinimapZoneText()
+	zone = GetAreaText()
+	pvpType, _, faction = GetZonePVPInfo()
+	pvpType = pvpType or "neutral"
+
+	local r, g, b = unpack(zoneInfo[pvpType][2])
+	if NDuiADB["ShowCoords"] then
+		RefreshCoords()
+		self:SetScript("OnUpdate", UpdateCoords)
+	end
+	UpdateLocationText(self)
+	self.text:SetTextColor(r, g, b)
+end
+
+info.onEnter = function(self)
+	RefreshCoords()
+	UpdateLocationText(self)
+	self.elapsed = 0
+	self:SetScript("OnUpdate", UpdateCoords)
+	UpdateTooltip(self)
+end
+
 info.onLeave = function(self)
-	self:SetScript("OnUpdate", nil)
+	if not NDuiADB["ShowCoords"] then
+		self:SetScript("OnUpdate", nil)
+	end
 	GameTooltip:Hide()
 end
 
-info.onMouseUp = function(_, btn)
+
+local zoneString = "|cffffff00|Hworldmap:%d+:%d+:%d+|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a %s: %s (%s) %s]|h|r"
+
+info.onMouseUp = function(self, btn)
 	if btn == "LeftButton" then
-		ToggleWorldMap()
+		if InCombatLockdown() then UIErrorsFrame:AddMessage(DB.InfoColor..ERR_NOT_IN_COMBAT) return end -- fix by LibShowUIPanel
+		ToggleFrame(WorldMapFrame)
+	elseif btn == "MiddleButton" then
+		NDuiADB["ShowCoords"] = not NDuiADB["ShowCoords"]
+		RefreshCoords()
+		UpdateLocationText(self)
+		self.elapsed = 0
+		if NDuiADB["ShowCoords"] or GameTooltip:IsOwned(self) then
+			self:SetScript("OnUpdate", UpdateCoords)
+		else
+			self:SetScript("OnUpdate", nil)
+		end
+		if GameTooltip:IsOwned(self) then
+			UpdateTooltip(self)
+		end
 	elseif btn == "RightButton" then
+		local mapID = C_Map_GetBestMapForUnit("player")
+		if not mapID then return end
+		RefreshCoords(mapID)
 		local hasUnit = UnitExists("target") and not UnitIsPlayer("target")
 		local unitName = nil
 		if hasUnit then unitName = UnitName("target") end
