f55ddeaec97f56e545fb69134da890f110dc1d01
siweia <siwei1990@outlook.com>
2026-09-19 01:50:23 +0800

    Fix error

--- Interface/AddOns/NDui/Plugins/DragEmAll.lua
+++ Interface/AddOns/NDui/Plugins/DragEmAll.lua
@@ -76,7 +76,7 @@ local lodFrames = {
 	Blizzard_Collections		= { ["WardrobeFrame"] = false, ["WardrobeOutfitEditFrame"] = false },
 	Blizzard_CovenantRenown		= { ["CovenantRenownFrame"] = false, },
 	Blizzard_CovenantSanctum	= { ["CovenantSanctumFrame"] = false, },
-	Blizzard_EncounterJournal	= { ["EncounterJournal"] = false },
+	--Blizzard_EncounterJournal	= { ["EncounterJournal"] = false },
 	Blizzard_FlightMap			= { ["FlightMapFrame"] = false },
 	Blizzard_GenericTraitUI		= { ["GenericTraitFrame"] = false },
 	Blizzard_GMSurveyUI			= { ["GMSurveyFrame"] = false },
